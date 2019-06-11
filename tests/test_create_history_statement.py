d44d85d9df512ca661411e139fb4b0f47e71f691
Unknown <up799232@myport.ac.uk>
2019-06-11 11:47:41 +0100

    Opened New Branch
    New branch for Snowflake fix.

--- tests/test_create_history_statement.py
+++ tests/test_create_history_statement.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 import sqlScript as sql
+import pandas as pd
+import sqlalchemy
 
 
 class TestCreateHistoryStatement(TestCase):
@@ -11,4 +13,15 @@ class TestCreateHistoryStatement(TestCase):
     def test_create_day_statement(self):
         statement = sql.create_day_statement()
         self.assertIsInstance(statement, str)
+
+    def test_execute_statement(self):
+        view_name = "test_view_test"
+        result = sql.execute_statement(view_name,
+                                       "CREATE VIEW {} AS "
+                                       "SELECT orderkey, quantity "
+                                       "FROM lineitem;".format(view_name))
+        self.assertIsInstance(result, pd.DataFrame)
+
+    def test_get_credentials(self):
+        credentials = sql.get_credentials("./")
 
