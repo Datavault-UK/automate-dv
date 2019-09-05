498db276a2d5d733876da5b4ba75402e63cd0ee2
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-09-05 07:57:53 +0100

    Modified main templates to include window function
    - Prevents duplicate load where only source is different (but same PK)

--- src/SnowflakeDV/macros/hub_template.sql
+++ src/SnowflakeDV/macros/hub_template.sql
@@ -2,12 +2,13 @@
 
 SELECT {{ snow_vault.cast([tgt_pk, tgt_nk, tgt_source, tgt_ldts]) }}
  FROM (
-  SELECT DISTINCT {{ src_cols }}
+  SELECT DISTINCT {{ src_cols }}, FIRST_SOURCE
   FROM
     ({{ snow_vault.union(src_pk, src_nk, src_source, src_ldts, tgt_pk|last, hash_model) }})
  AS b)
 AS stg
 {% if is_incremental() -%}
- WHERE {{ snow_vault.prefix(src_pk[0], 'stg') }} NOT IN (SELECT {{ tgt_pk|last }} FROM {{ this }})
+WHERE {{ snow_vault.prefix(src_pk[0], 'stg') }} NOT IN (SELECT {{ tgt_pk|last }} FROM {{ this }})
+AND FIRST_SOURCE IS NULL
 {%- endif -%}
 {%- endmacro -%}
--- src/SnowflakeDV/macros/utility/union.sql
+++ src/SnowflakeDV/macros/utility/union.sql
@@ -1,11 +1,13 @@
 {%- macro union(src_pk, src_nk, src_source, src_ldts, tgt_pk, hash_model) -%}
 
 {%- for src in src_source -%}
-    SELECT {{ snow_vault.prefix([src_pk[loop.index0],
+    SELECT DISTINCT {{ snow_vault.prefix([src_pk[loop.index0],
            src_nk[loop.index0],
            src_source[loop.index0],
            src_ldts[loop.index0]], 'a') }},
-           lag(c.{{src_ldts[loop.index0]}}, 1) over(partition by c.{{ tgt_pk }} order by c.{{src_ldts[loop.index0]}}) as FIRST_SEEN
+           lag(a.{{ src_source[loop.index0] }}, 1)
+           over(partition by a.{{ src_pk[loop.index0] }}
+           order by a.{{ src_pk[loop.index0] }}) as FIRST_SOURCE
     FROM {{ hash_model[loop.index0] }} AS a
     LEFT JOIN {{ this }} AS c
     ON a.CUSTOMER_PK = c.CUSTOMER_PK
