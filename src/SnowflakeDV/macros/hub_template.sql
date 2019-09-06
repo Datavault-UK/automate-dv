3c812de27463b2613afe660cda09b31ea936e5cf
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-09-06 16:48:52 +0100

    Using newly generated SQL from dbtTest
    - Also minor formatting changes to macros

--- src/SnowflakeDV/macros/hub_template.sql
+++ src/SnowflakeDV/macros/hub_template.sql
@@ -2,13 +2,9 @@
 
 SELECT {{ snow_vault.cast([tgt_pk, tgt_nk, tgt_source, tgt_ldts]) }}
  FROM (
-  SELECT DISTINCT {{ src_cols }}, FIRST_SOURCE
-  FROM
-    ({{ snow_vault.union(src_table, src_pk, src_nk, src_source, src_ldts, tgt_pk|last, hash_model) }})
+    {{ snow_vault.union(src_table, src_cols, src_pk, src_nk, src_source, src_ldts,
+      tgt_pk|last, hash_model) }}
  AS b)
 AS stg
-{% if is_incremental() -%}
-WHERE {{ snow_vault.prefix(src_pk[0], 'stg') }} NOT IN (SELECT {{ tgt_pk|last }} FROM {{ this }})
-AND FIRST_SOURCE IS NULL
-{%- endif -%}
+WHERE FIRST_SOURCE IS NULL
 {%- endmacro -%}
--- src/SnowflakeDV/macros/utility/union.sql
+++ src/SnowflakeDV/macros/utility/union.sql
@@ -1,23 +1,23 @@
-{%- macro union(src_table, src_pk, src_nk, src_source, src_ldts, tgt_pk, hash_model) -%}
-
-{%- for src in src_table -%}
-    SELECT DISTINCT {{ snow_vault.prefix([
-           src_pk[loop.index0],
-           src_nk[loop.index0],
-           src_source,
-           src_ldts], 'a') }},
-           lag(a.{{ src_source }}, 1)
-           over(partition by a.{{ src_pk[loop.index0] }}
-           order by a.{{ src_pk[loop.index0] }}) as FIRST_SOURCE
-    FROM {{ hash_model }} AS a
-    {% if is_incremental() -%}
-    LEFT JOIN {{ this }} AS c
-    ON a.{{ src_pk[loop.index0] }} = c.{{ tgt_pk }}
-    AND c.{{ tgt_pk }} IS NULL
-    {%- endif %}
-    {% if not loop.last -%}
-    UNION
-    {% endif %}
-{%- endfor -%}
-
+{%- macro union(src_table, src_cols, src_pk, src_nk, src_source, src_ldts, tgt_pk, hash_model) -%}
+    SELECT DISTINCT {{ src_cols }},
+           lag({{ src_source }}, 1)
+           over(partition by {{ src_pk[0] }}
+           order by {{ src_pk[0] }}) as FIRST_SOURCE
+    FROM (
+        {%- for src in src_table -%}
+        SELECT DISTINCT {{ snow_vault.prefix([
+        src_pk[loop.index0],
+        src_nk[loop.index0],
+        src_source,
+        src_ldts], 'a') }}
+        FROM {{ hash_model }} AS a
+        {% if is_incremental() -%}
+        LEFT JOIN {{ this }} AS c
+        ON a.{{ src_pk[loop.index0] }} = c.{{ tgt_pk }}
+        AND c.{{ tgt_pk }} IS NULL
+        {%- endif %}
+        {% if not loop.last -%}
+        UNION
+        {% endif %}
+        {%- endfor %})
 {%- endmacro -%}
