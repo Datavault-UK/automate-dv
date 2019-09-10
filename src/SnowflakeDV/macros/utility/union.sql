2795900a833922cf81c588a0760f95835a240268
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-09-10 12:03:15 +0100

    Modified union to ensure different letters for each union table

--- src/SnowflakeDV/macros/utility/union.sql
+++ src/SnowflakeDV/macros/utility/union.sql
@@ -1,20 +1,30 @@
 {%- macro union(src_table, src_cols, src_pk, src_nk, src_ldts, src_source, tgt_pk, hash_model) -%}
+
+ {% set letters='abcdefghijklmnopqrstuvwxyz' %}
+
     SELECT DISTINCT {{ src_cols|join(", ") }},
            LAG({{ src_source }}, 1)
            OVER(PARTITION by {{ src_pk[0] }}
            ORDER BY {{ src_pk[0] }}) AS FIRST_SOURCE
     FROM (
-        {% for src in src_table -%}
+        {%- for src in src_table -%}
+
+        {%- set letter = letters[loop.index0] -%}
+
         SELECT DISTINCT {{ snow_vault.prefix([
         src_pk[loop.index0],
         src_nk,
         src_ldts,
-        src_source], 'a') }}
-        FROM {{ hash_model }} AS a
+        src_source], letter ) }}
+        {% if hash_model is none -%}
+        FROM {{ src[loop.index0] }} AS {{ letter }}
+        {% else -%}
+        FROM {{ hash_model }} AS {{ letter }}
+        {%- endif %}
         {% if is_incremental() -%}
-        LEFT JOIN {{ this }} AS c
-        ON a.{{ src_pk[loop.index0] }} = c.{{ tgt_pk }}
-        AND c.{{ tgt_pk }} IS NULL
+        LEFT JOIN {{ this }} AS tgt_{{ letter }}
+        ON {{ letter }}.{{ src_pk[loop.index0] }} = tgt_{{ letter }}.{{ tgt_pk }}
+        AND tgt_{{ letter }}.{{ tgt_pk }} IS NULL
         {%- endif %}
         {% if not loop.last -%}
         UNION
