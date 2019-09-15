d0b53e6e4087e42e595204c5bb86a20b50574b5b
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-09-15 23:08:40 +0100

    Incremental only code is now added under the correct conditions

--- src/SnowflakeDV/macros/utility/union.sql
+++ src/SnowflakeDV/macros/utility/union.sql
@@ -1,10 +1,12 @@
 {%- macro union(src_pk, src_nk, src_ldts, src_source, tgt_cols, tgt_pk, src_table, hash_model) -%}
 
+    {%- if is_incremental() -%}
     SELECT {{ tgt_cols|join(", ") }},
     LAG({{ src_source }}, 1)
     OVER(PARTITION by {{ tgt_pk }}
     ORDER BY {{ tgt_pk }}) AS FIRST_SOURCE
     FROM (
+    {%- endif -%}
 
  {%- set letters='abcdefghijklmnopqrstuvwxyz' -%}
 
@@ -19,5 +21,5 @@
       {%- if not loop.last %}
       UNION
       {%- endif -%}
-      {%- endfor -%})
+      {%- endfor -%}{%- if is_incremental() -%}){%- endif -%}
 {%- endmacro -%}
