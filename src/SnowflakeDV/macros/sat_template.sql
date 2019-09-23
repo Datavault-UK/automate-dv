0d36cb85578e7d46fe4db1c61f0079a48e4c5504
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-09-23 17:42:24 +0100

    Further improvements/fixes to load cycle tests

--- src/SnowflakeDV/macros/sat_template.sql
+++ src/SnowflakeDV/macros/sat_template.sql
@@ -7,20 +7,15 @@
 SELECT DISTINCT {{ snow_vault.cast([tgt_hashdiff, tgt_pk, tgt_payload, tgt_ldts, tgt_eff, tgt_source]) }}
  FROM (
       SELECT {{ snow_vault.prefix([src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source], 'a') }},
-        LEAD({{ snow_vault.prefix([src_eff], 'a') }}, 1)
+        LEAD({{ snow_vault.prefix([src_ldts], 'a') }}, 1)
         OVER(PARTITION by {{ snow_vault.prefix([tgt_pk|first], 'a') }}
-        ORDER BY {{ snow_vault.prefix([src_eff], 'a') }}) AS LAST_SEEN
+        ORDER BY {{ snow_vault.prefix([src_ldts], 'a') }}) AS LAST_SEEN
         FROM {{ hash_model }} AS a
         {%- if is_incremental() %}
         LEFT JOIN {{ this }} AS tgt_a
         ON a.{{ src_hashdiff }} = tgt_a.{{ tgt_hashdiff|last }}
-        AND tgt_a.{{ tgt_hashdiff|last }} IS NULL
+        WHERE tgt_a.{{ tgt_hashdiff|last }} IS NULL
         {%- endif %}
  ) AS src
-{% if is_incremental() -%}
-WHERE src.{{ tgt_hashdiff|first }} NOT IN (SELECT {{ tgt_hashdiff|last }} FROM {{ this }} AS {{ tgt_hashdiff|first }})
-AND LAST_SEEN IS NULL
-{%- else -%}
 WHERE LAST_SEEN IS NULL
-{%- endif -%}
 {% endmacro %}
