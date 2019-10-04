60be885a4be874fa4c8f413a76735464335b5fa6
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-10-04 11:26:39 +0100

    Tidied ExternalTest models

--- src/ExternalTest/models/hub_sat_link_load/link_inventory_allocation.sql
+++ src/ExternalTest/models/hub_sat_link_load/link_inventory_allocation.sql
@@ -1,24 +1,24 @@
-{{config(materialized='incremental', schema ='VLT', enabled=true, tags=['static', 'incremental'])}}
+{{config(materialized='incremental', schema ='VLT', enabled=true, tags=['static', 'incremental']) }}
 
-{%- set src_pk = ['INVENTORY_ALLOCATION_PK']                                          -%}
-{%- set src_fk = [['PART_PK', 'SUPPLIER_PK', 'LINEITEM_PK']]                          -%}
-{%- set src_source = 'SOURCE'                                                         -%}
-{%- set src_ldts = 'LOADDATE'                                                         -%}
+{%- set src_pk = ['INVENTORY_ALLOCATION_PK']                                                     -%}
+{%- set src_fk = [['PART_PK', 'SUPPLIER_PK', 'LINEITEM_PK']]                                     -%}
+{%- set src_source = 'SOURCE'                                                                    -%}
+{%- set src_ldts = 'LOADDATE'                                                                    -%}
 
 {%- set tgt_cols = ['INVENTORY_ALLOCATION_PK',
                     'PART_PK', 'SUPPLIER_PK', 'LINEITEM_PK',
-                    'LOADDATE', 'SOURCE']                                             -%}
+                    'LOADDATE', 'SOURCE']                                                        -%}
 
-{%- set tgt_pk = ['INVENTORY_ALLOCATION_PK', 'BINARY(16)', 'INVENTORY_ALLOCATION_PK'] -%}
+{%- set tgt_pk = ['INVENTORY_ALLOCATION_PK', 'BINARY(16)', 'INVENTORY_ALLOCATION_PK']            -%}
 {%- set tgt_fk = [['PART_PK', 'BINARY(16)', 'PART_PK'],
                   ['SUPPLIER_PK', 'BINARY(16)', 'SUPPLIER_PK'],
-                  ['LINEITEM_PK', 'BINARY(16)', 'LINEITEM_PK']]                       -%}
+                  ['LINEITEM_PK', 'BINARY(16)', 'LINEITEM_PK']]                                  -%}
 
-{%- set tgt_ldts = ['LOADDATE', 'DATE', 'LOADDATE']                                   -%}
-{%- set tgt_source = ['SOURCE', 'VARCHAR(4)', 'SOURCE']                               -%}
+{%- set tgt_ldts = ['LOADDATE', 'DATE', 'LOADDATE']                                              -%}
+{%- set tgt_source = ['SOURCE', 'VARCHAR(4)', 'SOURCE']                                          -%}
 
-{%- set source = [ref('stg_orders_hashed') ]                                      -%}
+{%- set source = [ref('stg_orders_hashed') ]                                                     -%}
 
 {{ dbtvault.link_template(src_pk, src_fk, src_ldts, src_source,
-                           tgt_cols, tgt_pk, tgt_fk, tgt_ldts, tgt_source,
-                           source) }}
+                          tgt_cols, tgt_pk, tgt_fk, tgt_ldts, tgt_source,
+                          source)                                                                 }}
