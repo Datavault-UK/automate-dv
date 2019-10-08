705e46106ae9de7c433566741ea1e59a17446e7c
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-10-08 16:41:14 +0100

    Added boolean flag to hashing for sorting
    - Adding 'true' as the third component of a hashing pair (to make a triple)
    now sorts the columns. 
    - This is to prevent sorting on primary keys and allow for sorting on hashdiffs when needed.

--- src/dbtvault-dev/models/features/load_cycles/hashing/test_stg_booking_hashed.sql
+++ src/dbtvault-dev/models/features/load_cycles/hashing/test_stg_booking_hashed.sql
@@ -5,8 +5,9 @@
 {{ dbtvault.multi_hash([('BOOKING_REF', 'BOOKING_PK'),
                          ('CUSTOMER_ID', 'CUSTOMER_PK'),
                          (['CUSTOMER_ID', 'BOOKING_REF'],'CUSTOMER_BOOKING_PK'),
-                         (['CUSTOMER_ID', 'NATIONALITY', 'PHONE'], 'BOOK_CUSTOMER_HASHDIFF'),
-                         (['BOOKING_REF', 'BOOKING_DATE', 'DEPARTURE_DATE', 'PRICE', 'DESTINATION'], 'BOOK_BOOKING_HASHDIFF')]) -}},
+                         (['CUSTOMER_ID', 'NATIONALITY', 'PHONE'], 'BOOK_CUSTOMER_HASHDIFF', true),
+                         (['BOOKING_REF', 'BOOKING_DATE', 'DEPARTURE_DATE', 'PRICE', 'DESTINATION'],
+                         'BOOK_BOOKING_HASHDIFF', true)]) -}},
 
 {{ dbtvault.add_columns(source_table,
                         [('!STG_BOOKING', 'SOURCE'),
