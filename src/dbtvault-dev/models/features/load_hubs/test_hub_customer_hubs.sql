8ff6833e80c4100b87c8948a2db1e33828929c77
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-10-18 14:28:27 +0100

    Work on removing hardcoded types
    - Need to make it work for unions. Will force aliasing in staging.
    - Need to make it work for satellites (make some parameters optional and only uses provided)
    - Created an is_union macro to ensure better union checking.
    - Modified get_col_list to work with strings.
    - Create tgt cols gets source cols from the source relation and generates triples if they match provided src metadata.
    - Added check_relation macro to test an object and return if it is a relation or not.

--- src/dbtvault-dev/models/features/load_hubs/test_hub_customer_hubs.sql
+++ src/dbtvault-dev/models/features/load_hubs/test_hub_customer_hubs.sql
@@ -1,16 +1,16 @@
 {{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='feature') -}}
 
+{%- set source = [ref('test_stg_customer_hashed_hubs')]                                 -%}
+
 {%- set src_pk = 'CUSTOMER_PK'                                                          -%}
 {%- set src_nk = 'CUSTOMER_ID'                                                          -%}
 {%- set src_ldts = 'LOADDATE'                                                           -%}
 {%- set src_source = 'SOURCE'                                                           -%}
 
 {%- set tgt_pk = [src_pk, 'BINARY(16)', src_pk]                                         -%}
-{%- set tgt_nk = [src_nk, 'VARCHAR(38)', src_nk]                                        -%}
-{%- set tgt_ldts = [src_ldts, 'DATE', src_ldts]                                         -%}
-{%- set tgt_source = [src_source, 'VARCHAR(15)', src_source]                            -%}
-
-{%- set source = [ref('test_stg_customer_hashed_hubs')]                                 -%}
+{%- set tgt_nk = source                                                                 -%}
+{%- set tgt_ldts = source                                                               -%}
+{%- set tgt_source = source                                                             -%}
 
 {{ dbtvault.hub_template(src_pk, src_nk, src_ldts, src_source,
                          tgt_pk, tgt_nk, tgt_ldts, tgt_source,
--- src/dbtvault-dev/models/features/load_hubs/unions/test_hub_parts.sql
+++ src/dbtvault-dev/models/features/load_hubs/unions/test_hub_parts.sql
@@ -1,18 +1,18 @@
 {{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='feature') -}}
 
-{%- set src_pk = ['PART_PK', 'PART_PK', 'PART_PK']                                      -%}
-{%- set src_nk = ['PART_ID', 'PART_ID', 'PART_ID']                                      -%}
-{%- set src_ldts = 'LOADDATE'                                                           -%}
-{%- set src_source = 'SOURCE'                                                           -%}
-
-{%- set tgt_pk = [src_pk[0], 'BINARY(16)', src_pk[0]]                                   -%}
-{%- set tgt_nk = [src_nk[0], 'NUMBER(38,0)', src_nk[0]]                                 -%}
-{%- set tgt_ldts = [src_ldts, 'DATE', src_ldts]                                         -%}
-{%- set tgt_source = [src_source, 'VARCHAR(15)', src_source]                            -%}
-
 {%- set source = [ref('test_stg_parts_hashed'),
                   ref('test_stg_supplier_hashed'),
                   ref('test_stg_lineitem_hashed')]                                      -%}
+
+{%- set src_pk = 'PART_PK'                                                              -%}
+{%- set src_nk = 'PART_ID'                                                              -%}
+{%- set src_ldts = 'LOADDATE'                                                           -%}
+{%- set src_source = 'SOURCE'                                                           -%}
+
+{%- set tgt_pk = source                                                                 -%}
+{%- set tgt_nk = source                                                                 -%}
+{%- set tgt_ldts = source                                                               -%}
+{%- set tgt_source = source                                                             -%}
 
 {{ dbtvault.hub_template(src_pk, src_nk, src_ldts, src_source,
                          tgt_pk, tgt_nk, tgt_ldts, tgt_source,
