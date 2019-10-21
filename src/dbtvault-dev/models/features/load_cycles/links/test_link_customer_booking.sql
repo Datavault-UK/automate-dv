aaecafafb452e6dac1da6d4cf5892e34bea94b19
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-10-21 13:46:39 +0100

    Removal of hard-coded types re-factor complete.
    - All table templates updated to take advantage of new functionality
    - source variable assignment moved to the top in every model (so it can be used throughout)
    - Lists removed from all src variables, as users are now expected to alias in the hashing/stage layer
    - All tests models re-factored accordingly.
    - All tests still passing.
    - Test feature files renamed from snow_vault to dbtvault (finally)
    - create_tgt_cols is the driver of this change. It converts 'source' references to column/type mappings/triples as in the old format. It also works generically as long as expected columns are provided to the macro as keyword arguments.

--- src/dbtvault-dev/models/features/load_cycles/links/test_link_customer_booking.sql
+++ src/dbtvault-dev/models/features/load_cycles/links/test_link_customer_booking.sql
@@ -1,5 +1,7 @@
 {{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='feature') -}}
 
+{%- set source = [ref('test_stg_booking_hashed')]                                       -%}
+
 {%- set src_pk = 'CUSTOMER_BOOKING_PK'                                                  -%}
 {%- set src_fk = ['CUSTOMER_PK', 'BOOKING_PK']                                          -%}
 {%- set src_ldts = 'LOADDATE'                                                           -%}
@@ -11,8 +13,6 @@
 
 {%- set tgt_ldts = [src_ldts, 'DATE', src_ldts]                                         -%}
 {%- set tgt_source = [src_source, 'VARCHAR(15)', src_source]                            -%}
-
-{%- set source = [ref('test_stg_booking_hashed')]                                       -%}
 
 {{ dbtvault.link_template(src_pk, src_fk, src_ldts, src_source,
                           tgt_pk, tgt_fk, tgt_ldts, tgt_source,
--- src/dbtvault-dev/models/features/load_cycles/sats/test_sat_book_customer_details.sql
+++ src/dbtvault-dev/models/features/load_cycles/sats/test_sat_book_customer_details.sql
@@ -1,29 +1,28 @@
-{{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='feature')             -}}
+{{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='feature') -}}
 
-{%- set src_pk = 'CUSTOMER_PK'                                                                      -%}
-{%- set src_hashdiff = 'BOOK_CUSTOMER_HASHDIFF'                                                     -%}
-{%- set src_payload = ['PHONE', 'NATIONALITY']                                                      -%}
+{%- set source = [ref('test_stg_booking_hashed')]                                       -%}
 
-{%- set src_eff = 'EFFECTIVE_FROM'                                                                  -%}
-{%- set src_ldts = 'LOADDATE'                                                                       -%}
-{%- set src_source = 'SOURCE'                                                                       -%}
+{%- set src_pk = 'CUSTOMER_PK'                                                          -%}
+{%- set src_hashdiff = 'BOOK_CUSTOMER_HASHDIFF'                                         -%}
+{%- set src_payload = ['PHONE', 'NATIONALITY']                                          -%}
 
-{%- set tgt_pk = [ src_pk , 'BINARY(16)', src_pk]                                                   -%}
-{%- set tgt_hashdiff = [ src_hashdiff , 'BINARY(16)', 'HASHDIFF']                                   -%}
-{%- set tgt_payload = [[ src_payload[0], 'VARCHAR(15)', src_payload[0]],
-                       [ src_payload[1], 'VARCHAR(30)', src_payload[1]]]                            -%}
+{%- set src_eff = 'EFFECTIVE_FROM'                                                      -%}
+{%- set src_ldts = 'LOADDATE'                                                           -%}
+{%- set src_source = 'SOURCE'                                                           -%}
 
-{%- set tgt_eff = ['EFFECTIVE_FROM', 'DATE', 'EFFECTIVE_FROM']                                      -%}
-{%- set tgt_ldts = ['LOADDATE', 'DATE', 'LOADDATE']                                                 -%}
-{%- set tgt_source = ['SOURCE', 'VARCHAR(15)', 'SOURCE']                                            -%}
+{%- set tgt_pk = source                                                                 -%}
+{%- set tgt_hashdiff = [ src_hashdiff , 'BINARY(16)', 'HASHDIFF']                       -%}
+{%- set tgt_payload = source                                                            -%}
 
-{%- set source = [ref('test_stg_booking_hashed')]                                                   -%}
+{%- set tgt_eff = source                                                                -%}
+{%- set tgt_ldts = source                                                               -%}
+{%- set tgt_source = source                                                             -%}
 
 {{  dbtvault.sat_template(src_pk, src_hashdiff, src_payload,
                           src_eff, src_ldts, src_source,
                           tgt_pk, tgt_hashdiff, tgt_payload,
                           tgt_eff, tgt_ldts, tgt_source,
-                          source)                                                                    }}
+                          source)                                                        }}
 
 
 
