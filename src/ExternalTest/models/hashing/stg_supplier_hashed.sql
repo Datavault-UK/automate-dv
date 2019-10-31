9ad955d61cae75b093db07322dcac3538530a074
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-10-31 10:49:24 +0000

    Removed ExternalTest
    No longer necessary now that the demo is re-factored

--- src/ExternalTest/models/hashing/stg_supplier_hashed.sql
+++
@@ -1,9 +0,0 @@
-{{- config(materialized='view', schema='STG', tags=['static', 'incremental'], enabled=true) -}}
-
-{% set source_table = source('test', 'supplier')                                            -%}
-
-{{ dbtvault.multi_hash([('PART_ID', 'PART_PK')])                                             }},
-
-{{ dbtvault.add_columns(source_table)                                                        }}
-
-{{ dbtvault.from(source_table)                                                               }}
