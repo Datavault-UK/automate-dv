c46109239efdb0ec256d7f52bf1edca811b13b81
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2019-11-13 23:46:09 +0000

    Full dbUtils integration almost complete
    - Just need to fix the df compare functionality

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,12 +1,31 @@
+import logging
+from unittest.mock import Mock
+
+from vaultBase.cliParse import CLIParse
+from vaultBase.configReader import ConfigReader
+from vaultBase.connector import Connector
+from vaultBase.logger import Logger
+from vaultBase.testing.configGenerator import ConfigGenerator
 from vaultBase.testing.dbTestUtils import DBTestUtils
 
-from definitions import TESTS_ROOT
+from definitions import FEATURES_ROOT
 
 
-# TODO: Fully integrate DBUtils
 def before_scenario(context, scenario):
+    config_path = FEATURES_ROOT / 'config'
+    credentials_path = FEATURES_ROOT / 'config' / 'credentials.json'
+
     context.dbutils = DBTestUtils()
 
+    cli_args = CLIParse("dbtvault Behave Tests", "dbtvault")
+    cli_args.config_name = str(config_path / 'config')
+    cli_args.get_log_level = Mock(return_value=logging.DEBUG)
 
-def after_scenario(context, scenario):
-    context.testdata.close_connection()
+    config_gen = ConfigGenerator()
+    config_gen.from_template("MAIN", file_path=config_path / "config.ini",
+                             amend_dict={'credentials': {'path': credentials_path}})
+
+    logger = Logger("dbtvault-test", cli_args)
+    config = ConfigReader(logger, cli_args)
+
+    context.connection = Connector(logger, config.credentials)
---
+++ tests/features/steps/steps_dbtvault_load_cycles.py
@@ -0,0 +1,149 @@
+import os
+
+from behave import *
+
+from definitions import DBT_ROOT
+
+use_step_matcher("parse")
+
+
+# ============== Create Staging =================
+
+
+@given("there is an empty TEST_STG_CUSTOMER table")
+def step_impl(context):
+    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
+    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_customer",
+                                    ["CUSTOMER_ID VARCHAR(38)", "CUSTOMER_NAME VARCHAR(60)", "CUSTOMER_DOB DATE",
+                                     "LOADDATE DATE", "EFFECTIVE_FROM DATE"], connection=context.connection)
+
+
+@step("there is an empty TEST_STG_BOOKING table")
+def step_impl(context):
+    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
+    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_booking",
+                                    ["BOOKING_REF NUMBER(38,0)", "CUSTOMER_ID VARCHAR(38)", "BOOKING_DATE DATE",
+                                     "PRICE DOUBLE", "DEPARTURE_DATE DATE", "DESTINATION VARCHAR(3)",
+                                     "PHONE VARCHAR(15)", "NATIONALITY VARCHAR(30)", "LOADDATE DATE"],
+                                    connection=context.connection)
+
+
+# ============ Load empty tables ================
+
+
+@step("the vault is empty")
+def step_impl(context):
+    os.chdir(DBT_ROOT)
+
+    os.system("dbt run --full-refresh --models dbtvault.features.load_cycles.*")
+
+
+# ================ Data inserts =================
+
+@when('the TEST_STG_CUSTOMER table has data inserted into it for day {day_number}')
+def step_impl(context, day_number):
+    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
+    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_customer",
+                                    ["CUSTOMER_ID VARCHAR(38)", "CUSTOMER_NAME VARCHAR(60)", "CUSTOMER_DOB DATE",
+                                     "LOADDATE DATE", "EFFECTIVE_FROM DATE"],
+                                    connection=context.connection)
+
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_customer", "DEMO_TEST_STG", connection=context.connection)
+
+
+@step('the TEST_STG_BOOKING table has data inserted into it for day {day_number}')
+def step_impl(context, day_number):
+    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
+    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_booking",
+                                    ["BOOKING_REF NUMBER(38,0)", "CUSTOMER_ID VARCHAR(38)", "BOOKING_DATE DATE",
+                                     "PRICE DOUBLE", "DEPARTURE_DATE DATE", "DESTINATION VARCHAR(3)",
+                                     "PHONE VARCHAR(15)", "NATIONALITY VARCHAR(30)", "LOADDATE DATE"],
+                                    connection=context.connection)
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_booking", "DEMO_TEST_STG", connection=context.connection)
+
+
+@step('the vault is loaded for day {day_number}')
+def step_impl(context, day_number):
+    os.chdir(DBT_ROOT)
+
+    os.system("dbt run --models dbtvault.features.load_cycles.*")
+
+
+# ============== Check loaded data ==============
+
+
+@then("we expect the TEST_HUB_CUSTOMER table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'])
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT.test_hub_customer",
+                                               binary_columns=['CUSTOMER_PK'], ignore_columns=['SOURCE'],
+                                               order_by='CUSTOMER_ID',
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
+
+
+@step("we expect the TEST_HUB_BOOKING table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'])
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT.test_hub_booking",
+                                               binary_columns=['BOOKING_PK'], ignore_columns=['SOURCE'],
+                                               order_by='BOOKING_REF',
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
+
+
+@step("we expect the TEST_LINK_CUSTOMER_BOOKING table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'], order_by='BOOKING_PK')
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT."
+                                                               "test_link_customer_booking",
+                                               binary_columns=['CUSTOMER_BOOKING_PK', 'CUSTOMER_PK', 'BOOKING_PK'],
+                                               ignore_columns=['SOURCE'], order_by='BOOKING_PK',
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
+
+
+@step("we expect the TEST_SAT_CUST_CUSTOMER_DETAILS table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'],
+                                                   order_by=['NAME', 'LOADDATE'])
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT."
+                                                               "test_sat_cust_customer_details",
+                                               binary_columns=['CUSTOMER_PK', 'HASHDIFF'], ignore_columns=['SOURCE'],
+                                               order_by=['NAME', 'LOADDATE'],
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
+
+
+@step("we expect the TEST_SAT_BOOK_CUSTOMER_DETAILS table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'], order_by='CUSTOMER_PK')
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT."
+                                                               "test_sat_book_customer_details",
+                                               binary_columns=['CUSTOMER_PK', 'HASHDIFF'], ignore_columns=['SOURCE'],
+                                               order_by='CUSTOMER_PK',
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
+
+
+@step("we expect the TEST_SAT_BOOK_BOOKING_DETAILS table to contain")
+def step_impl(context):
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'], order_by='BOOKING_PK')
+
+    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT."
+                                                               "test_sat_book_booking_details",
+                                               binary_columns=['BOOKING_PK', 'HASHDIFF'], ignore_columns=['SOURCE'],
+                                               order_by='BOOKING_PK',
+                                               connection=context.connection)
+
+    assert context.dbutils.compare_dataframes(table_df, result_df)
