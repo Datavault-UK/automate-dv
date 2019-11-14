402d3d1507252334bfd2901e738145a340d0c041
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-11-14 12:30:49 +0000

    Partial re-factor of testing environment
    - Giving testing its own Database and dbt installation and moved test models to this new installation, much cleaner!
    - Added a step_vars file to keep a single record of the database and schemas to use for easier modification and cleaner env

--- tests/features/steps/steps_dbtvault_load_hubs.py
+++ tests/features/steps/steps_dbtvault_load_hubs.py
@@ -30,11 +30,11 @@ def step_impl(context):
 
 @step("a {table_name} table does not exist")
 def step_impl(context, table_name):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", context.connection)
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA, context.connection)
 
-    context.dbutils.drop_table("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", table_name, connection=context.connection)
+    context.dbutils.drop_table(DATABASE, VLT_SCHEMA, table_name, connection=context.connection)
 
-    is_exist = context.dbutils.table_exists(db="DV_PROTOTYPE_DB", schema="DEMO_TEST_VLT", table_name=table_name,
+    is_exist = context.dbutils.table_exists(db=DATABASE, schema=VLT_SCHEMA, table_name=table_name,
                                             connection=context.connection)
 
     assert is_exist is False
@@ -45,35 +45,35 @@ def step_impl(context, table_name):
 
 @step("there are records in the TEST_STG_CUSTOMER table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_customer_hubs",
+    context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_customer_hubs",
                                     ["CUSTOMER_ID VARCHAR(38)", "CUSTOMER_NAME VARCHAR(25)", "CUSTOMER_DOB DATE",
                                      "LOADDATE DATE", "SOURCE VARCHAR(4)", ], connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_customer_hubs", "DEMO_TEST_STG", context.connection)
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_customer_hubs", STG_SCHEMA, context.connection)
 
 
 @given("there are records in the TEST_HUB_CUSTOMER table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_hub_customer_hubs",
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_hub_customer_hubs",
                                     ["CUSTOMER_PK BINARY(16)", "CUSTOMER_ID VARCHAR(38)", "LOADDATE DATE",
                                      "SOURCE VARCHAR(4)", ], connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_hub_customer_hubs", "DEMO_TEST_VLT", context.connection)
+    context.dbutils.insert_data_from_ct(context.table, "test_hub_customer_hubs", VLT_SCHEMA, context.connection)
 
 
 @step("there is an empty TEST_HUB_CUSTOMER table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_hub_customer_hubs",
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_hub_customer_hubs",
                                     ["CUSTOMER_PK BINARY(16)", "CUSTOMER_ID VARCHAR(38)", "LOADDATE DATE",
                                      "SOURCE VARCHAR(4)"], context.connection)
 
 
 @step("the TEST_HUB_CUSTOMER table should contain")
 def step_impl(context):
-    table_df = context.dbutils.context_table_to_df(context.table)
+    table_df = context.dbutils.context_table_to_df(context.table, binary_columns=['CUSTOMER_PK'])
 
-    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT.test_hub_customer_hubs",
+    result_df = context.dbutils.get_table_data(full_table_name=DATABASE + VLT_SCHEMA + ".test_hub_customer_hubs",
                                                binary_columns=['CUSTOMER_PK'], order_by='CUSTOMER_ID',
                                                connection=context.connection)
 
@@ -85,57 +85,57 @@ def step_impl(context):
 
 @step("there are records in the TEST_STG_PARTS table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_parts",
+    context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_parts",
                                     ["PART_ID VARCHAR(38)", "PART_NAME VARCHAR(60)", "PART_TYPE VARCHAR(10)",
                                      "PART_SIZE VARCHAR(5)", "PART_RETAILPRICE DOUBLE", "LOADDATE DATE",
                                      "SOURCE VARCHAR(4)"], connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_parts", "DEMO_TEST_STG")
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_parts", STG_SCHEMA)
 
 
 @step("there are records in the TEST_STG_SUPPLIER table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_supplier",
+    context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_supplier",
                                     ["PART_ID VARCHAR(38)", "SUPPLIER_ID VARCHAR(38)", "AVAILQTY INT",
                                      "SUPPLYCOST DOUBLE", "LOADDATE DATE", "SOURCE VARCHAR(4)"],
                                     connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_supplier", "DEMO_TEST_STG")
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_supplier", STG_SCHEMA)
 
 
 @step("there are records in the TEST_STG_LINEITEM table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_lineitem",
+    context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_lineitem",
                                     ["ORDER_ID VARCHAR(38)", "PART_ID VARCHAR(38)", "SUPPLIER_ID VARCHAR(38)",
                                      "LINENUMBER NUMBER(38)", "QUANTITY INT", "EXTENDED_PRICE DOUBLE",
                                      "DISCOUNT DOUBLE", "LOADDATE DATE", "SOURCE VARCHAR(4)"],
                                     connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_lineitem", "DEMO_TEST_STG")
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_lineitem", STG_SCHEMA)
 
 
 @step("there is an empty TEST_HUB_PARTS table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_hub_parts",
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_hub_parts",
                                     ["PART_PK BINARY(16)", "PART_ID VARCHAR(38)", "SOURCE VARCHAR(4)", "LOADDATE DATE"],
                                     connection=context.connection)
 
 
 @given("there are records in the TEST_HUB_PARTS table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", context.connection)
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_hub_parts",
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA, context.connection)
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_hub_parts",
                                     ["PART_PK BINARY(16)", "PART_ID VARCHAR(38)", "SOURCE VARCHAR(4)", "LOADDATE DATE"],
                                     connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_hub_parts", "DEMO_TEST_VLT")
+    context.dbutils.insert_data_from_ct(context.table, "test_hub_parts", VLT_SCHEMA)
 
 
 @step("the TEST_HUB_PARTS table should contain")
 def step_impl(context):
-    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'])
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns=['SOURCE'], binary_columns=['PART_PK'])
 
-    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT.test_hub_parts",
+    result_df = context.dbutils.get_table_data(full_table_name=DATABASE + VLT_SCHEMA + ".test_hub_parts",
                                                binary_columns=['PART_PK'], order_by='PART_ID',
                                                ignore_columns=['SOURCE'])
 
--- tests/features/steps/steps_dbtvault_load_sats.py
+++ tests/features/steps/steps_dbtvault_load_sats.py
@@ -21,42 +21,42 @@ def step_impl(context):
 
 @step('the TEST_STG_CUSTOMER table has data inserted into it')
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_STG")
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_STG", "test_stg_customer_details",
-                                     ["CUSTOMER_ID VARCHAR(38)", "CUSTOMER_NAME VARCHAR(60)", "CUSTOMER_DOB DATE",
-                                      "CUSTOMER_PHONE VARCHAR(15)", "LOADDATE DATE", "SOURCE VARCHAR(4)"],
-                                     materialise="table")
+    context.dbutils.create_schema(DATABASE, STG_SCHEMA)
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_customer_details",
+                                    ["CUSTOMER_ID VARCHAR(38)", "CUSTOMER_NAME VARCHAR(60)", "CUSTOMER_DOB DATE",
+                                     "CUSTOMER_PHONE VARCHAR(15)", "LOADDATE DATE", "SOURCE VARCHAR(4)"],
+                                    connection=context.connection)
 
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_customer_details", "DEMO_TEST_STG")
+    context.dbutils.insert_data_from_ct(context.table, "test_stg_customer_details", STG_SCHEMA)
 
 
 @given("I have an empty TEST_SAT_CUSTOMER_DETAILS table")
 def step_impl(context):
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_sat_customer_details",
-                                     ["HASHDIFF BINARY(16)", "CUSTOMER_PK BINARY(16)", "NAME VARCHAR(60)",
-                                      "PHONE VARCHAR(15)", "DOB DATE", "LOADDATE DATE", "EFFECTIVE_FROM DATE",
-                                      "SOURCE VARCHAR(4)"], materialise="table")
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_sat_customer_details",
+                                    ["HASHDIFF BINARY(16)", "CUSTOMER_PK BINARY(16)", "NAME VARCHAR(60)",
+                                     "PHONE VARCHAR(15)", "DOB DATE", "LOADDATE DATE", "EFFECTIVE_FROM DATE",
+                                     "SOURCE VARCHAR(4)"], connection=context.connection)
 
 
 @given("there are records in the TEST_SAT_CUSTOMER_DETAILS table")
 def step_impl(context):
-    context.dbutils.create_schema("DV_PROTOTYPE_DB", "DEMO_TEST_VLT")
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_sat_customer_details",
-                                     ["HASHDIFF BINARY(16)", "CUSTOMER_PK BINARY(16)", "NAME VARCHAR(60)",
-                                      "PHONE VARCHAR(15)", "DOB DATE", "LOADDATE DATE", "EFFECTIVE_FROM DATE",
-                                      "SOURCE VARCHAR(4)"],
-                                     materialise="table")
+    context.dbutils.create_schema(DATABASE, VLT_SCHEMA)
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_sat_customer_details",
+                                    ["HASHDIFF BINARY(16)", "CUSTOMER_PK BINARY(16)", "NAME VARCHAR(60)",
+                                     "PHONE VARCHAR(15)", "DOB DATE", "LOADDATE DATE", "EFFECTIVE_FROM DATE",
+                                     "SOURCE VARCHAR(4)"], connection=context.connection)
 
-    context.dbutils.insert_data_from_ct(context.table, "test_sat_customer_details", "DEMO_TEST_VLT")
+    context.dbutils.insert_data_from_ct(context.table, "test_sat_customer_details", VLT_SCHEMA)
 
 
 @step("the TEST_SAT_CUSTOMER_DETAILS table should contain")
 def step_impl(context):
-    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns='SOURCE')
+    table_df = context.dbutils.context_table_to_df(context.table, ignore_columns='SOURCE',
+                                                   binary_columns=['CUSTOMER_PK', 'HASHDIFF'])
 
-    result_df = context.dbutils.get_table_data(full_table_name="DV_PROTOTYPE_DB.DEMO_TEST_VLT.test_sat_customer_details",
-                                                binary_columns=['CUSTOMER_PK', 'HASHDIFF'], ignore_columns='SOURCE',
-                                                order_by='NAME')
+    result_df = context.dbutils.get_table_data(
+        full_table_name=DATABASE + VLT_SCHEMA + ".test_sat_customer_details",
+        binary_columns=['CUSTOMER_PK', 'HASHDIFF'], ignore_columns='SOURCE', order_by='NAME')
 
     assert context.dbutils.compare_dataframes(table_df, result_df)
 
@@ -65,11 +65,9 @@ def step_impl(context):
 
 @given("I have an empty TEST_SAT_CUST_CUSTOMER table")
 def step_impl(context):
-    context.dbutils.drop_and_create("DV_PROTOTYPE_DB", "DEMO_TEST_VLT", "test_sat_cust_customer_details",
-                                     ["CUSTOMER_PK BINARY(16)", "HASHDIFF BINARY(16)", "NAME VARCHAR(60)",
-                                      "DOB DATE", "EFFECTIVE_FROM DATE", "LOADDATE DATE",
-                                      "SOURCE VARCHAR(4)"],
-                                     materialise="table")
+    context.dbutils.drop_and_create(DATABASE, VLT_SCHEMA, "test_sat_cust_customer_details",
+                                    ["CUSTOMER_PK BINARY(16)", "HASHDIFF BINARY(16)", "NAME VARCHAR(60)", "DOB DATE",
+                                     "EFFECTIVE_FROM DATE", "LOADDATE DATE", "SOURCE VARCHAR(4)"], connection=context.connection)
 
 
 @step('the TEST_SAT_CUST_CUSTOMER is loaded for day {day_number}')
