459f47992c1d5d5a63f922fccff6e0ebfa068915
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-11-21 11:16:43 +0000

    Renamed tlink to t_link
    - Easier to differentiate between t_link and link

--- src/dbtvault-dev/macros/tables/t_link_template.sql
+++ src/dbtvault-dev/macros/tables/t_link_template.sql
@@ -12,6 +12,6 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 -#}
-{%- macro tlink_template() -%}
+{%- macro t_link_template() -%}
 
 {%- endmacro -%}
