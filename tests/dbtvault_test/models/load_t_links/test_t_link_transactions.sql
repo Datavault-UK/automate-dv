90762c169e0f06440283be91aa6e9cc39bf8b4b4
Alex Higgs <alex.higgs@data-vault.co.uk>
2019-11-21 16:35:30 +0000

    Added t_link feature file and basic template
    - Need to create the view next so that these work

---
+++ tests/dbtvault_test/models/load_t_links/test_t_link_transactions.sql
@@ -0,0 +1,21 @@
+{{- config(materialized='incremental', schema='test_vlt', enabled=true, tags='t_link') -}}
+
+{%- set source = '' -%}
+
+{%- set src_pk = 'TRANSACTION_PK' -%}
+{%- set src_fk = 'ACCOUNT_PK' -%}
+{%- set src_payload = ['TRANSACTION_NUMBER', 'TRANSACTION_DATE', 'TYPE', 'AMOUNT'] -%}
+{%- set src_eff = 'EFFECTIVE_FROM' -%}
+{%- set src_ldts = 'LOADDATE' -%}
+{%- set src_source = 'SOURCE' -%}
+
+{%- set tgt_pk = source -%}
+{%- set tgt_fk = source -%}
+{%- set tgt_payload = source -%}
+{%- set tgt_eff = source -%}
+{%- set tgt_ldts = source -%}
+{%- set tgt_source = source -%}
+
+{{ dbtvault.t_link_template(src_pk, src_fk, src_payload, src_eff, src_ldts, src_source,
+                            tgt_pk, tgt_fk, tgt_payload, tgt_eff, tgt_ldts, tgt_source,
+                            source) }}
