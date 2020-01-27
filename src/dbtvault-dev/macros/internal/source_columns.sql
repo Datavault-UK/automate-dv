10346f22f7c32ce2823c68336a5f8eb1e3627aa2
Christopher Fisher <christopher.fisher@data-vault.co.uk>
2020-01-27 16:42:47 +0000

    Removal of target macros for hubs
    The new hub macro requires only source parameters, deprecating any associated target macros for the hub macro. All these parameters are specified in the dbt_project yaml file. Feature file tests need to be adjust to test new model structure.

--- src/dbtvault-dev/macros/internal/source_columns.sql
+++ src/dbtvault-dev/macros/internal/source_columns.sql
@@ -12,9 +12,17 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 -#}
-{%- macro single(src_pk, src_nk, src_ldts, src_source,
-                 source, letter='a') -%}
-      {{ log('reached', true) }}
-      SELECT {{ dbtvault.prefix([src_pk, src_nk, src_ldts, src_source], letter) }}
-      FROM {{ source }} AS {{ letter }}
+{%- macro source_columns(src_pk, src_nk, src_ldts, src_source,
+                        source, is_union) -%}
+
+    {%- if not is_union -%}
+
+        {{- dbtvault.single(src_pk, src_nk, src_ldts, src_source, source[0], 'a') -}}
+
+    {%- else -%}
+
+        {{- dbtvault.new_union(src_pk, src_nk, src_ldts, src_source, src_pk, source) -}}
+
+    {%- endif -%}
+
 {%- endmacro -%}
