11261e5d3861f2c737c463e375ea0eb8f0a61e85
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-03-28 10:49:36 +0000

    Removed commented deprecated models

--- tests/dbtvault_test/models/current/load_links/test_link_customer_nation_current.sql
+++ tests/dbtvault_test/models/current/load_links/test_link_customer_nation_current.sql
@@ -1,22 +1,4 @@
 {{- config(materialized='incremental', schema='vlt', enabled=true, tags=['load_links', 'current'])     -}}
 
-{#- {%- set source = [ref('test_stg_crm_customer_hashed_links_current')]                                -%}
-
-{%- set src_pk = 'CUSTOMER_NATION_PK'                                                       -%}
-{%- set src_fk = ['CUSTOMER_PK', 'NATION_PK']                                               -%}
-{%- set src_ldts = 'LOADDATE'                                                               -%}
-{%- set src_source = 'SOURCE'                                                               -%}
-
-{%- set tgt_pk = source                                                                     -%}
-{%- set tgt_fk = [['CUSTOMER_PK', 'BINARY(16)', 'CUSTOMER_FK'],
-                  ['NATION_PK', 'BINARY(16)', 'NATION_FK']]                                 -%}
-
-{%- set tgt_ldts = source                                                                   -%}
-{%- set tgt_source = source                                                                 -%}
-
-{{ dbtvault.link_template(src_pk, src_fk, src_ldts, src_source,
-                          tgt_pk, tgt_fk, tgt_ldts, tgt_source,
-                          source)                                                            }} -#}
-
 {{ dbtvault.link(var('src_pk'), var('src_fk'), var('src_ldts'),
                  var('src_source'), var('source')) }}
--- tests/dbtvault_test/models/current/load_links/test_link_customer_nation_union_current.sql
+++ tests/dbtvault_test/models/current/load_links/test_link_customer_nation_union_current.sql
@@ -1,24 +1,4 @@
 {{- config(materialized='incremental', schema='vlt', enabled=true, tags=['load_links', 'current'])     -}}
 
-{#- {%- set source = [ref('test_stg_sap_customer_hashed_links_current'),
-                  ref('test_stg_crm_customer_hashed_links_current'),
-                  ref('test_stg_web_customer_hashed_links_current')]                                -%}
-
-{%- set src_pk = 'CUSTOMER_NATION_PK'                                                       -%}
-{%- set src_fk = ['CUSTOMER_PK', 'NATION_PK']                                               -%}
-{%- set src_ldts = 'LOADDATE'                                                               -%}
-{%- set src_source = 'SOURCE'                                                               -%}
-
-{%- set tgt_pk = [src_pk, 'BINARY(16)', src_pk]                                             -%}
-{%- set tgt_fk = [['CUSTOMER_PK', 'BINARY(16)', 'CUSTOMER_FK'],
-                  ['NATION_PK', 'BINARY(16)', 'NATION_FK']]                                 -%}
-
-{%- set tgt_ldts = [src_ldts, 'DATE', src_ldts]                                             -%}
-{%- set tgt_source = [src_source, 'VARCHAR(15)', src_source]                                -%}
-
-{{ dbtvault.link_template(src_pk, src_fk, src_ldts, src_source,
-                          tgt_pk, tgt_fk, tgt_ldts, tgt_source,
-                          source)                                                            }} -#}
-
 {{ dbtvault.link(var('src_pk'), var('src_fk'), var('src_ldts'),
                  var('src_source'), var('source')) }}
