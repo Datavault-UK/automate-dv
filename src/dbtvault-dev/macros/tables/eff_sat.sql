1b8006e6adc881c213de7d6907e720d80d781461
Chris Fisher <christopher.fisher@data-vault.co.uk>
2020-03-30 11:57:43 +0100

    Improved Eff Sat to exclude NULL SFKs and closed previous entries
    The eff sat now does not add new entries if the secondary foreign key is NULL. If a secondary foreign key is NULL then it closes the most recent eff sat entry with the common driving key. Eff sat feature tests currently all pass. Link macro needs to adjust for this and maybe another scenario written for the link.

--- src/dbtvault-dev/macros/tables/eff_sat.sql
+++ src/dbtvault-dev/macros/tables/eff_sat.sql
@@ -67,6 +67,7 @@ LEFT JOIN (
     ) AS eff
 ON {{ dbtvault.prefix([src_pk], 'eff') }}={{ dbtvault.prefix([src_pk], 'e') }}
 WHERE {{ dbtvault.prefix([src_pk], 'eff') }} IS NULL
+AND {{ dbtvault.prefix([src_sfk], 'e') }} IS NOT NULL
 UNION
 SELECT
   {{ dbtvault.prefix([src_pk], 'y') }},
