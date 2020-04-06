f1ac7d65439fb47e8646aa213dc5d445beafaec2
Chris Fisher <christopher.fisher@data-vault.co.uk>
2020-04-06 16:33:16 +0100

    Eff sat macro refactor.
    The eff sat macro code has been refactored and cleaned up. This has been done by creating a new macro called multikey. This macro takes in the driving keys or secondary keys, any aliases in a list, and the type of for loop need. Based off the input parameters, it writes the required sql. May need a few adjustments to clean up compiled code, however this is not essential.

--- src/dbtvault-dev/macros/tables/eff_sat.sql
+++ src/dbtvault-dev/macros/tables/eff_sat.sql
@@ -33,16 +33,7 @@ c AS (SELECT DISTINCT
 , p AS (
     SELECT q.* FROM {{ ref(link) }} AS q
     INNER JOIN {{ ref(source) }} AS r ON
-    {% for dfk in src_dfk if not src_dfk is string %}
-    {% if loop.index  == src_dfk|length %}
-    {{ dbtvault.prefix([dfk], 'q') }}={{ dbtvault.prefix([dfk], 'r') }}
-    {% else %}
-    {{ dbtvault.prefix([dfk], 'q') }}={{ dbtvault.prefix([dfk], 'r') }}
-    AND
-    {% endif %}
-    {% else %}
-    {{ dbtvault.prefix([src_dfk], 'q') }}={{ dbtvault.prefix([src_dfk], 'r') }}
-    {% endfor %}
+    {{ dbtvault.multikey(src_dfk, ['q', 'r'], 'join') }}
 )
 , x AS (
     SELECT p.*
@@ -53,50 +44,18 @@ c AS (SELECT DISTINCT
     {% endfor %}
     FROM p
     LEFT JOIN {{ ref(source) }} AS s ON
-    {% for dfk in src_dfk if not src_dfk is string %}
-    {% if loop.index  == src_dfk|length %}
-    {{ dbtvault.prefix([dfk], 'p') }}={{ dbtvault.prefix([dfk], 's') }}
-    {% else %}
-    {{ dbtvault.prefix([dfk], 'p') }}={{ dbtvault.prefix([dfk], 's') }}
+    {{ dbtvault.multikey(src_dfk, ['p', 's'], 'join') }}
     AND
-    {% endif %}
-    {% else %}
-    {{ dbtvault.prefix([src_dfk], 'p') }}={{ dbtvault.prefix([src_dfk], 's') }}
-    {% endfor %}
-    AND
-    {% for sfk in src_sfk if not src_sfk is string %}
-    {% if loop.index  == src_sfk|length %}
-    {{ dbtvault.prefix([sfk], 'p') }}={{ dbtvault.prefix([sfk], 's') }}
-    {% else %}
-    {{ dbtvault.prefix([sfk], 'p') }}={{ dbtvault.prefix([sfk], 's') }}
-    AND
-    {% endif %}
-    {% else %}
-    {{ dbtvault.prefix([src_sfk], 'p') }}={{ dbtvault.prefix([src_sfk], 's') }}
-    {% endfor %}
+    {{ dbtvault.multikey(src_sfk, ['p', 's'], 'join') }}
     WHERE (
-    {% for dfk in src_dfk if not src_dfk is string %}
-    {{ dbtvault.prefix([dfk], 's') }} IS NULL
+    {{ dbtvault.multikey(src_dfk, ['s'], 'where null') }}
     AND
-    {% else %}
-    {{ dbtvault.prefix([src_dfk], 's') }} IS NULL AND
-    {% endfor %}
-    {% for sfk in src_sfk if not src_sfk is string %}
-    {% if loop.index  == src_sfk|length %}
-    {{ dbtvault.prefix([sfk], 's') }} IS NULL
-    {% else %}
-    {{ dbtvault.prefix([sfk], 's') }} IS NULL
-    AND
-    {% endif %}
-    {% else %}
-    {{ dbtvault.prefix([src_sfk], 's') }} IS NULL
-    {% endfor %}
+    {{ dbtvault.multikey(src_sfk, ['s'], 'where null') }}
     )
 )
 , y AS (
   SELECT
     {{ dbtvault.prefix([src_pk, src_ldts, src_source, src_eff_from, src_start_date, src_end_date], 't') }}
-
     {% for dfk in src_dfk if not src_dfk is string %}
     , {{ dbtvault.prefix(['DFK_'~loop.index ], 'x') }}
     {% else %}
@@ -125,27 +84,10 @@ LEFT JOIN (
 ON {{ dbtvault.prefix([src_pk], 'eff') }}={{ dbtvault.prefix([src_pk], 'e') }}
 WHERE ({{ dbtvault.prefix([src_pk], 'eff') }} IS NULL
 AND
-{% for sfk in src_sfk if not src_sfk is string %}
-{% if loop.index  == src_sfk|length %}
-{{ dbtvault.prefix([sfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
-{% else %}
-{{ dbtvault.prefix([sfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
+{{ dbtvault.multikey(src_sfk, ['e'], 'where not null') }}
 AND
-{% endif %}
-{% else %}
-{{ dbtvault.prefix([src_sfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
-{% endfor %}
-AND
-{% for dfk in src_dfk if not src_dfk is string %}
-{% if loop.index  == src_dfk|length %}
-{{ dbtvault.prefix([dfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
-{% else %}
-{{ dbtvault.prefix([dfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
-AND
-{% endif %}
-{% else %}
-{{ dbtvault.prefix([src_dfk], 'e') }}<>{{ dbtvault.hash_check(var('hash')) }}
-{% endfor %})
+{{ dbtvault.multikey(src_dfk, ['e'], 'where not null') }}
+)
 UNION
 SELECT
   {{ dbtvault.prefix([src_pk], 'y') }},
@@ -164,17 +106,7 @@ SELECT
   THEN {{ dbtvault.prefix([src_eff_from], 'z') }} ELSE {{ max_date }} END AS {{ src_end_date }}
 FROM y
 LEFT JOIN {{ ref(source) }} AS z ON
-{% for dfk in src_dfk if not src_dfk is string %}
-{% if loop.index  == src_dfk|length %}
-{{ dbtvault.prefix([dfk], 'y') }}={{ dbtvault.prefix([dfk], 'z') }}
-{% else %}
-{{ dbtvault.prefix([dfk], 'y') }}={{ dbtvault.prefix([dfk], 'z') }}
-AND
-{% endif %}
-{% else %}
-{{ dbtvault.prefix([src_dfk], 'y') }}={{ dbtvault.prefix([src_dfk], 'z') }}
-{% endfor %}
-WHERE y.CURR_FLG='Y' AND {{ dbtvault.prefix([src_end_date], 'y') }}={{ max_date }}
+{{ dbtvault.multikey(src_dfk, ['y', 'z'], 'join') }}
+WHERE (y.CURR_FLG='Y' AND {{ dbtvault.prefix([src_end_date], 'y') }}={{ max_date }})
 {%- endif -%}
-
 {% endmacro %}
