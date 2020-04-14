e72c6bd72c0be61ec7ce175a5935508ae92b25cc
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-04-14 11:35:19 +0100

    Add columns re-factored and tests passing

--- tests/unit/staging/test_add_columns.py
+++ tests/unit/staging/test_add_columns.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 from tests.unit.dbt_test_utils import *
 
+
 class TestAddColumnsMacro(TestCase):
 
     @classmethod
@@ -14,27 +15,33 @@ class TestAddColumnsMacro(TestCase):
 
         os.chdir(TESTS_DBT_ROOT)
 
+        cls.dbt_test.run_model(mode='run', model='add_columns_source')
+
     def setUp(self) -> None:
 
         self.dbt_test.clean_target()
 
     # ADD_COLUMNS
 
-    def test_add_columns_with_mapping_correctly_generates_SQL(self):
+    def test_add_columns_correctly_generates_SQL_with_source_columns(self):
 
         model = 'test_add_columns'
 
         var_dict = {
             'source_table': 'add_columns_source',
-            'columns': {"!STG_BOOKING": 'SOURCE', 'EFFECTIVE_FROM': 'LOADDATE'}
+            'columns': {"!STG_BOOKING": 'SOURCE',
+                        'EFFECTIVE_FROM': 'LOADDATE'}
         }
 
         process_logs = self.dbt_test.run_model(model=model, model_vars=var_dict)
 
         actual_sql = self.dbt_test.retrieve_compiled_model(model)
 
-        expected_sql = """'STG_BOOKING' AS SOURCE, EFFECTIVE_FROM AS LOADDATE"""
+        expected_sql = """'STG_BOOKING' AS SOURCE, EFFECTIVE_FROM AS LOADDATE, TEST_COLUMN_2, """ \
+                       """TEST_COLUMN_3, TEST_COLUMN_4, TEST_COLUMN_5, TEST_COLUMN_6, TEST_COLUMN_7, """ \
+                       """TEST_COLUMN_8, TEST_COLUMN_9"""
 
         self.assertIn('Done', process_logs)
 
         self.assertEqual(expected_sql, actual_sql)
+
