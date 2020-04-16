1e94158c3f3e153ac3b3996c63735fad8c41df9c
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-04-16 18:18:09 +0100

    Minor cleanup of eff sat tests

--- tests/features/steps/steps_dbtvault_load_eff_sats.py
+++ tests/features/steps/steps_dbtvault_load_eff_sats.py
@@ -80,25 +80,35 @@ def step_impl(context, table):
 
 @step("staging_data loaded on {date}")
 def step_impl(context, date):
+    table_name = f"test_stg_eff_sat_{MODE.lower()}"
+
     context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
-    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_eff_sat_{}".format(MODE.lower()),
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, table_name,
                                     ["CUSTOMER_ORDER_PK BINARY", "LOADDATE DATE", "SOURCE VARCHAR(6)",
                                      "CUSTOMER_FK BINARY", "CUSTOMER_ID VARCHAR(38)", "ORDER_FK BINARY",
-                                     "ORDER_ID VARCHAR(3)", "EFFECTIVE_FROM DATE"], connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_eff_sat_{}".format(MODE.lower()), STG_SCHEMA,
+                                     "ORDER_ID VARCHAR(3)", "EFFECTIVE_FROM DATE"],
+                                    connection=context.connection)
+    context.dbutils.insert_data_from_ct(context.table,
+                                        table_name,
+                                        STG_SCHEMA,
                                         context.connection)
 
 
 @step("the multipart staging_data loaded on {date}")
 def step_impl(context, date):
+    table_name = f"test_stg_eff_sat_{MODE.lower()}"
+
     context.dbutils.create_schema(DATABASE, STG_SCHEMA, context.connection)
-    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, "test_stg_eff_sat_{}".format(MODE.lower()),
+    context.dbutils.drop_and_create(DATABASE, STG_SCHEMA, table_name,
                                     ["CUSTOMER_ORDER_PK BINARY", "LOADDATE DATE", "SOURCE VARCHAR(6)",
                                      "CUSTOMER_FK BINARY", "CUSTOMER_ID VARCHAR(38)", "ORDER_FK BINARY",
                                      "ORDER_ID VARCHAR(3)", "NATION_FK BINARY", "NATION_ID VARCHAR(3)",
                                      "PRODUCT_FK BINARY", "PRODUCT_GROUP VARCHAR(6)", "ORGANISATION_FK BINARY",
-                                     "ORGANISATION_ID VARCHAR(9)", "EFFECTIVE_FROM DATE"], connection=context.connection)
-    context.dbutils.insert_data_from_ct(context.table, "test_stg_eff_sat_{}".format(MODE.lower()), STG_SCHEMA,
+                                     "ORGANISATION_ID VARCHAR(9)", "EFFECTIVE_FROM DATE"],
+                                    connection=context.connection)
+    context.dbutils.insert_data_from_ct(context.table,
+                                        table_name,
+                                        STG_SCHEMA,
                                         context.connection)
 
 
@@ -106,14 +116,20 @@ def step_impl(context, date):
 def step_impl(context, date):
     os.chdir(TESTS_DBT_ROOT)
 
-    os.system("dbt run --models +test_link_customer_order_current +test_eff_customer_order_current")
+    link = 'test_link_customer_order_current'
+    eff = 'test_eff_customer_order_current'
+
+    os.system(f"dbt run --models +{link} +{eff}")
 
 
 @step("I run a multipart Load Cycle for {date}")
 def step_impl(context, date):
     os.chdir(TESTS_DBT_ROOT)
 
-    os.system("dbt run --models +test_link_customer_order_multipart_current +test_eff_customer_order_multipart_current")
+    link = 'test_link_customer_order_multipart_current'
+    eff = 'test_eff_customer_order_multipart_current'
+
+    os.system(f"dbt run --models +{link} +{eff}")
 
 
 @step("I expect the following {table_name}")
@@ -121,24 +137,28 @@ def step_impl(context, table_name):
 
     if 'link' in table_name.lower():
 
-        table_df = context.dbutils.context_table_to_df(context.table, order_by='CUSTOMER_ORDER_PK',
+        table_df = context.dbutils.context_table_to_df(context.table,
+                                                       order_by='CUSTOMER_ORDER_PK',
                                                        binary_columns=['CUSTOMER_ORDER_PK', "CUSTOMER_FK", "ORDER_FK"])
 
         result_df = context.dbutils.get_table_data(
-            full_table_name=DATABASE + "." + VLT_SCHEMA + ".test_link_customer_order_{}".format(MODE.lower()),
+            full_table_name=f"{DATABASE}.{VLT_SCHEMA}.test_link_customer_order_{MODE.lower()}",
             binary_columns=['CUSTOMER_ORDER_PK', "CUSTOMER_FK", "ORDER_FK"],
-            order_by='CUSTOMER_ORDER_PK', connection=context.connection)
+            order_by='CUSTOMER_ORDER_PK',
+            connection=context.connection)
 
         assert context.dbutils.compare_dataframes(table_df, result_df)
 
     elif 'eff' in table_name.lower():
 
-        table_df = context.dbutils.context_table_to_df(context.table, order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
+        table_df = context.dbutils.context_table_to_df(context.table,
+                                                       order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
                                                        binary_columns=['CUSTOMER_ORDER_PK'])
 
         result_df = context.dbutils.get_table_data(
-            full_table_name=DATABASE + "." + VLT_SCHEMA + ".test_eff_customer_order_{}".format(MODE.lower()),
-            binary_columns=['CUSTOMER_ORDER_PK'], order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
+            full_table_name=f"{DATABASE}.{VLT_SCHEMA}.test_eff_customer_order_{MODE.lower()}",
+            binary_columns=['CUSTOMER_ORDER_PK'],
+            order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
             connection=context.connection)
 
         assert context.dbutils.compare_dataframes(table_df, result_df)
@@ -149,24 +169,28 @@ def step_impl(context, table_name):
 
     if 'link' in table_name.lower():
 
-        table_df = context.dbutils.context_table_to_df(context.table, order_by='CUSTOMER_ORDER_PK',
+        table_df = context.dbutils.context_table_to_df(context.table,
+                                                       order_by='CUSTOMER_ORDER_PK',
                                                        binary_columns=['CUSTOMER_ORDER_PK', "CUSTOMER_FK", "NATION_FK",
                                                                        "ORDER_FK", "PRODUCT_FK", "ORGANISATION_FK"])
         result_df = context.dbutils.get_table_data(
-            full_table_name=DATABASE + "." + VLT_SCHEMA + ".test_link_customer_order_multipart_{}".format(MODE.lower()),
+            full_table_name=f"{DATABASE}.{VLT_SCHEMA}.test_link_customer_order_multipart_{MODE.lower()}",
             binary_columns=['CUSTOMER_ORDER_PK', "CUSTOMER_FK", "NATION_FK", "ORDER_FK", "PRODUCT_FK", "ORGANISATION_FK"],
-            order_by='CUSTOMER_ORDER_PK', connection=context.connection)
+            order_by='CUSTOMER_ORDER_PK',
+            connection=context.connection)
 
         assert context.dbutils.compare_dataframes(table_df, result_df)
 
     elif 'eff' in table_name.lower():
 
-        table_df = context.dbutils.context_table_to_df(context.table, order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
+        table_df = context.dbutils.context_table_to_df(context.table,
+                                                       order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
                                                        binary_columns=['CUSTOMER_ORDER_PK'])
 
         result_df = context.dbutils.get_table_data(
-            full_table_name=DATABASE + "." + VLT_SCHEMA + ".test_eff_customer_order_multipart_{}".format(MODE.lower()),
-            binary_columns=['CUSTOMER_ORDER_PK'], order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
+            full_table_name=f"{DATABASE}.{VLT_SCHEMA}.test_eff_customer_order_multipart_{MODE.lower()}",
+            binary_columns=['CUSTOMER_ORDER_PK'],
+            order_by=['CUSTOMER_ORDER_PK', 'LOADDATE'],
             connection=context.connection)
 
         assert context.dbutils.compare_dataframes(table_df, result_df)
