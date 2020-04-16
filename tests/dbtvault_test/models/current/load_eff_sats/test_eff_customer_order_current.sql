04cb821071ffaf39fa9158e80212f65f7726ae28
Chris Fisher <christopher.fisher@data-vault.co.uk>
2020-04-16 13:11:28 +0100

    Change end date and start date variable names.
    Changed the end date and start date variables from "src_end_date" and "src_start_date" to "eff_end_date" and "eff_start_date".

--- tests/dbtvault_test/models/current/load_eff_sats/test_eff_customer_order_current.sql
+++ tests/dbtvault_test/models/current/load_eff_sats/test_eff_customer_order_current.sql
@@ -1,5 +1,5 @@
 {{- config(materialized='incremental', schema='vlt', enabled=true, tags=['feature', 'current']) -}}
 -- depends_on: {{ ref(var('link')) }}
 {{ dbtvault.eff_sat(var('src_pk'), var('src_dfk'), var('src_sfk'), var('src_ldts'),
-                    var('src_eff_from'), var('src_start_date'), var('src_end_date'),
+                    var('src_eff_from'), var('eff_start_date'), var('eff_end_date'),
                     var('src_source'), var('link'), var('source'))                               }}
