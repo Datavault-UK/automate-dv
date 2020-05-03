7273a6eaff0a83912d2347f3fd8d39beeeada9fa
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-05-03 12:59:11 +0100

    Updated staging macros to used named arguments
    - Makes things less ambiguous
    - Also fixed feature tests (definitions was deleted)
    - Added logic to start exclude feature for hashing
    - Renamed include_source flag to include_source_columns

--- tests/dbtvault_test/models/unit/staging/derive_columns/test_derive_columns_with_source_columns.sql
+++ tests/dbtvault_test/models/unit/staging/derive_columns/test_derive_columns_with_source_columns.sql
@@ -1,8 +1,8 @@
 -- depends_on: {{ ref('raw_source') }}
 {%- if execute -%}
 {%- if var('source_model', '') != '' -%}
-{%- set table_relation = ref(var('source_model')) -%}
+{%- set source_relation = ref(var('source_model')) -%}
 {% endif %}
 {% endif %}
 
-{{ dbtvault.derive_columns(source_model=table_relation, columns=var('columns')) }}
+{{ dbtvault.derive_columns(source_relation=source_relation, columns=var('columns')) }}
