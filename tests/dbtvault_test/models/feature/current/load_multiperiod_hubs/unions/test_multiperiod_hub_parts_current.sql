6e713d8b947da8560da53aa5d12fbad82b1a883c
Chris Fisher <christopher.fisher@data-vault.co.uk>
2020-05-04 16:14:38 +0100

    New hub sql replaces old hub sql.
    The multiperiod_hub macro sql is now the sql for the hub macro. Multiperiod_hub macro has been deleted and test models adjust to use the new hub macro.

--- tests/dbtvault_test/models/feature/current/load_multiperiod_hubs/unions/test_multiperiod_hub_parts_current.sql
+++ tests/dbtvault_test/models/feature/current/load_multiperiod_hubs/unions/test_multiperiod_hub_parts_current.sql
@@ -1,4 +1,4 @@
 {{- config(materialized='incremental', schema='vlt', enabled=true, tags=['feature', 'current']) -}}
 
-{{- dbtvault.multiperiod_hub(var('src_pk'), var('src_nk'), var('src_ldts'),
+{{- dbtvault.hub(var('src_pk'), var('src_nk'), var('src_ldts'),
                              var('src_source'), var('source'))                                  -}}
