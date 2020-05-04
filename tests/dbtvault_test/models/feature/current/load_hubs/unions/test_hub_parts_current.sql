9201c23fdc2afd2c616160faa95dbaa0ee079cb1
Chris Fisher <christopher.fisher@data-vault.co.uk>
2020-05-04 17:15:20 +0100

    Tidied up hub feature file tests and models
    There are no longer separate hub models for the standard hub feature files and the multiperiod hub feature files. They now both use the same models and in the load cycles. Removed the multiperiod hub feature file models.

--- tests/dbtvault_test/models/feature/current/load_hubs/unions/test_hub_parts_current.sql
+++ tests/dbtvault_test/models/feature/current/load_hubs/unions/test_hub_parts_current.sql
@@ -1,5 +1,4 @@
 {{- config(materialized='incremental', schema='vlt', enabled=true, tags=['feature', 'current']) -}}
 
-{{ dbtvault.hub(var('src_pk'), var('src_nk'), var('src_ldts'),
-                var('src_source'), var('source'))                                        }}
-
+{{- dbtvault.hub(var('src_pk'), var('src_nk'), var('src_ldts'),
+                             var('src_source'), var('source'))                                  -}}
