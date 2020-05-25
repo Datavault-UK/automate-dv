b75ede6127e85dba95f095c63e05d9c544139ac3
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-05-25 14:56:31 +0100

    source_models -> source_model

--- tests/dbtvault_test/models/feature/current/load_links/hashing/test_stg_crm_customer_order_hashed_current.sql
+++ tests/dbtvault_test/models/feature/current/load_links/hashing/test_stg_crm_customer_order_hashed_current.sql
@@ -1,4 +1,4 @@
 {{ dbtvault.stage(include_source_columns=var('include_source_columns', none),
-                  source_models=var('source_models', none),
+                  source_model=var('source_model', none),
                   hashed_columns=var('hashed_columns', none),
                   derived_columns=var('derived_columns', none)) }}
