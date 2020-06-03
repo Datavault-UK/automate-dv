23bdbfbcd1b4885bc35a10b2c2ae2fba597041ec
Alex Higgs <alexander.higgs.2015@live.rhul.ac.uk>
2020-06-04 00:39:12 +0100

    Start of BDD refactor

---
+++ tests/features/environment.py
@@ -0,0 +1,9 @@
+from tests.dbt_test_utils import *
+
+
+def before_all(context):
+    """
+    Set up the full test environment and add objects to the context for use in steps
+    """
+
+    context.dbt_test_utils = DBTTestUtils()
