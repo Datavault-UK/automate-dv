9b77925287d056059864d371b6b0f8f341784a3b
Alex Higgs <alex.higgs@data-vault.co.uk>
2020-07-07 15:58:51 +0100

    All scenarios passing

--- tests/features/steps/shared_steps.py
+++ tests/features/steps/shared_steps.py
@@ -151,6 +151,9 @@ def create_csv(context, raw_stage_model_name):
     seed_file_name = context.dbt_test_utils.context_table_to_csv(table=context.table,
                                                                  model_name=raw_stage_model_name)
 
+    dbtvault_generator.add_seed_config(seed_name=seed_file_name,
+                                       seed_config=context.seed_config[raw_stage_model_name])
+
     logs = context.dbt_test_utils.run_dbt_seed(seed_file_name=seed_file_name)
 
     context.raw_stage_models = seed_file_name
@@ -201,7 +204,11 @@ def stage(context):
     logs = context.dbt_test_utils.run_dbt_model(mode='run', model_name=hashed_model_name, args=stage_args)
 
     if hasattr(context, 'hashed_stage_model_name'):
-        context.hashed_stage_model_name = [context.hashed_stage_model_name] + [hashed_model_name]
+
+        context.hashed_stage_model_name = context.dbt_test_utils.process_hashed_stage_names(
+            context.hashed_stage_model_name,
+            hashed_model_name)
+
     else:
         context.hashed_stage_model_name = hashed_model_name
 
