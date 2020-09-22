c31a750bca210b5e05782c5a010ad15f01e75873
Alex Higgs <alex.higgs@data-vault.co.uk>
2020-09-22 08:43:04 +0000

    Removed licence from macros

--- src/dbtvault-dev/macros/tables/hub.sql
+++ src/dbtvault-dev/macros/tables/hub.sql
@@ -1,15 +1,3 @@
-{#- Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
--#}
 {%- macro hub(src_pk, src_nk, src_ldts, src_source, source_model) -%}
 
     {{- adapter.dispatch('hub', packages = ['dbtvault'])(src_pk=src_pk, src_nk=src_nk,
