8e4e9bcfc039808fc9d66c7a7a89a03e5244bf27
Alex Higgs <alex.higgs@data-vault.co.uk>
2020-09-23 19:24:53 +0000

    Fixed tests

--- test_project/test_utils/dbt_test_utils.py
+++ test_project/test_utils/dbt_test_utils.py
@@ -15,8 +15,8 @@ from pandas import Series
 
 PROJECT_ROOT = PurePath(__file__).parents[2]
 PROFILE_DIR = Path(f"{PROJECT_ROOT}/profiles")
-TESTS_ROOT = Path(f"{PROJECT_ROOT}/tests")
-TESTS_DBT_ROOT = Path(f"{PROJECT_ROOT}/tests/dbtvault_test")
+TESTS_ROOT = Path(f"{PROJECT_ROOT}/test_project")
+TESTS_DBT_ROOT = Path(f"{TESTS_ROOT}/dbtvault_test")
 MODELS_ROOT = TESTS_DBT_ROOT / 'models'
 SCHEMA_YML_FILE = MODELS_ROOT / 'schema.yml'
 TEST_SCHEMA_YML_FILE = MODELS_ROOT / 'schema_test.yml'
@@ -24,7 +24,7 @@ DBT_PROJECT_YML_FILE = TESTS_DBT_ROOT / 'dbt_project.yml'
 BACKUP_TEST_SCHEMA_YML_FILE = TESTS_ROOT / 'backup_files/schema_test.bak.yml'
 BACKUP_DBT_PROJECT_YML_FILE = TESTS_ROOT / 'backup_files/dbt_project.bak.yml'
 FEATURE_MODELS_ROOT = MODELS_ROOT / 'feature'
-COMPILED_TESTS_DBT_ROOT = Path(f"{TESTS_ROOT}/dbtvault_test/target/compiled/dbtvault_test/models/unit")
+COMPILED_TESTS_DBT_ROOT = Path(f"{TESTS_DBT_ROOT}/target/compiled/dbtvault_test/models/unit")
 EXPECTED_OUTPUT_FILE_ROOT = Path(f"{TESTS_ROOT}/unit/expected_model_output")
 FEATURES_ROOT = TESTS_ROOT / 'features'
 CSV_DIR = TESTS_DBT_ROOT / 'data/temp'
