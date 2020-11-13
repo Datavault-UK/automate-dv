29d0c210668d5704a20cebbf5b4789db4e0eccfd
Flynn Sheridan <flynn.sheridan@business-thinking.com>
2020-11-13 11:48:23 +0000

    Finished altering the expected .sql for stage
    All expected .sql files for stage now expect sql in the format of cte's

--- test_project/unit/expected_model_output/staging/stage/test_stage_correctly_generates_sql_for_only_derived_from_yaml.sql
+++ test_project/unit/expected_model_output/staging/stage/test_stage_correctly_generates_sql_for_only_derived_from_yaml.sql
@@ -1,6 +1,19 @@
-SELECT
+WITH stage AS (
+    SELECT *
+    FROM [DATABASE_NAME].[SCHEMA_NAME].raw_source_table
+),
 
-'STG_BOOKING' AS SOURCE,
-LOAD_DATETIME AS EFFECTIVE_FROM
 
-FROM [DATABASE_NAME].[SCHEMA_NAME].raw_source
+derived_columns AS (
+    SElECT
+    'STG_BOOKING' AS SOURCE,
+    LOADDATE AS EFFECTIVE_FROM
+    FROM stage
+),
+
+hashed_columns AS (
+    SELECT *
+    FROM derived_stage
+)
+
+SELECT * FROM hashed_columns
