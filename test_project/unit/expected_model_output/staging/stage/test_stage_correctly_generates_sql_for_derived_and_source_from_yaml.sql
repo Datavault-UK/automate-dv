ddf25e64d8c6e70e82d8e7be434730baba5872bd
Flynn Sheridan <flynn.sheridan@business-thinking.com>
2020-11-18 15:46:16 +0000

    Created two more unit tests for stage and refactored stage macro

---
+++ test_project/unit/expected_model_output/staging/stage/test_stage_correctly_generates_sql_for_derived_and_source_from_yaml.sql
@@ -0,0 +1,59 @@
+WITH stage AS (
+    SELECT
+
+    BOOKING_FK,
+    ORDER_FK,
+    CUSTOMER_PK,
+    CUSTOMER_ID,
+    LOADDATE,
+    RECORD_SOURCE,
+    CUSTOMER_DOB,
+    CUSTOMER_NAME,
+    NATIONALITY,
+    PHONE,
+    TEST_COLUMN_2,
+    TEST_COLUMN_3,
+    TEST_COLUMN_4,
+    TEST_COLUMN_5,
+    TEST_COLUMN_6,
+    TEST_COLUMN_7,
+    TEST_COLUMN_8,
+    TEST_COLUMN_9
+
+    FROM [DATABASE_NAME].[SCHEMA_NAME].raw_source
+),
+
+derived_columns AS (
+    SElECT
+
+    'STG_BOOKING' AS SOURCE,
+    BOOKING_DATE AS EFFECTIVE_FROM,
+    BOOKING_FK,
+    ORDER_FK,
+    CUSTOMER_PK,
+    CUSTOMER_ID,
+    LOADDATE,
+    RECORD_SOURCE,
+    CUSTOMER_DOB,
+    CUSTOMER_NAME,
+    NATIONALITY,
+    PHONE,
+    TEST_COLUMN_2,
+    TEST_COLUMN_3,
+    TEST_COLUMN_4,
+    TEST_COLUMN_5,
+    TEST_COLUMN_6,
+    TEST_COLUMN_7,
+    TEST_COLUMN_8,
+    TEST_COLUMN_9
+
+    FROM stage
+),
+
+hashed_columns AS (
+    SELECT *
+
+    FROM derived_columns
+)
+
+SELECT * FROM hashed_columns
