ae4b45273093bffade02a1d8d16750f6e8c358be
Flynn Sheridan <flynn.sheridan@business-thinking.com>
2020-11-19 11:35:55 +0000

    WIP: Staging features, fixtures and steps

--- test_project/features/fixtures.py
+++ test_project/features/fixtures.py
@@ -40,6 +40,40 @@ def sha(context):
 
 
 @fixture
+def staging(context):
+    """
+    Define the structures and metadata to load a hashed staging layer
+    """
+
+    context.seed_config = {
+
+        'STG_CUSTOMER': {
+            'column_types': {
+                'CUSTOMER_ID': 'VARCHAR',
+                'CUSTOMER_NAME': 'VARCHAR',
+                'CUSTOMER_DOB': 'VARCHAR',
+                'CUSTOMER_PHONE': 'VARCHAR',
+                'LOAD_DATE': 'DATE',
+                'SOURCE': 'VARCHAR',
+                'CUSTOMER_PK': 'BINARY(16)',
+                'HASHDIFF': 'BINARY(16)',
+                'EFFECTIVE_FROM': 'DATE'
+            }
+        },
+        'RAW_STAGE': {
+            'column_types': {
+                'CUSTOMER_ID': 'VARCHAR',
+                'CUSTOMER_NAME': 'VARCHAR',
+                'CUSTOMER_DOB': 'VARCHAR',
+                'CUSTOMER_PHONE': 'VARCHAR',
+                'LOAD_DATE': 'DATE',
+                'SOURCE': 'VARCHAR'
+            }
+        }
+    }
+
+
+@fixture
 def single_source_hub(context):
     """
     Define the structures and metadata to load single-source hubs
