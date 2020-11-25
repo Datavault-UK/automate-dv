423f3dfbc26f0403dcc220690b9cf4749ea320ad
Flynn <Flynn.sheridan@business-thinking.com>
2020-11-25 18:33:14 +0000

    Removed repeated logic in the helper stage macros

--- dbtvault/macros/staging/source_columns.sql
+++ dbtvault/macros/staging/source_columns.sql
@@ -1,11 +1,5 @@
 {%- macro source_columns(source_relation=none) -%}
 
-    {{- adapter.dispatch('source_columns', packages = ['dbtvault'])(source_relation=source_relation) -}}
-
-{%- endmacro %}
-
-{%- macro snowflake__source_columns(source_relation=none, columns=none) -%}
-
 
 {%- set include_columns = [] -%}
 
@@ -14,20 +8,13 @@
 {%- endif %}
 
 
-
  {#- Add all columns from source_model relation -#}
 {%- for source_col in source_model_cols -%}
         {%- set _ = include_columns.append(source_col.column) -%}
 {%- endfor -%}
 
-    {#- Print out all columns in includes -#}
-{%- for col in include_columns -%}
-    {{ col }}
-    {{- ',\n' if not loop.last -}}
 
-{%- endfor -%}
-
-
+{%- do return(include_columns) -%}
 
 
 {%- endmacro -%}
