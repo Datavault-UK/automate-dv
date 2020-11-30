8609d5c8c77f3560274719f941a38f8b43265921
Flynn <Flynn.sheridan@business-thinking.com>
2020-11-30 17:17:19 +0000

    WIP: Exclude flag in hashdiff during staging
    refactored process_exclude macro and started taking logic out and putting it into its own macro in the same file. Added a unit test for exclude flag for hashdiff.

--- dbtvault/macros/internal/process_exclude.sql
+++ dbtvault/macros/internal/process_exclude.sql
@@ -1,4 +1,4 @@
-{%- macro process_exclude(source_relation=source_releation, derived_columns=none ,columns=none) -%}
+{%- macro process_exclude(source_relation=none, derived_columns=none ,columns=none) -%}
 
 {%- set exclude_columns = [] -%}
 {%- set include_columns = [] -%}
@@ -8,40 +8,48 @@
 
 {#- getting all the source collumns and derived columns -#}
 
-{% set source_columns =(dbtvault.source_columns(source_relation=source_relation)) %}
+{%- set source_columns = dbtvault.source_columns(source_relation=source_relation) -%}
 
 
 {%- if columns is mapping -%}
 
     {%- for col in columns -%}
 
+
         {% if columns[col] is mapping and columns[col].exclude_flag -%}
 
-            {%- set exclude_columns = columns[col]['columns'] -%}
+
+
+            {%- for flagged_cols in columns[col]['columns'] -%}
+
+                {%- set _ = exclude_columns.append(flagged_cols) -%}
+
+            {%- endfor -%}
 
             {%- if derived_columns is not none -%}
-                {%- for col in derived_columns_columns -%}
-                    {%- if col not in exclude_columns -%}
-                         {%- set _ = include_columns.append(col) -%}
+                {%- for derived_col in derived_columns -%}
+                    {%- if derived_col not in exclude_columns -%}
+                        {% set column_str = dbtvault.as_constant(derived_columns[derived_col]) %}
+                        {%- set _ = include_columns.append(column_str ~ " AS " ~ derived_col) -%}
+                        {%- set _ = exclude_columns.append(derived_col)  -%}
                      {%- endif -%}
                 {%- endfor -%}
 
-                {%- set _ = exclude_columns.apend(derived_columns)  -%}
 
             {%- endif -%}
 
 
-            {%- for col in source_columns -%}
-                {%- if col not in exclude_columns -%}
-                     {%- set _ = include_columns.append(col) -%}
+            {%- for source_col in source_columns -%}
+                {%- if source_col not in exclude_columns -%}
+                     {%- set _ = include_columns.append(source_col) -%}
                  {%- endif -%}
             {%- endfor -%}
-            {{ log('before'~included_columns, true) }}
+
 
             {%- do columns[col].update({'columns': include_columns}) -%}
-              {{ log('after',included_columns) }}
             {%- set include_columns = [] -%}
             {%- set exclude_columns = [] -%}
+
 
         {%- endif -%}
     {%- endfor -%}
@@ -50,5 +58,37 @@
 
 {%- do return(columns) -%}
 
+
+{%- endmacro -%}
+
+
+
+{%- macro process_source_and_derived(primary_set_list=none, secondary_set_list=none ,exclude_columns=none) -%}
+
+{%- set include_columns = [] -%}
+
+{%- if exclude_columns is none -%}
+    {%- set exclude_columns = [] -%}
+{%- endif -%}
+
+
+{%- if primary_set_list is not none -%}
+    {%- for primary_col in primary_set_list -%}
+        {%- if primary_col not in exclude_columns -%}
+            {% set column_str = dbtvault.as_constant(derived_columns[derived_col]) %}
+            {%- set _ = include_columns.append(column_str ~ " AS " ~ derived_col) -%}
+            {%- set _ = exclude_columns.append(derived_col)  -%}
+        {%- endif -%}
+    {%- endfor -%}
+{%- endif -%}
+
+
+{%- for source_col in source_columns -%}
+    {%- if source_col not in exclude_columns -%}
+        {%- set _ = include_columns.append(source_col) -%}
+    {%- endif -%}
+{%- endfor -%}
+
+{%- do return(columns) -%}
 
 {%- endmacro -%}
