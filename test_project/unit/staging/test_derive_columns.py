76cf8aff9d36e3169d14402d9a541a7047420f62
Alex Higgs <alex.higgs@data-vault.co.uk>
2020-12-09 16:06:33 +0000

    Fixed failing derived columns test

--- test_project/unit/staging/test_derive_columns.py
+++ test_project/unit/staging/test_derive_columns.py
@@ -26,13 +26,12 @@ class TestDeriveColumnsMacro(TestCase):
         assert 'Done' in process_logs
         self.assertEqual(expected_sql, actual_sql)
 
-    def test_derive_columns_correctly_generates_sql_with_only_source_columns(self):
+    def test_derive_columns_raises_error_with_only_source_columns(self):
         var_dict = {'source_model': 'raw_source'}
 
         process_logs = self.dbt_test_utils.run_dbt_model(model_name=self.current_test_name,
                                                          args=var_dict)
-        actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-        expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
 
-        assert 'Done' in process_logs
-        self.assertEqual(expected_sql, actual_sql)
+        warning_message = "Invalid column configuration:"
+
+        assert warning_message in process_logs
