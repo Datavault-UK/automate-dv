a9eea1784b24dbe6896a7a2598c023dddc7e97b3
Frankie Homewood <frankie.homewood@business-thinking.com>
2020-12-17 10:54:14 +0000

    Expanded adapter_packages option to all adapter.dispatch calls

--- macros/supporting/prefix.sql
+++ macros/supporting/prefix.sql
@@ -1,6 +1,6 @@
 {%- macro prefix(columns, prefix_str, alias_target) -%}
 
-    {{- adapter.dispatch('prefix', packages = ['dbtvault'])(columns=columns, prefix_str=prefix_str, alias_target=alias_target) -}}
+    {{- adapter.dispatch('prefix', packages = var('adapter_packages', ['dbtvault']))(columns=columns, prefix_str=prefix_str, alias_target=alias_target) -}}
 
 {%- endmacro -%}
 
