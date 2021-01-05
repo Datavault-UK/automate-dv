9f85b30fdc7571e5e570bc573ab9529665cdbc2a
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-05 17:48:04 +0000

    Draft of the Pit Macro
    First version of the PIT table macro. feature tests Unfinished

--- macros/tables/PIT.sql
+++ macros/tables/PIT.sql
@@ -1,7 +1,42 @@
-{%- macro PIT(src_pk,src_ldts,src_payload) -%}
+{%- macro PIT(src_pk,satellite,as_of_dates_table,source_model) -%}
 
-    {{- adapter.dispatch('PIT', packages = var('adapter_packages', ['dbtvault']))(src_pk=src_pk, src_ldts=src_ldts,
-                                                                                  src_payload=src_payload,
+    {{- adapter.dispatch('PIT', packages = var('adapter_packages', ['dbtvault']))(src_pk=src_pk, satellite=satellite,
+                                                                                  as_of_dates_table=as_of_dates_table,
                                                                                   source_model=source_model) -}}
 
 {%- endmacro -%}
+
+{%- macro PIT(src_pk,satellite,as_of_dates_table,source_model) -%}
+
+{% set maxdate = '9999-12-31 23:59:59.999999' %}
+{% set ghost_pk = cast_binary('0000000000000000') %}
+{% set ghost_date = '0000-00-00 00:00:00.000000' %}
+
+
+Insert into {{ src_pk_,"PIT" }}(
+    SELECT
+        {{ as_of_dates_table -}}.as_of_date,
+        {{ src_pk }},
+        {% for sats in satellites -%}
+            coalesce(max({{- sat.pk -}}),ghost_pk) AS {{ sat -}}_PK,
+            coalesce(max({{- sat.LDTS -}}),ghost_date) {{ LDTS -}}_LDTS
+            {{- ',' if not loop.last -}}
+        {%- endfor %}
+
+    From {{ source_model }} AS h
+
+    INNER JOIN {{ as_of_dates_table }} AS x
+        ON (1=1)
+
+
+    {% for sat in satelites -%}
+        LEFT JOIN {{ sat }}
+            ON {{ h.src_pk }} = {{ sat -}}.{{ sat.pk }},
+        WHERE {{ sat -}}.LDTS <= x.as_of_date
+    {% endfor %}
+
+    GROUP BY
+    x.as_of_date, h.{{- src_pk }},
+    ORDER BY 1,2;
+
+
