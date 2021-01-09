b469e73b16d724cb21f75ae69ccbf8a60ef43ff6
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-09 19:27:44 +0000

    Fixed XTS model generation

--- macros/tables/xts.sql
+++ macros/tables/xts.sql
@@ -1,8 +1,10 @@
-{%- macro xts(src_pk, src_ldts, src_satellite, src_source, source_model) -%}
+{%- macro xts(src_pk, src_satellite, src_ldts, src_source, source_model) -%}
 
-    {{- adapter.dispatch('xts', packages = var('adapter_packages', ['dbtvault']))
-                                                ( src_pk=src_pk, src_ldts=src_ldts, src_satellite= src_satellite,
-                                                  src_source=src_source, source_model=source_model               ) -}}
+    {{- adapter.dispatch('xts', packages = var('adapter_packages', ['dbtvault']))(src_pk=src_pk,
+                                                                                  src_satellite=src_satellite,
+                                                                                  src_ldts=src_ldts,
+                                                                                  src_source=src_source,
+                                                                                  source_model=source_model) -}}
 
 {%- endmacro %}
 
