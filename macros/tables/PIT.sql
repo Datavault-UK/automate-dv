0e4790072f0f2fd03ab9f726228cf82904eb9753
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-09 17:25:55 +0000

    WIP: Additions to feature tests and macro for PIT

--- macros/tables/PIT.sql
+++ macros/tables/PIT.sql
@@ -1,43 +1,54 @@
-{%- macro PIT(src_pk,source_model,as_of_dates_table,satellite) -%}
+{%- macro pit(src_pk,source_model,as_of_dates_table,satellite) -%}
 
-    {{- adapter.dispatch('PIT', packages = var('adapter_packages', ['dbtvault']))(source_model=source_model,src_pk=src_pk,
+    {{- adapter.dispatch('pit', packages = var('adapter_packages', ['dbtvault']))(source_model=source_model,src_pk=src_pk,
                                                                                    as_of_dates_table=as_of_dates_table,
                                                                                    satellite=satellite) -}}
 
 {%- endmacro -%}
 
-{%- macro default__PIT(src_pk,source_model,as_of_dates_table,satellite) -%}
+{%- macro default__pit(src_pk,source_model,as_of_dates_table,satellite) -%}
 
+{# Set deafualts and obtain source model paths #}
 {% set maxdate = '9999-12-31 23:59:59.999999' %}
-{% set ghost_pk = cast_binary('0000000000000000') %}
+{% set ghost_pk =   ('0000000000000000') %}
 {% set ghost_date = '0000-00-00 00:00:00.000000' %}
 
+    {#  Loop to get the source relatiosn using source relation macro and can specify refs i think
+    Loop throught the dict and call the 1st key or have a source model key pair in the sub dict
+    Not in loop i can get the hub source relation #}
 
-Insert into PIT(
-    SELECT
-        {{ as_of_dates_table -}}.as_of_date,
-        {{ src_pk }},
-        {% for sats in satellites -%}
-            coalesce(max({{- sat.pk -}}),ghost_pk) AS {{ sat -}}_PK,
-            coalesce(max({{- sat.LDTS -}}),ghost_date) AS {{ LDTS -}}_LDTS
-            {{- ',' if not loop.last -}}
-        {%- endfor %}
-
-    From {{ source_model }} AS h
-
-    INNER JOIN {{ as_of_dates_table }} AS x
-        ON (1=1)
+{%- set source_relation = ref(source_model) %}
+{%- set as_of_dates_table = ref(as_of_dates_table) -%}
+{% for sat in satelites -%}
+    {%- set sat_src= dbtvault.prefix(columns=sat,prefix='_src') -%}
+    {%- set sat_src = ref(sat) %}
+{% endfor %}
 
 
-    {% for sat in satelites -%}
-        LEFT JOIN {{ sat }}
-            ON {{ h.src_pk }} = {{ sat -}}.{{ sat.pk }},
-        WHERE {{ sat -}}.LDTS <= x.as_of_date
-    {% endfor %}
+SELECT
+    {{ as_of_dates_table -}}.as_of_date,
+    {{ src_pk }},
+    {% for sats in satellites -%}
+        coalesce(max({{- sat ~'_src' -}}.{{- sat[pk] -}}),Cast(ghost_pk AS BINARY) AS {{ sat -}}_PK,
+        coalesce(max({{- sat ~'_src' -}}.{{- sat[LDTS] -}}),ghost_date) AS {{ sat -}}_LDTS
+        {{- ',' if not loop.last -}}
+    {%- endfor %}
 
-    GROUP BY
-    x.as_of_date, h.{{- src_pk }},
-    ORDER BY 1,2;
+From {{ source_relation }} AS h
+
+INNER JOIN {{ as_of_dates_table }} AS x
+    ON (1=1)
+
+
+{% for sat in satelites -%}
+    LEFT JOIN {{- sat ~'_src' -}}
+        ON {{ h.src_pk }} = {{ sat -}}.{{ sat.pk }},
+    WHERE {{ sat -}}.LDTS <= x.as_of_date
+{% endfor %}
+
+GROUP BY
+x.as_of_date, h.{{- src_pk }},
+ORDER BY 1,2;
 
 
 {%- endmacro -%}
