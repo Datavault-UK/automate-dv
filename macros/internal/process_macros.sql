4250714b0a8cbe0f0ba298a28dd6ea04e5b938ce
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-11 21:53:31 +0000

    WIP: stage fixes
    - Working on calculating exclude columns

--- macros/internal/process_macros.sql
+++ macros/internal/process_macros.sql
@@ -91,3 +91,36 @@
 {%- do return(include_columns) -%}
 
 {%- endmacro -%}
+
+
+
+
+{%- macro process_columns_to_select(columns_list=none, exclude_columns_list=none) -%}
+
+    {%- do log("columns_list: " ~ columns_list, true) -%}
+    {%- do log("exclude_columns_list: " ~ exclude_columns_list, true) -%}
+
+    {% set columns_to_select = [] %}
+
+    {% if not dbtvault.is_list(columns_list) or not dbtvault.is_list(exclude_columns_list)  %}
+
+        {{- exceptions.raise_compiler_error("One or both arguments are not of list type.") -}}
+
+    {%- endif -%}
+
+    {%- if columns_list is not none and columns_list and exclude_columns_list is not none and exclude_columns_list -%}
+
+        {%- for col in columns_list -%}
+
+            {%- if col not in exclude_columns_list -%}
+                {%- do columns_to_select.append(col) -%}
+            {%- endif -%}
+
+        {%- endfor -%}
+
+    {%- endif -%}
+
+    {%- do log("columns_to_select: " ~ columns_to_select, true) -%}
+    {%- do return(columns_to_select) -%}
+
+{%- endmacro -%}
