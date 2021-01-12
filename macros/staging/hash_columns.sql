5a3bd35b030c37aa5f731663c8091b107ce90df5
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-12 01:12:12 +0000

    Two more macro tests passing

--- macros/staging/hash_columns.sql
+++ macros/staging/hash_columns.sql
@@ -18,8 +18,8 @@
 
         {%- elif columns[col] is not mapping -%}
 
-            {{- dbtvault.hash(columns=columns[col], 
-                              alias=col, 
+            {{- dbtvault.hash(columns=columns[col],
+                              alias=col,
                               is_hashdiff=false) -}}
         
         {%- elif columns[col] is mapping and not columns[col].is_hashdiff -%}
@@ -28,20 +28,12 @@
                 {%- do exceptions.warn("[" ~ this ~ "] Warning: You provided a list of columns under a 'columns' key, but did not provide the 'is_hashdiff' flag. Use list syntax for PKs.") -%}
             {% endif %}
 
-            {{- dbtvault.hash(columns=columns[col]['columns'], 
-                              alias=col) -}}
+            {{- dbtvault.hash(columns=columns[col]['columns'], alias=col) -}}
 
         {%- endif -%}
 
-        {%- if not loop.last -%},
-{% endif %}
+        {{- ",\n" if not loop.last -}}
     {%- endfor -%}
-
-{%- else -%}
-
-{%- if execute -%}
-{{ exceptions.raise_compiler_error("Invalid column configuration provided to hash_columns macro.") }}
-{%- endif %}
 
 {%- endif %}
 {%- endmacro -%}
