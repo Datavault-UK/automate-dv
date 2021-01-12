4fe70354bd7eeb3a4552948a2b7165680f1aef65
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-12 17:42:46 +0000

    WIP: Changes to pit macro and added feature tests
    3/5 tests passing. Tests that do not pass are the ones which will contain NULLS as Snowlfake does not support the special date 0000:00:00

--- dbtvault-dev/macros/tables/pit.sql
+++ dbtvault-dev/macros/tables/pit.sql
@@ -2,23 +2,23 @@
 
     {{- adapter.dispatch('pit', packages = var('adapter_packages', ['dbtvault']))(source_model=source_model,src_pk=src_pk,
                                                                                    as_of_dates_table=as_of_dates_table,
-                                                                                   satellites=satellites) -}}
+                                                                                  satellites=satellites) -}}
 
 {%- endmacro -%}
 
 {%- macro default__pit(src_pk, as_of_dates_table, satellites, source_model) -%}
 
 {# Set deafualts and obtain source model paths #}
-{% set maxdate = '9999-12-31 23:59:59.999999' %}
-{% set ghost_pk =   ('0000000000000000') %}
-{% set ghost_date = '0000-00-00 00:00:00.000000' %}
+{%- set maxdate = '9999-12-31 23:59:59.999999' -%}
+{%- set ghost_pk =   ('0000000000000000') -%}
+{%- set ghost_date = '9999-12-31 00:00:00.000000' -%}
 
-    {#  Loop to get the source relatiosn using source relation macro and can specify refs i think
+    {#-  Loop to get the source relatiosn using source relation macro and can specify refs i think
     Loop throught the dict and call the 1st key or have a source model key pair in the sub dict
-    Not in loop i can get the hub source relation #}
+    Not in loop i can get the hub source relation -#}
 
 
-{% if as_of_dates_table is mapping and as_of_dates_table is not none -%}
+{%- if as_of_dates_table is mapping and as_of_dates_table is not none -%}
 
     {%- set source_name = as_of_dates_table | first -%}
     {%- set source_table_name = as_of_dates_table[source_name] -%}
@@ -30,44 +30,40 @@
     {%- set source_relation = as_of_dates_table -%}
 {%- endif -%}
 
-
+    {#-
 {% for sat in satelites -%}
     {%- set sat_src = dbtvault.prefix(columns=sat, prefix='_src') -%}
-    {%- set sat_src = ref( sat_src) %}
-{{ log("sat_src: " ~ sat_src , true )}}
+    {%- set sat ~ '_src' = ref(sat) %}
 {% endfor %}
-
-{{ log("satellites: " ~ satellites , true )}}
+-#}
 
 SELECT
-    {{ as_of_dates_table_src -}}.as_of_date,
-    {{ src_pk }},
+        h.{{ src_pk }},
+        x.AS_OF_DATE,
     {% for sat in satellites -%}
-
-        {%- set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
-        {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] -%}
-        {{ log("sat_ldts: " ~ sat_ldts , true )}}
-        COALESCE(MAX({{- sat ~ '_src' -}}.{{- satellites[sat]['pk'][sat_key] -}}), CAST( ghost_pk AS BINARY)) AS {{ sat -}}_{{- sat_key -}},
-        COALESCE(MAX({{- sat ~ '_src' -}}.{{- satellites[sat]['ldts'][sat_ldts] -}}), ghost_date) AS {{ sat -}}_{{ sat_ldts }}
-        {{- ',' if not loop.last -}}
+        {% set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
+        {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] %}
+        COALESCE(MAX({{  sat -}}_SRC.{{- satellites[sat]['pk'][sat_key] -}}), CAST( '{{ ghost_pk }}' AS BINARY)) AS {{ sat -}}_{{- sat_key -}},
+        COALESCE(MAX({{  sat -}}_SRC.{{- satellites[sat]['ldts'][sat_ldts] -}}),TO_TIMESTAMP('{{ ghost_date }}')) AS {{ sat -}}_{{ sat_ldts }}
+        {{- ',' if not loop.last }}
     {%- endfor %}
 
 FROM {{ ref(source_model) }} AS h
 
-INNER JOIN {{ as_of_dates_table_src }} AS x
+INNER JOIN {{ ref(as_of_dates_table) }} AS x
     ON (1=1)
 
-
-{% for sat in satelites -%}
+{% for sat in satellites -%}
     {%- set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
     {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] -%}
-    LEFT JOIN {{- sat ~'_src' -}}
-        ON {{ h.src_pk }} = {{ sat -}}.{{ satellites[sat]['pk'][sat_key] }},
-    WHERE {{ sat -}}.{{ satellites[sat]['ldts'][sat_key] }} <= x.as_of_date
+    LEFT JOIN {{ ref(sat) }} AS {{  sat -}}_SRC
+        ON  h.{{- src_pk }} = {{ sat -}}_SRC.{{ satellites[sat]['pk'][sat_key] }}
+    AND {{ sat -}}_SRC.{{ satellites[sat]['ldts'][sat_ldts] }} <= x.AS_OF_DATE
+
 {% endfor %}
 
 GROUP BY
-x.as_of_date , h.{{- src_pk }}
+ h.{{- src_pk }}, x.AS_OF_DATE
 ORDER BY (1, 2)
 
 
