fa4f008ac2b89d9fc8edc6690849b1abefada5a4
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-12 08:42:30 +0000

    Updated expected as per new logic
    Fix

--- test_project/unit/expected_model_output/staging/stage/test_stage_correctly_generates_sql_for_hashing_with_exclude_flag.sql
+++ test_project/unit/expected_model_output/staging/stage/test_stage_correctly_generates_sql_for_hashing_with_exclude_flag.sql
@@ -1,34 +1,23 @@
 WITH stage AS (
-    SELECT
-
-    BOOKING_FK,
-    ORDER_FK,
-    CUSTOMER_PK,
-    CUSTOMER_ID,
-    LOADDATE,
-    RECORD_SOURCE,
-    CUSTOMER_DOB,
-    CUSTOMER_NAME,
-    NATIONALITY,
-    PHONE,
-    TEST_COLUMN_2,
-    TEST_COLUMN_3,
-    TEST_COLUMN_4,
-    TEST_COLUMN_5,
-    TEST_COLUMN_6,
-    TEST_COLUMN_7,
-    TEST_COLUMN_8,
-    TEST_COLUMN_9
+    SELECT *
 
     FROM [DATABASE_NAME].[SCHEMA_NAME].raw_source
 ),
 
 derived_columns AS (
-    SElECT
+    SElECT *,
+
+    'STG_BOOKING' AS SOURCE,
+    LOADDATE AS EFFECTIVE_FROM
+
+    FROM stage
+),
+
+hashed_columns AS (
+    SELECT
 
     BOOKING_FK,
     ORDER_FK,
-    CUSTOMER_PK,
     CUSTOMER_ID,
     LOADDATE,
     RECORD_SOURCE,
@@ -44,14 +33,6 @@ derived_columns AS (
     TEST_COLUMN_7,
     TEST_COLUMN_8,
     TEST_COLUMN_9,
-    'STG_BOOKING' AS SOURCE,
-    LOADDATE AS EFFECTIVE_FROM
-
-    FROM stage
-),
-
-hashed_columns AS (
-    SELECT *,
 
     CAST((MD5_BINARY(NULLIF(UPPER(TRIM(CAST(CUSTOMER_ID AS VARCHAR))), ''))) AS BINARY(16)) AS CUSTOMER_PK,
     CAST(MD5_BINARY(CONCAT(
