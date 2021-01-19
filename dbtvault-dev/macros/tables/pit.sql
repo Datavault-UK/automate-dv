88016d202bea902820e1d395f620f6bd7f0edd2a
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-19 18:06:06 +0000

    Pit macro re factor

--- dbtvault-dev/macros/tables/pit.sql
+++ dbtvault-dev/macros/tables/pit.sql
@@ -7,8 +7,8 @@
 {%- endmacro -%}
 
 {%- macro default__pit(src_pk, as_of_dates_table, satellites, source_model) -%}
-    {#
-{% if (as_of_dates_table is none) and execute %}
+    {#-
+{%- if (as_of_dates_table is none) and execute -%}
 
     {%- set error_message -%}
     "pit error: Missing as_of_dates table configuration. A as_of_dates_table must be provided."
@@ -17,7 +17,7 @@
     {{- exceptions.raise_compiler_error(error_message) -}}
 {%- endif -%}
 
-{% if (as_of_dates_table[0] != 'AS_OF_DATE') and execute %}
+{%- if (as_of_dates_table[0] != 'AS_OF_DATE') and execute -%}
 
     {%- set error_message -%}
     "pit error: as_of_table column must be called 'AS_OF_DATE'."
@@ -26,14 +26,7 @@
     {{- exceptions.raise_compiler_error(error_message) -}}
 {%- endif -%} -%}
 
-#}
-
-
-{# Set defaults and obtain source model paths #}
-{%- set maxdate = '9999-12-31 23:59:59.999999' -%}
-{%- set ghost_pk =   ('0000000000000000') -%}
-{%- set ghost_date = '0000-01-01 00:00:00.000000' -%}
-
+-#}
 {#- Aquiring the source reltion for the AS_OF table -#}
 {%- if as_of_dates_table is mapping and as_of_dates_table is not none -%}
 
@@ -47,35 +40,64 @@
     {%- set source_relation_AS_OF = ref(as_of_dates_table) -%}
 {%- endif -%}
 
+WITH hub AS (
 
-SELECT
-        h.{{ src_pk }},
-        x.AS_OF_DATE,
-    {% for sat in satellites -%}
-        {% set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
-        {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] %}
-        COALESCE(MAX({{  sat -}}_SRC.{{- satellites[sat]['pk'][sat_key] -}}), CAST( '{{ ghost_pk }}' AS BINARY)) AS {{ sat -}}_{{- sat_key -}},
-        COALESCE(MAX({{  sat -}}_SRC.{{- satellites[sat]['ldts'][sat_ldts] -}}),TO_TIMESTAMP('{{ ghost_date }}')) AS {{ sat -}}_{{ sat_ldts }}
-        {{- ',' if not loop.last }}
-    {%- endfor %}
+	SELECT * FROM {{ ref(source_model) }}
 
-FROM {{ ref(source_model) }} AS h
+),
+as_of_dates_PK_join AS (
+    SELECT
+        hub.{{ src_pk }},
+        as_of.AS_OF_DATE
+    FROM hub
 
-INNER JOIN {{ source_relation_AS_OF}} AS x
+    INNER JOIN {{ source_relation_AS_OF}} AS as_of
     ON (1=1)
+),
 
-{% for sat in satellites -%}
-    {%- set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
-    {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] -%}
-    LEFT JOIN {{ ref(sat) }} AS {{  sat -}}_SRC
-        ON  h.{{- src_pk }} = {{ sat -}}_SRC.{{ satellites[sat]['pk'][sat_key] }}
-    AND {{ sat -}}_SRC.{{ satellites[sat]['ldts'][sat_ldts] }} <= x.AS_OF_DATE
+satellites_cte AS (
 
-{% endfor %}
+    SELECT
+        a.CUSTOMER_PK,
+        a.AS_OF_DATE,
+    {{ dbtvault.process_join_or_columns(satellites=satellites,columns= true) }}
+    FROM as_of_dates_PK_join AS a
 
-GROUP BY
- h.{{- src_pk }}, x.AS_OF_DATE
-ORDER BY (1, 2)
+    {{ dbtvault.process_join_or_columns(satellites=satellites, src_pk=src_pk ,join= true) }}
 
+    GROUP BY
+        a.{{- src_pk }}, a.AS_OF_DATE
+    ORDER BY (1, 2)
+)
 
+SELECT * FROM satellites_cte
 {%- endmacro -%}
+
+
+
+
+{%- macro process_join_or_columns(satellites=none, src_pk=none, join=none, columns=none) -%}
+
+{%- set maxdate = '9999-12-31 23:59:59.999999' -%}
+{%- set ghost_pk = ('0000000000000000') -%}
+{%- set ghost_date = '0000-01-01 00:00:00.000000' -%}
+
+{%- for sat in satellites -%}
+
+    {% set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
+    {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] -%}
+
+    {%- if columns is not none and columns is true %}
+        {{'COALESCE(MAX('~ sat ~'_SRC.'~ satellites[sat]['pk'][sat_key]~'), CAST( '"'"~ghost_pk~"'"' AS BINARY)) AS '~ sat ~'_'~ sat_key ~','}}
+        {{'COALESCE(MAX('~ sat ~'_SRC.'~ satellites[sat]['ldts'][sat_ldts]~'), TO_TIMESTAMP( '"'"~ghost_date~"'"')) AS '~ sat ~'_'~ sat_ldts }}
+        {{- ',' if not loop.last }}
+    {%- endif -%}
+
+    {%- if join is not none and join is true %}
+        {{ 'LEFT JOIN '~ ref(sat) ~' AS '~  sat ~'_SRC' }}
+        {{' ON a.'~ src_pk ~' = '~ sat ~'_SRC.'~ satellites[sat]['pk'][sat_key] }}
+        {{ 'AND '~ sat ~'_SRC.'~ satellites[sat]['ldts'][sat_ldts] ~' <= a.AS_OF_DATE' }}
+
+    {% endif -%}
+{%- endfor %}
+{% endmacro %}
