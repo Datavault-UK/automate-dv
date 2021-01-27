7c5afa6a409f75170cc97c15db1d51d447af64ee
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-27 15:27:01 +0000

    WIP: Incremental PIT load

--- dbtvault-dev/macros/tables/pit.sql
+++ dbtvault-dev/macros/tables/pit.sql
@@ -29,22 +29,152 @@
 {%- set maxdate = '9999-12-31 23:59:59.999999' -%}
 {%- set ghost_pk = ('0000000000000000') -%}
 {%- set ghost_date = '0000-01-01 00:00:00.000000' %}
+
+
+
 WITH hub AS (
 
 	SELECT * FROM {{ ref(source_model) }}
 
+
+as_of AS (
+    SELECT * FROM {{ source_relation_AS_OF}}
 ),
-as_of_dates_PK_join AS (
+
+{% if materilizsation is incremental  -%}
+
+    old_as_of_grain AS (
+        SELECT DISTINCT AS_OF_DATE FROM old_pit
+    ),
+
+
+    as_of_grain_lost_entries(
+        SELECT AS_OF_DATE
+            FROM old_as_of_grain AS a
+
+        LEFT OUTER JOIN as_of AS b
+    	ON a.AS_OF_DATE = b.AS_OF_DATE
+    	AND a.AS_OF_DATE < (SELECT MIN(b.AS_OF_DATE) FROM as_of)
+    ),
+
+    as_of_grain_new_entries AS (
+        SELECT AS_OF_DATE
+        FROM as_of AS a
+        LEFT OUTER JOIN old_as_of_grain AS b
+        ON a.AS_OF_DATE = b.AS_OF_DATE
+        AND a.AS_OF_DATE < (SELECT last_safe_load_datetime from last_safe_load_datetime)
+
+    ),
+
+
+    min_date AS(
+        SELECT min(AS_OF_DATE) AS MIN_DATE
+        FROM as_of_date_table
+    ),
+
+    backfill_as_of AS (
+        SELECT AS_OF_DATE
+    	from as_of
+    	WHERE as_of.AS_OF_DATE <= (SELECT last_safe_load_datetime FROM last_safe_load_datetime)
+    ),
+
+
+    new_hubs AS (
+        SELECT {{ src_pk }}
+    	FROM hub AS h
+    	WHERE h.{{ src_ldts }} >= (select last_safe_load_datetime from last_safe_load_datetime)
+    	),
+
+
+    new_row_as_of AS (
+        SELECT AS_OF_DATE
+    	FROM as_of_date_table
+    	WHERE as_of_date_table.AS_OF_DATE > last_safe_load_datetime.last_safe_load_datetime
+
+    	UNION ALL
+
+    	SELECT as_of_date
+    	FROM as_of_grain_new_entries
+    ),
+
+    overlap AS (
+        SELECT * FROM old_pit AS p
+    	WHERE p.{{ src.pk }} = h.{{ src_pk }}
+    	AND  >= min_date.MIN_DATE
+    	AND p.AS_OF_DATE < last_safe_load_datetime.last_safe_load_datetime
+    	AND p.AS_OF_DATE NOT IN (SELECT * FROM as_of_grain_lost_entries)
+    ),
+
+
+
+    -- backfill any newly arrived hubs, set all historical pit dates to ghost records
+
+    bf_hub(
+        SELECT
+            nh.{{ src_pk }},
+            nr.AS_OF_DATE,
+        FROM new_hubs AS nh
+
+        INNER JOIN new_row_as_of AS nr
+          ON (1=1)
+    ),
+
+
+    bf_satellites_cte AS (
+        SELECT
+            bf.{{ src_pk }},
+            bf.AS_OF_DATE,
+            {%- for sat in satellites -%}
+                {%- filter indent(width=8) -%}
+                {% set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
+                {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0]  -%}
+                {{- "\n" -}}
+                {{ 'CAST( '"'"~ghost_pk~"'"' AS BINARY)) AS '~ sat ~'_'~ sat_key ~','  }}
+                {{- "\n" -}}
+                {{ 'TO_TIMESTAMP( '"'"~ghost_date~"'"')) AS '~ sat ~'_'~ sat_ldts  }}
+                {{- ',' if not loop.last -}}
+                {% endfilter %}
+            {%- endfor %}S
+
+          FROM bf_hub AS bf
+
+         {% for sat in satellites -%}
+                {%- set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
+                {%- set sat_ldts =(satellites[sat]['ldts'].keys() | list )[0] -%}
+                LEFT JOIN {{ ref(sat) }} AS {{  sat -}}_SRC
+                    ON  bf.{{- src_pk }} = {{ sat -}}_SRC.{{ satellites[sat]['pk'][sat_key] }}
+                AND {{ sat -}}_SRC.{{ satellites[sat]['ldts'][sat_ldts] }} <= bf.AS_OF_DATE
+
+             {% endfor %}
+
+            GROUP BY
+                bf.{{- src_pk }}, bf.AS_OF_DATE
+            ORDER BY (1, 2)
+
+    ),
+
+
+    backfill AS (
+        SELECT * FROM bf_satellites
+    ),
+
+{% else %}
+
+
+
+
+),
+new_as_of_dates_PK_join AS (
     SELECT
         hub.{{ src_pk }},
-        as_of.AS_OF_DATE
+        x.AS_OF_DATE
     FROM hub
 
-    INNER JOIN {{ source_relation_AS_OF}} AS as_of
+    INNER JOIN new_row_as_of AS x
     ON (1=1)
 ),
 
-satellites_cte AS (
+new_row_satellites_cte AS (
 
     SELECT
         a.{{ src_pk }},
@@ -61,7 +191,7 @@ satellites_cte AS (
             {% endfilter %}
         {%- endfor %}
 
-    FROM as_of_dates_PK_join AS a
+    FROM new_as_of_dates_PK_join AS a
 
     {% for sat in satellites -%}
         {%- set sat_key = (satellites[sat]['pk'].keys() | list )[0] -%}
@@ -75,7 +205,31 @@ satellites_cte AS (
     GROUP BY
         a.{{- src_pk }}, a.AS_OF_DATE
     ORDER BY (1, 2)
+),
+
+new_rows AS(
+    SELECT * FROM satellites_cte
 )
 
-SELECT * FROM satellites_cte
+SELECT * FROM new_rows
+{% if materilizsation is incremental  -%}
+    UNION ALL
+    SELECT * FROM overlap
+    UNION ALL
+    SELECT * FROM backfill
+{%- endif -%}
+
+
 {%- endmacro -%}
+
+
+{#
+source model
+src_pk
+as of dates
+satelites and their key pairs
+
+
+src_ldts
+list of stages used for pit as well as the date column
+#}
