c9c95adba1eb4870b2f5b7227eaec297ab52da98
Flynn <Flynn.sheridan@business-thinking.com>
2021-01-27 15:56:51 +0000

    WIP:Incremental PIT load

--- dbtvault-dev/macros/tables/pit.sql
+++ dbtvault-dev/macros/tables/pit.sql
@@ -31,7 +31,6 @@
 {%- set ghost_date = '0000-01-01 00:00:00.000000' %}
 
 
-
 WITH hub AS (
 
 	SELECT * FROM {{ ref(source_model) }}
@@ -41,7 +40,19 @@ as_of AS (
     SELECT * FROM {{ source_relation_AS_OF}}
 ),
 
-{% if materilizsation is incremental  -%}
+{% if model.config.materialized == "incremental"  -%}
+
+    last_safe_load_datetime AS (
+    SELECT min(LOAD_DATE_TIME) AS LAST_SAFE_LOAD_DATETIME FROM (
+        {%- filter indent(width=8) -%}
+        {% for stg in stage_tables %}
+            {{ "SELECT min("~[stg]~"AS LOAD_DATE_TIME FROM "~ref(stg) }}
+            {{- 'UNION ALL' if not loop.last -}}
+        {%- endfor -%}
+        {%- endfilter -%}
+        )
+    ),
+
 
     old_as_of_grain AS (
         SELECT DISTINCT AS_OF_DATE FROM old_pit
@@ -62,7 +73,7 @@ as_of AS (
         FROM as_of AS a
         LEFT OUTER JOIN old_as_of_grain AS b
         ON a.AS_OF_DATE = b.AS_OF_DATE
-        AND a.AS_OF_DATE < (SELECT last_safe_load_datetime from last_safe_load_datetime)
+        AND a.AS_OF_DATE < (SELECT LAST_SAFE_LOAD_DATETIME from last_safe_load_datetime)
 
     ),
 
@@ -75,21 +86,21 @@ as_of AS (
     backfill_as_of AS (
         SELECT AS_OF_DATE
     	from as_of
-    	WHERE as_of.AS_OF_DATE <= (SELECT last_safe_load_datetime FROM last_safe_load_datetime)
+    	WHERE as_of.AS_OF_DATE <= (SELECT LAST_SAFE_LOAD_DATETIME FROM last_safe_load_datetime)
     ),
 
 
     new_hubs AS (
         SELECT {{ src_pk }}
     	FROM hub AS h
-    	WHERE h.{{ src_ldts }} >= (select last_safe_load_datetime from last_safe_load_datetime)
+    	WHERE h.{{ src_ldts }} >= (SELECT LAST_SAFE_LOAD_DATETIME FROM last_safe_load_datetime)
     	),
 
 
     new_row_as_of AS (
         SELECT AS_OF_DATE
     	FROM as_of_date_table
-    	WHERE as_of_date_table.AS_OF_DATE > last_safe_load_datetime.last_safe_load_datetime
+    	WHERE as_of_date_table.AS_OF_DATE > last_safe_load_datetime.LAST_SAFE_LOAD_DATETIME
 
     	UNION ALL
 
@@ -101,7 +112,7 @@ as_of AS (
         SELECT * FROM old_pit AS p
     	WHERE p.{{ src.pk }} = h.{{ src_pk }}
     	AND  >= min_date.MIN_DATE
-    	AND p.AS_OF_DATE < last_safe_load_datetime.last_safe_load_datetime
+    	AND p.AS_OF_DATE < last_safe_load_datetime.LAST_SAFE_LOAD_DATETIME
     	AND p.AS_OF_DATE NOT IN (SELECT * FROM as_of_grain_lost_entries)
     ),
 
@@ -160,7 +171,11 @@ as_of AS (
 
 {% else %}
 
+    new_row_as_of AS(
+    SELECT * FROM as_of
+    ),
 
+{% endif %}
 
 
 ),
@@ -212,7 +227,7 @@ new_rows AS(
 )
 
 SELECT * FROM new_rows
-{% if materilizsation is incremental  -%}
+{% if model.config.materialized == "incremental"  -%}
     UNION ALL
     SELECT * FROM overlap
     UNION ALL
