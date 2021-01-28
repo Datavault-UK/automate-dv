b42881f6e4e9badfce75acbf2d853edc786c6f3c
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-28 14:22:38 +0000

    Updated test project to dbt 0.19.0

--- test_project/dbtvault_test/models/unit/staging/stage/test_stage_correctly_generates_sql_for_hashing_and_source_from_yaml_for_single_hash_and_include.sql
+++ test_project/dbtvault_test/models/unit/staging/stage/test_stage_correctly_generates_sql_for_hashing_and_source_from_yaml_for_single_hash_and_include.sql
@@ -1,5 +1,29 @@
-{{ dbtvault.stage(include_source_columns=var('include_source_columns', none), 
-                  source_model=var('source_model', none), 
-                  hashed_columns=var('hashed_columns', none), 
-                  derived_columns=var('derived_columns', none),
-                  ranked_columns=var('ranked_columns', none)) }}
+{%- set yaml_metadata -%}
+source_model: raw_source
+include_source_columns: true
+hashed_columns:
+  HASHDIFF:
+    is_hashdiff: true
+    columns:
+      - CUSTOMER_DOB
+      - CUSTOMER_ID
+      - CUSTOMER_NAME
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{% set include_source_columns = metadata_dict.get('include_source_columns', none) %}
+
+{% set source_model = metadata_dict.get('source_model', none) %}
+
+{% set derived_columns = metadata_dict.get('derived_columns', none) %}
+
+{% set hashed_columns = metadata_dict.get('hashed_columns', none) %}
+
+{% set ranked_columns = metadata_dict.get('ranked_columns', none) %}
+
+{{ dbtvault.stage(include_source_columns=include_source_columns,
+                  source_model=source_model,
+                  derived_columns=derived_columns,
+                  hashed_columns=hashed_columns,
+                  ranked_columns=ranked_columns) }}
