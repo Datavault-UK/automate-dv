57065815d87d801620a94a7e829db06a6582e5c2
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-28 17:24:00 +0000

    Update dbt_project.yml files
    Update Pipfile
    
    Prevent pipfile installing pre release dependencies by default
    Fix Feature test execution

---
+++ test_project/dbtvault_test/macros/generate_schema_name.sql
@@ -0,0 +1,18 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {{- target.schema -}}_{{ env_var('SNOWFLAKE_DB_USER') }}{{ '_' ~ env_var('CIRCLE_BRANCH', '') | replace('-','_') | replace('.','_') if env_var('CIRCLE_BRANCH', '') -}}
+    {{- '_' ~ env_var('CIRCLE_JOB', '') if env_var('CIRCLE_JOB', '') -}}
+    {{- '_' ~ env_var('CIRCLE_NODE_INDEX', '') if env_var('CIRCLE_NODE_INDEX', '') -}}
+{%- endmacro %}
+
+
+{% macro get_schema_name() -%}
+
+    {%- set schema_name -%}
+        {{- target.schema -}}_{{ env_var('SNOWFLAKE_DB_USER') }}{{ '_' ~ env_var('CIRCLE_BRANCH', '') | replace('-','_') | replace('.','_') if env_var('CIRCLE_BRANCH', '') -}}
+        {{- '_' ~ env_var('CIRCLE_JOB', '') if env_var('CIRCLE_JOB', '') -}}
+        {{- '_' ~ env_var('CIRCLE_NODE_INDEX', '') if env_var('CIRCLE_NODE_INDEX', '') -}}
+    {%- endset -%}
+
+    {% do return(schema_name) %}
+
+{%- endmacro %}
