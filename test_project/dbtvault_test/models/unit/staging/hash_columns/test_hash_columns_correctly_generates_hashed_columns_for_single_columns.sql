aac5e0f3707e5a4cf16a71dae5f638782bafdaab
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-01-28 14:22:38 +0000

    Updated test project to dbt 0.19.0

--- test_project/dbtvault_test/models/unit/staging/hash_columns/test_hash_columns_correctly_generates_hashed_columns_for_single_columns.sql
+++ test_project/dbtvault_test/models/unit/staging/hash_columns/test_hash_columns_correctly_generates_hashed_columns_for_single_columns.sql
@@ -1,1 +1,9 @@
-{{ dbtvault.hash_columns(columns=var('columns')) }}
+{%- set yaml_metadata -%}
+columns:
+  BOOKING_PK: BOOKING_REF
+  CUSTOMER_PK: CUSTOMER_ID
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ dbtvault.hash_columns(columns=metadata_dict['columns']) }}
