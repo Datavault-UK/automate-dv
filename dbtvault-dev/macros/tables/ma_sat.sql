d9b60409efb59a4af40ece9f9cb617c924b40614
Norbert Acatrinei <norbert.acatrinei@business-thinking.com>
2021-02-22 11:20:14 +0000

    Run config and Rename

---
+++ dbtvault-dev/macros/tables/ma_sat.sql
@@ -0,0 +1,79 @@
+{%- macro ma_sat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+
+    {{- adapter.dispatch('ma_sat', packages = dbtvault.get_dbtvault_namespaces())(src_pk=src_pk, src_hashdiff=src_hashdiff,
+                                                                                  src_payload=src_payload, src_eff=src_eff, src_ldts=src_ldts,
+                                                                                  src_source=src_source, source_model=source_model) -}}
+
+{%- endmacro %}
+
+{%- macro default__msat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+
+
+{{- dbtvault.check_required_parameters(src_pk=src_pk, src_hashdiff=src_hashdiff, src_payload=src_payload,
+                                       src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
+                                       source_model=source_model) -}}
+
+{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source]) -%}
+{%- set rank_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_ldts]) -%}
+
+{%- if model.config.materialized == 'vault_insert_by_rank' %}
+    {%- set source_cols_with_rank = source_cols + [config.get('rank_column')] -%}
+{%- endif -%}
+
+{{ dbtvault.prepend_generated_by() }}
+
+WITH source_data AS (
+    {%- if model.config.materialized == 'vault_insert_by_rank' %}
+    SELECT {{ dbtvault.prefix(source_cols_with_rank, 'a', alias_target='source') }}
+    {%- else %}
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='source') }}
+    {%- endif %}
+    FROM {{ ref(source_model) }} AS a
+    {%- if model.config.materialized == 'vault_insert_by_period' %}
+    WHERE __PERIOD_FILTER__
+    {% endif %}
+    {%- set source_cte = "source_data" %}
+),
+
+{%- if model.config.materialized == 'vault_insert_by_rank' %}
+rank_col AS (
+    SELECT * FROM source_data
+    WHERE __RANK_FILTER__
+    {%- set source_cte = "rank_col" %}
+),
+{% endif -%}
+
+{% if dbtvault.is_vault_insert_by_period() or dbtvault.is_vault_insert_by_rank() or is_incremental() %}
+
+update_records AS (
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='target') }}
+    FROM {{ this }} as a
+    JOIN source_data as b
+    ON a.{{ src_pk }} = b.{{ src_pk }}
+),
+
+latest_records AS (
+    SELECT {{ dbtvault.prefix(rank_cols, 'c', alias_target='target') }},
+           CASE WHEN RANK()
+           OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'c') }}
+           ORDER BY {{ dbtvault.prefix([src_ldts], 'c') }} DESC) = 1
+    THEN 'Y' ELSE 'N' END AS latest
+    FROM update_records as c
+    QUALIFY latest = 'Y'
+),
+{%- endif %}
+
+records_to_insert AS (
+    SELECT DISTINCT {{ dbtvault.alias_all(source_cols, 'e') }}
+    FROM {{ source_cte }} AS e
+    {%- if dbtvault.is_vault_insert_by_period() or dbtvault.is_vault_insert_by_rank() or is_incremental() %}
+    LEFT JOIN latest_records
+    ON {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} = {{ dbtvault.prefix([src_hashdiff], 'e') }}
+    WHERE {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} IS NULL
+    {%- endif %}
+)
+
+SELECT * FROM records_to_insert
+
+
+{%- endmacro -%}
