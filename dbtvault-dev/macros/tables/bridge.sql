16a85a54b031781663c562376e336afe0e4978e3
Norbert Acatrinei <norbert.acatrinei@business-thinking.com>
2021-03-12 16:43:19 +0000

    WIP - Updated bridge macro 2

--- dbtvault-dev/macros/tables/bridge.sql
+++ dbtvault-dev/macros/tables/bridge.sql
@@ -30,80 +30,87 @@
 {%- set ghost_date = '1990-01-01 00:00:00.000000' %}
 
 WITH
-  AS_OF_DATES_FOR_BRIDGE AS
-    (
-       SELECT
-             a.AS_OF_DATE
-       FROM {{ source_relation_AS_OF }} AS a
-       WHERE
-             a.AS_OF_DATE <= CURRENT_DATE()
-{#             AND #}
-{#             a.AS_OF_DATE > #}
-{#             (#}
-{#             CASE#}
-{#                    WHEN (SELECT MAX(b.AS_OF_DATE) FROM "SANDBOX"."NORBERT_ACATRINEI"."BRIDGE" AS b) IS NULL#}
-{#                           THEN TO_TIMESTAMP( '"'"~ghost_date~"'"')#}
-{#                    ELSE#}
-{#                           (SELECT MAX(b."AS_OF_DATE") FROM "SANDBOX"."NORBERT_ACATRINEI"."BRIDGE" AS b)#}
-{#             END#}
-{#         )#}
+    AS_OF_DATES_FOR_BRIDGE AS(
+         SELECT
+               a.AS_OF_DATE
+         FROM {{ source_relation_AS_OF }} AS a
+         WHERE
+               a.AS_OF_DATE <= CURRENT_DATE()
+          {#      AND #}
+          {#      a.AS_OF_DATE > #}
+          {#      (#}
+          {#      CASE#}
+          {#             WHEN (SELECT MAX(b.AS_OF_DATE) FROM "SANDBOX"."NORBERT_ACATRINEI"."BRIDGE" AS b) IS NULL#}
+          {#                    THEN TO_TIMESTAMP( '"'"~ghost_date~"'"')#}
+          {#             ELSE#}
+          {#                    (SELECT MAX(b."AS_OF_DATE") FROM "SANDBOX"."NORBERT_ACATRINEI"."BRIDGE" AS b)#}
+          {#      END#}
+          {#  )#}
     ),
 
-BRIDGE_WALK AS
-    (
-      SELECT
-          {%-  for hub in hubs -%}
-               {%- filter indent(width=8) -%}
-               {% set hub_key = (hubs[hub]['pk'].keys() | list )[0] -%}
-               {{- "\n" -}}
-               a.hubs[hub]['pk'][hub_key]
-               {{- ',' if not loop.last -}}
-               {% endfilter %}
-          {%- endfor %}
-          b."AS_OF_DATE",
-          {%-  for link in links -%}
-               {%- filter indent(width=8) -%}
-               {% set link_key = (links[link]['pk'].keys() | list )[0] -%}
-               {{- "\n" -}}
-               a.links[link]['pk'][link_key]
-               {{- ',' if not loop.last -}}
-               {% endfilter %}
-          {%- endfor %}
-          COALESCE(MAX(c."LINK_CUSTOMER_NATION_PK"), $ghost_pk) AS LINK1_PK,
-          COALESCE(MAX(e."ORDER_CUSTOMER_PK"), $ghost_pk) AS LINK2_PK,
-          COALESCE(MAX(efs1."END_DATE"), $ghost_date) AS EFF1_END_DATE,
-          COALESCE(MAX(efs2."END_DATE"), $ghost_date) AS EFF2_END_DATE
+BRIDGE_WALK AS (
+    SELECT
+        {%-  for hub in hubs.keys() -%}
+             {%- filter indent(width=8) -%}
+             {% set hub_key = (hub['pk'].keys() | list )[0] -%}
+             {{- "\n" -}}
+             a.hubs[hub]['pk'][hub_key]
+             {{- ',' if not loop.last -}}
+             {% endfilter %}
+        {%- endfor -%}
+        b."AS_OF_DATE",
+        {# THIS BIT NEEDS TO BE DONE AFTER THE FROM BIT AS YOU NEED THE CORRECT ALIASES #}
+        {%-  for link in links -%}
+             {%- filter indent(width=8) -%}
+             {% set link_key = (links[link]['pk'].keys() | list )[0] -%}
+             {{- "\n" -}}
+             a.links[link]['pk'][link_key]
+             {{- ',' if not loop.last -}}
+             {% endfilter %}
+        {%- endfor %}
+        COALESCE(MAX(c."LINK_CUSTOMER_NATION_PK"), $ghost_pk) AS LINK1_PK,
+        COALESCE(MAX(e."ORDER_CUSTOMER_PK"), $ghost_pk) AS LINK2_PK,
+        COALESCE(MAX(efs1."END_DATE"), $ghost_date) AS EFF1_END_DATE,
+        COALESCE(MAX(efs2."END_DATE"), $ghost_date) AS EFF2_END_DATE
 
 
-      FROM
-          {%-  for hub in hubs.keys() -%}
-               {%- filter indent(width=8) -%}
-               {{- "\n" -}}
-               {{ ref(hub) }}
-               {{- ',' if not loop.last -}}
-               {% endfilter %}
-          {%- endfor %}
-      INNER JOIN AS_OF_DATES_FOR_BRIDGE AS b
-          ON (1=1)
+    FROM
+    {# IN ORDER TO GET RID OF THE FOR LOOP - IF WE INDEED ARE GOING TO USE ONLY ONE HUB AT MOST #}
+    {# THEN WE NEED TO DEFINE SPLIT "hubs" INTO "hub" : "HUB_..." & "src_pk": "..._PK" #}
+        {%-  for hub in hubs.keys() -%}
+             {%- filter indent(width=8) -%}
+             {{- "\n" -}}
+             {{ ref(hub) }}
+             {{- ',' if not loop.last -}}
+             {% endfilter %}
+        {%- endfor %}
+    INNER JOIN AS_OF_DATES_FOR_BRIDGE AS b
+        ON (1=1)
 
-      LEFT JOIN "SANDBOX"."NORBERT_ACATRINEI"."LINK_CUSTOMER_NATION" AS c
-          ON c."CUSTOMER_PK" = a."CUSTOMER_PK"
-      INNER JOIN "SANDBOX"."NORBERT_ACATRINEI".EFF_SAT_CUSTOMER_NATION AS efs1
-          ON c."LINK_CUSTOMER_NATION_PK" = efs1."LINK_CUSTOMER_NATION_PK"
-          AND efs1."LOAD_DATE" <= b."AS_OF_DATE"
+    {# THINK OF HOW YOU CAN WRITE AS FEW LOOPS FOR THIS NEXT SECTION #}
+    LEFT JOIN "SANDBOX"."NORBERT_ACATRINEI"."LINK_CUSTOMER_NATION" AS c
+        ON c."CUSTOMER_PK" = a."CUSTOMER_PK"
+    INNER JOIN "SANDBOX"."NORBERT_ACATRINEI".EFF_SAT_CUSTOMER_NATION AS efs1
+        ON c."LINK_CUSTOMER_NATION_PK" = efs1."LINK_CUSTOMER_NATION_PK"
+        AND efs1."LOAD_DATE" <= b."AS_OF_DATE"
 
-      LEFT JOIN "SANDBOX"."NORBERT_ACATRINEI"."LINK_CUSTOMER_ORDER" AS e
-          ON c."CUSTOMER_PK" = e."CUSTOMER_PK"
-      INNER JOIN "SANDBOX"."NORBERT_ACATRINEI".EFF_SAT_CUSTOMER_ORDER AS efs2
-          ON e."ORDER_CUSTOMER_PK" = efs2."ORDER_CUSTOMER_PK"
-          AND efs2."LOAD_DATE" <= b."AS_OF_DATE"
+    LEFT JOIN "SANDBOX"."NORBERT_ACATRINEI"."LINK_CUSTOMER_ORDER" AS e
+        ON c."CUSTOMER_PK" = e."CUSTOMER_PK"
+    INNER JOIN "SANDBOX"."NORBERT_ACATRINEI".EFF_SAT_CUSTOMER_ORDER AS efs2
+        ON e."ORDER_CUSTOMER_PK" = efs2."ORDER_CUSTOMER_PK"
+        AND efs2."LOAD_DATE" <= b."AS_OF_DATE"
 
-      WHERE
+    WHERE
 
-      GROUP BY
-          b."AS_OF_DATE", a."CUSTOMER_PK"
+    GROUP BY
+        b."AS_OF_DATE", a."CUSTOMER_PK"
 
-      ORDER BY 1,2)
+    ORDER BY 1,2)
+
+
+SELECT * FROM BRIDGE_WALK WHERE
+    EFF1_END_DATE = $maxdate and
+    EFF2_END_DATE = $maxdate;
 
 
 
