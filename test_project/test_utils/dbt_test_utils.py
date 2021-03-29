f0dfb842791a2c888a51d4bac530838347cec574
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-03-29 10:49:58 +0000

    Remove OOS and XTS references

--- test_project/test_utils/dbt_test_utils.py
+++ test_project/test_utils/dbt_test_utils.py
@@ -569,8 +569,6 @@ class DBTVAULTGenerator:
             "sat": self.sat,
             "eff_sat": self.eff_sat,
             "t_link": self.t_link,
-            "xts": self.xts,
-            "oos_sat": self.oos_sat,
             "ma_sat": self.ma_sat
         }
 
@@ -719,51 +717,8 @@ class DBTVAULTGenerator:
 
         self.template_to_file(template, model_name)
 
-    def xts(self, model_name, source_model, src_pk, src_ldts, src_satellite, src_source, config=None):
-        """
-        Generate a XTS template
-        """
-
-        template = f"""
-        {{% set src_satellite = {src_satellite} %}}
-
-        {{{{ config({config}) }}}}
-        {{{{ dbtvault.xts({src_pk}, {src_satellite}, {src_ldts}, {src_source},
-                          {source_model})   }}}}
-        """
-
-        textwrap.dedent(template)
-
-        self.template_to_file(template, model_name)
-
-    def oos_sat(self, model_name, src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model,
-                out_of_sequence=None, config=None):
-        """
-        Generate a out of sequence satellite model template
-            :param model_name: Name of the model file
-            :param src_pk: Source pk
-            :param src_hashdiff: Source hashdiff
-            :param src_payload: Source payload
-            :param src_eff: Source effective from
-            :param src_ldts: Source load date timestamp
-            :param src_source: Source record source column
-            :param source_model: Model name to select from
-            :param out_of_sequence: Optional dictionary of metadata required for out of sequence sat
-            :param config: Optional model config
-        """
-
-        template = f"""
-        {{{{ config({config}) }}}}
-        {{{{ dbtvault.oos_sat({src_pk}, {src_hashdiff}, {src_payload},
-                              {src_eff}, {src_ldts}, {src_source},
-                              {source_model}, {out_of_sequence}) }}}}
-        """
-
-        self.template_to_file(template, model_name)
-
     def ma_sat(self, model_name, src_pk, src_cdk, src_hashdiff, src_payload,
-             src_eff, src_ldts, src_source, source_model,
-             config):
+               src_eff, src_ldts, src_source, source_model, config):
         """
         Generate a multi active satellite model template
             :param model_name: Name of the model file
