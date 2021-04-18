4f8b8ad80bf4aae0a046cb7698d8559a241f6af7
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-04-18 16:02:41 +0000

    Partition by now allows multiple columns

--- macros/staging/rank_columns.sql
+++ macros/staging/rank_columns.sql
@@ -18,7 +18,13 @@
                 {%- set order_by_str = columns[col].order_by -%}
             {%- endif -%}
 
-            {{- "RANK() OVER (PARTITION BY {} ORDER BY {}) AS {}".format(columns[col].partition_by, order_by_str, col) | indent(4) -}}
+            {%- if dbtvault.is_list(columns[col].partition_by) -%}
+                {%- set partition_by_str = columns[col].partition_by | join(", ") -%}
+            {%- else -%}
+                {%- set partition_by_str = columns[col].partition_by -%}
+            {%- endif -%}
+
+            {{- "RANK() OVER (PARTITION BY {} ORDER BY {}) AS {}".format(partition_by_str, order_by_str, col) | indent(4) -}}
 
         {%- endif -%}
 
