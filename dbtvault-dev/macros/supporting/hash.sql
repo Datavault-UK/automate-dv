001420f0967be19246c8a65c0b0dfa74de43328b
Jocelyn Shannon <jocelyn.shannon@data-vault.com>
2021-07-15 11:39:33 +0000

    Fixed hash and schema_tests for BigQuery

--- dbtvault-dev/macros/supporting/hash.sql
+++ dbtvault-dev/macros/supporting/hash.sql
@@ -16,12 +16,24 @@
 {%- set hash = var('hash', 'MD5') -%}
 
 {#- Select hashing algorithm -#}
-{%- if hash == 'MD5' -%}
+{%- if target.type == 'bigquery' -%}
     {%- set hash_alg = 'MD5' -%}
-    {%- set hash_size = 16 -%}
+    {%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
+{%- elif target.type == 'snowflake' -%}
+    {%- if hash == 'MD5' -%}
+        {%- set hash_alg = 'MD5_BINARY' -%}
+        {%- set hash_size = 16 -%}
+    {%- elif hash == 'SHA' -%}
+        {%- set hash_alg = 'SHA2_BINARY' -%}
+        {%- set hash_size = 32 -%}
+    {%- else -%}
+        {%- set hash_alg = 'MD5_BINARY' -%}
+        {%- set hash_size = 16 -%}
+    {%- endif -%}
+    {%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')" %}
 {%- endif -%}
 
-{%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
+
 
 {#- Alpha sort columns before hashing if a hashdiff -#}
 {%- if is_hashdiff and dbtvault.is_list(columns) -%}
@@ -29,42 +41,45 @@
 {%- endif -%}
 
 {#- If single column to hash -#}
-{%- if columns is string -%}
-    {%- set column_str = dbtvault.as_constant(columns) -%}
-    {{- "CAST(({}({})) AS BYTES({})) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), hash_size, alias) | indent(4) -}}
+{%- if target.type == 'bigquery' -%}
+    {%- if columns is string -%}
+        {%- set column_str = dbtvault.as_constant(columns) -%}
+        {{- "CAST(({}({})) AS BYTES) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), alias) | indent(4) -}}
 
-{#- Else a list of columns to hash -#}
-{%- else -%}
-    {%- set all_null = [] -%}
-
-    {%- if is_hashdiff -%}
-        {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {#- Else a list of columns to hash -#}
     {%- else -%}
-        {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
-    {%- endif -%}
+     {%- set all_null = [] -%}
 
-    {%- for column in columns -%}
-
-        {%- do all_null.append(null_placeholder_string) -%}
-
-        {%- set column_str = dbtvault.as_constant(column) -%}
-        {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
-        {{- "," if not loop.last -}}
-
-        {%- if loop.last -%}
-
-            {% if is_hashdiff %}
-                {{- "\n)) AS BYTES({})) AS {}".format(hash_size, alias) -}}
-            {%- else -%}
-                {{- "\n), '{}')) AS BYTES({})) AS {}".format(all_null | join(""), hash_size, alias) -}}
-            {%- endif -%}
+        {%- if is_hashdiff -%}
+           {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
         {%- else -%}
-
-            {%- do all_null.append(concat_string) -%}
-
+            {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
         {%- endif -%}
 
-    {%- endfor -%}
+        {%- for column in columns -%}
+
+            {%- do all_null.append(null_placeholder_string) -%}
+
+            {%- set column_str = dbtvault.as_constant(column) -%}
+            {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
+            {{- "," if not loop.last -}}
+
+            {%- if loop.last -%}
+
+                {% if is_hashdiff %}
+                    {{- "\n)) AS BYTES) AS {}".format(alias) -}}
+                {%- else -%}
+                    {{- "\n), '{}')) AS BYTES) AS {}".format(all_null | join(""), alias) -}}
+                {%- endif -%}
+            {%- else -%}
+
+                {%- do all_null.append(concat_string) -%}
+
+            {%- endif -%}
+
+        {%- endfor -%}
+
+    {%- endif -%}
 
 {%- endif -%}
 
