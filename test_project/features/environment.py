e291ae3ba851af2ec884ee2cac752683eb7a47a2
Jocelyn Shannon <jocelyn.shannon@data-vault.com>
2021-07-21 09:32:27 +0000

    setting up bigquery fixtures/feature for sat

--- test_project/features/environment.py
+++ test_project/features/environment.py
@@ -1,5 +1,6 @@
 from behave.fixture import use_fixture_by_tag
 
+from test_project.features.bigquery_fixtures import satellite_bigquery
 from test_project.features.fixtures import *
 from test_project.test_utils.dbt_test_utils import *
 
@@ -12,6 +13,7 @@ fixture_registry = {
     "fixture.single_source_link": single_source_link,
     "fixture.multi_source_link": multi_source_link,
     "fixture.satellite": satellite,
+    "fixture.satellite_bigquery": satellite_bigquery,
     "fixture.satellite_cycle": satellite_cycle,
     "fixture.eff_satellite": eff_satellite,
     "fixture.eff_satellite_testing_auto_end_dating": eff_satellite_testing_auto_end_dating,
