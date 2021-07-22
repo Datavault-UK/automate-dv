4b67adba920d306b80957fe34889ea4d5cfb2f04
Jocelyn Shannon <jocelyn.shannon@data-vault.com>
2021-07-22 14:05:03 +0000

    added dispatch to hash macro

--- dbtvault-dev/macros/supporting/hash.sql
+++ dbtvault-dev/macros/supporting/hash.sql
@@ -8,6 +8,68 @@
 
 {%- endmacro %}
 
+{%- macro bigquery__hash(columns, alias, is_hashdiff) -%}
+
+{%- set concat_string = "||" -%}
+{%- set null_placeholder_string = "^^" -%}
+
+{%- set hash = var('hash', 'MD5') -%}
+
+{#- Select hashing algorithm -#}
+{%- if hash == 'MD5' -%}
+    {%- set hash_alg = 'MD5' -%}
+{%- elif hash == 'SHA' -%}
+    {%- set hash_alg = 'SHA256' -%}
+{%- endif -%}
+{%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
+
+{#- Alpha sort columns before hashing if a hashdiff -#}
+{%- if is_hashdiff and dbtvault.is_list(columns) -%}
+    {%- set columns = columns|sort -%}
+{%- endif -%}
+
+{#- If single column to hash -#}
+{%- if columns is string -%}
+    {%- set column_str = dbtvault.as_constant(columns) -%}
+    {{- "CAST(UPPER(TO_HEX({}({}))) AS STRING) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), alias) | indent(4) -}}
+
+{#- Else a list of columns to hash -#}
+{%- else -%}
+ {%- set all_null = [] -%}
+
+    {%- if is_hashdiff -%}
+       {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- else -%}
+        {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- endif -%}
+
+    {%- for column in columns -%}
+
+        {%- do all_null.append(null_placeholder_string) -%}
+
+        {%- set column_str = dbtvault.as_constant(column) -%}
+        {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
+        {{- "," if not loop.last -}}
+
+        {%- if loop.last -%}
+
+            {% if is_hashdiff %}
+                {{- "\n)) AS BYTES) AS {}".format(alias) -}}
+            {%- else -%}
+                {{- "\n), '{}')) AS BYTES) AS {}".format(all_null | join(""), alias) -}}
+            {%- endif -%}
+        {%- else -%}
+
+            {%- do all_null.append(concat_string) -%}
+
+        {%- endif -%}
+
+    {%- endfor -%}
+
+{%- endif -%}
+
+{%- endmacro -%}
+
 {%- macro default__hash(columns, alias, is_hashdiff) -%}
 
 {%- set concat_string = "||" -%}
@@ -16,23 +78,18 @@
 {%- set hash = var('hash', 'MD5') -%}
 
 {#- Select hashing algorithm -#}
-{%- if target.type == 'bigquery' -%}
-    {%- set hash_alg = 'MD5' -%}
-    {%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
-{%- elif target.type == 'snowflake' -%}
-    {%- if hash == 'MD5' -%}
-        {%- set hash_alg = 'MD5_BINARY' -%}
-        {%- set hash_size = 16 -%}
-    {%- elif hash == 'SHA' -%}
-        {%- set hash_alg = 'SHA2_BINARY' -%}
-        {%- set hash_size = 32 -%}
-    {%- else -%}
-        {%- set hash_alg = 'MD5_BINARY' -%}
-        {%- set hash_size = 16 -%}
-    {%- endif -%}
-    {%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')" %}
+{%- if hash == 'MD5' -%}
+    {%- set hash_alg = 'MD5_BINARY' -%}
+    {%- set hash_size = 16 -%}
+{%- elif hash == 'SHA' -%}
+    {%- set hash_alg = 'SHA2_BINARY' -%}
+    {%- set hash_size = 32 -%}
+{%- else -%}
+    {%- set hash_alg = 'MD5_BINARY' -%}
+    {%- set hash_size = 16 -%}
 {%- endif -%}
 
+{%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')" %}
 
 
 {#- Alpha sort columns before hashing if a hashdiff -#}
@@ -41,45 +98,42 @@
 {%- endif -%}
 
 {#- If single column to hash -#}
-{%- if target.type == 'bigquery' -%}
-    {%- if columns is string -%}
-        {%- set column_str = dbtvault.as_constant(columns) -%}
-        {{- "CAST(({}({})) AS BYTES) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), alias) | indent(4) -}}
+{%- if columns is string -%}
+    {%- set column_str = dbtvault.as_constant(columns) -%}
+    {{- "CAST(({}({})) AS BINARY({})) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), hash_size, alias) | indent(4) -}}
 
-    {#- Else a list of columns to hash -#}
+{#- Else a list of columns to hash -#}
+{%- else -%}
+    {%- set all_null = [] -%}
+
+    {%- if is_hashdiff -%}
+        {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
     {%- else -%}
-     {%- set all_null = [] -%}
+        {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- endif -%}
 
-        {%- if is_hashdiff -%}
-           {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- for column in columns -%}
+
+        {%- do all_null.append(null_placeholder_string) -%}
+
+        {%- set column_str = dbtvault.as_constant(column) -%}
+        {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
+        {{- "," if not loop.last -}}
+
+        {%- if loop.last -%}
+
+            {% if is_hashdiff %}
+                {{- "\n)) AS BINARY({})) AS {}".format(hash_size, alias) -}}
+            {%- else -%}
+                {{- "\n), '{}')) AS BINARY({})) AS {}".format(all_null | join(""), hash_size, alias) -}}
+            {%- endif -%}
         {%- else -%}
-            {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+
+            {%- do all_null.append(concat_string) -%}
+
         {%- endif -%}
 
-        {%- for column in columns -%}
-
-            {%- do all_null.append(null_placeholder_string) -%}
-
-            {%- set column_str = dbtvault.as_constant(column) -%}
-            {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
-            {{- "," if not loop.last -}}
-
-            {%- if loop.last -%}
-
-                {% if is_hashdiff %}
-                    {{- "\n)) AS BYTES) AS {}".format(alias) -}}
-                {%- else -%}
-                    {{- "\n), '{}')) AS BYTES) AS {}".format(all_null | join(""), alias) -}}
-                {%- endif -%}
-            {%- else -%}
-
-                {%- do all_null.append(concat_string) -%}
-
-            {%- endif -%}
-
-        {%- endfor -%}
-
-    {%- endif -%}
+    {%- endfor -%}
 
 {%- endif -%}
 
