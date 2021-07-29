5b0c510ae03762906f3aee336ee6e0850da31f5c
Jocelyn Shannon <jocelyn.shannon@data-vault.com>
2021-07-29 15:56:02 +0000

    WIP datetime conversion still not working

--- dbtvault-dev/macros/tables/pit.sql
+++ dbtvault-dev/macros/tables/pit.sql
@@ -135,8 +135,8 @@ backfill AS (
         {%- set sat_key_name = (satellites[sat_name]['pk'].keys() | list )[0] | upper -%}
         {%- set sat_ldts_name = (satellites[sat_name]['ldts'].keys() | list )[0] | upper -%}
         {%- set sat_name = sat_name | upper %}
-        {{ "'{}'::BINARY(16) AS {}".format(ghost_pk, sat_name, sat_key_name) }},
-        {{ "'{}'::TIMESTAMP_NTZ AS {}_{}".format(ghost_date, sat_name, sat_ldts_name) }}
+        {{ "CAST('{}' AS BYTES) AS {}".format(ghost_pk, sat_name, sat_key_name) }},
+        {{ "CAST('{}' AS DATETIME) AS {}_{}".format(ghost_date, sat_name, sat_ldts_name) }}
         {{- ',' if not loop.last -}}
     {%- endfor %}
     FROM backfill_rows_as_of_dates AS a
@@ -176,7 +176,7 @@ new_rows AS (
         {%- set sat_pk = satellites[sat_name]['pk'][sat_pk_name] -%}
         {%- set sat_ldts = satellites[sat_name]['ldts'][sat_ldts_name] %}
         {{ ("COALESCE(MAX({}_src.{}), CAST({} AS STRING)) AS {}_{}".format(sat_name | lower, sat_pk, ghost_pk, sat_name | upper, sat_pk_name | upper )) }},
-        {{ ("COALESCE(MAX({}_src.{}), CAST(PARSE_DATETIME('%y-%m-%d %H:%M:%S.%U', '{}') AS DATETIME)) AS {}_{}".format(sat_name | lower, sat_ldts, ghost_date, sat_name | upper, sat_ldts_name | upper)) }}
+        {{ ("COALESCE(MAX({}_src.{}), CAST('{}' AS DATETIME)) AS {}_{}".format(sat_name | lower, sat_ldts, ghost_date, sat_name | upper, sat_ldts_name | upper)) }}
         {{- "," if not loop.last }}
     {%- endfor %}
     FROM new_rows_as_of_dates AS a
