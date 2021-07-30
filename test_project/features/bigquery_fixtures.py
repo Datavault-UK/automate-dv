0d4f7e86d07053b5f0b4a32aec5563b384733467
ADattaDV <86599615+ADattaDV@users.noreply.github.com>
2021-07-30 10:16:23 +0000

    Feat/bigquery tlink (#40)
    * WIP connected to GCP, scenario 1: 4/5 passing
    
    * WIP expected seed invalid cast bytes > string
    
    * WIP s1 4/5 passing: customer_fk and order_fk still in bytes.
    
    * WIP 5/5 scenario 1 passing, fixed FK BYTES > STRING in hash
    
    * WIP scenario 1 and 2 passing; null in expanded_list macro removed.
    
    * Added _bigquery
    
    * All scenarios passing, removed log statements used for debugging.
    
    Co-authored-by: Aakash Datta <aakash.datta@data-vault.com>
    Co-authored-by: Alex Higgs <alex.higgs@data-vault.co.uk>

--- test_project/features/bigquery_fixtures.py
+++ test_project/features/bigquery_fixtures.py
@@ -478,4 +478,64 @@ def satellite_cycle_bigquery(context):
                 "SOURCE": "STRING"
             }
         }
+
+@fixture
+def t_link_bigquery(context):
+    """
+    Define the structures and metadata to load transactional links
+    """
+
+    context.hashed_columns = {
+        "STG_CUSTOMER": {
+            "TRANSACTION_PK": ["CUSTOMER_ID", "ORDER_ID", "TRANSACTION_NUMBER"],
+            "CUSTOMER_FK": "CUSTOMER_ID",
+            "ORDER_FK": "ORDER_ID"
+        }
+    }
+
+    context.derived_columns = {
+        "STG_CUSTOMER": {
+            "EFFECTIVE_FROM": "TRANSACTION_DATE"
+        }
+    }
+
+    context.vault_structure_columns = {
+        "T_LINK": {
+            "src_pk": "TRANSACTION_PK",
+            "src_fk": ["CUSTOMER_FK", "ORDER_FK"],
+            "src_payload": ["TRANSACTION_NUMBER", "TRANSACTION_DATE",
+                            "TYPE", "AMOUNT"],
+            "src_eff": "EFFECTIVE_FROM",
+            "src_ldts": "LOAD_DATE",
+            "src_source": "SOURCE"
+        }
+    }
+
+    context.seed_config = {
+        "RAW_STAGE": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "ORDER_ID": "STRING",
+                "TRANSACTION_NUMBER": "NUMERIC",
+                "TRANSACTION_DATE": "DATE",
+                "TYPE": "STRING",
+                "AMOUNT": "NUMERIC",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        },
+        "T_LINK": {
+            "+column_types": {
+                "TRANSACTION_PK": "BYTES",
+                "CUSTOMER_FK": "BYTES",
+                "ORDER_FK": "BYTES",
+                "TRANSACTION_NUMBER": "NUMERIC",
+                "TRANSACTION_DATE": "DATE",
+                "TYPE": "STRING",
+                "AMOUNT": "NUMERIC",
+                "EFFECTIVE_FROM": "DATE",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        }
     }
