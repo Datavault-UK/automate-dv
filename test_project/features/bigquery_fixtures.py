a6281bbd4e4973aeb029af1932590c5e798197ec
jossyshannon <86599636+jossyshannon@users.noreply.github.com>
2021-08-09 17:17:39 +0100

    Feat/bigquery xts (#45)
    * WIP actual hashdiff is different to expected
    
    * XTS tests passing!
    
    * Revert "Merge branch 'feat/bigquery' into feat/bigquery-xts"
    
    This reverts commit 4c92083d72ea8bbc9094c65c45292d382269f7f1.
    
    Co-authored-by: Jocelyn Shannon <jocelyn.shannon@data-vault.com>
    Co-authored-by: Alex Higgs <alex.higgs@data-vault.co.uk>

--- test_project/features/bigquery_fixtures.py
+++ test_project/features/bigquery_fixtures.py
@@ -3244,6 +3244,348 @@ def cycle(context):
         }
     }
 
+      
+@fixture
+def xts_bigquery(context):
+    """
+    Define the structures and metadata to load xts
+    """
+
+    context.vault_structure_type = "xts"
+
+    context.hashed_columns = {
+        "STG_CUSTOMER": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF": {"is_hashdiff": True,
+                         "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]}
+        },
+        "STG_CUSTOMER_1": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF": {"is_hashdiff": True,
+                         "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]}
+        },
+        "STG_CUSTOMER_2": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF": {"is_hashdiff": True,
+                         "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]}
+        },
+        "STG_CUSTOMER_2SAT": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF_1": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]},
+            "HASHDIFF_2": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_DOB", "CUSTOMER_PHONE"]}
+        },
+        "STG_CUSTOMER_2SAT_1": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF_1": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]},
+            "HASHDIFF_2": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_DOB", "CUSTOMER_PHONE"]}
+        },
+        "STG_CUSTOMER_2SAT_2": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF_1": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]},
+            "HASHDIFF_2": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_DOB", "CUSTOMER_PHONE"]}
+        },
+        "STG_CUSTOMER_3SAT": {
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "HASHDIFF_1": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME"]},
+            "HASHDIFF_2": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_DOB", "CUSTOMER_PHONE"]},
+            "HASHDIFF_3": {"is_hashdiff": True,
+                           "columns": ["CUSTOMER_ID", "CUSTOMER_COUNTY", "CUSTOMER_CITY"]}
+        }
+    }
+
+    context.derived_columns = {
+        "STG_CUSTOMER": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_NAME": "!SAT_CUSTOMER"
+        },
+        "STG_CUSTOMER_1": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_NAME": "!SAT_CUSTOMER"
+        },
+        "STG_CUSTOMER_2": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_NAME": "!SAT_CUSTOMER"
+        },
+        "STG_CUSTOMER_2SAT": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_1": "!SAT_CUSTOMER",
+            "SATELLITE_2": "!SAT_CUSTOMER_DETAILS",
+        },
+        "STG_CUSTOMER_2SAT_1": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_1": "!SAT_CUSTOMER",
+            "SATELLITE_2": "!SAT_CUSTOMER_DETAILS",
+        },
+        "STG_CUSTOMER_2SAT_2": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_1": "!SAT_CUSTOMER",
+            "SATELLITE_2": "!SAT_CUSTOMER_DETAILS",
+        },
+        "STG_CUSTOMER_3SAT": {
+            "EFFECTIVE_FROM": "LOAD_DATE",
+            "SATELLITE_1": "!SAT_CUSTOMER",
+            "SATELLITE_2": "!SAT_CUSTOMER_DETAILS",
+            "SATELLITE_3": "!SAT_CUSTOMER_LOCATION",
+        }
+    }
+
+    context.vault_structure_columns = {
+        "XTS": {
+            "src_pk": "CUSTOMER_PK",
+            "src_ldts": "LOAD_DATE",
+            "src_satellite": {
+                "SATELLITE_CUSTOMER": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_NAME"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF"
+                    }
+                },
+            },
+            "src_source": "SOURCE"
+        },
+        "XTS_2SAT": {
+            "src_pk": "CUSTOMER_PK",
+            "src_ldts": "LOAD_DATE",
+            "src_satellite": {
+                "SATELLITE_CUSTOMER": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_1"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF_1"
+                    }
+                },
+                "SATELLITE_CUSTOMER_DETAILS": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_2"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF_2"
+                    }
+                }
+            },
+            "src_source": "SOURCE"
+        },
+        "XTS_3SAT": {
+            "src_pk": "CUSTOMER_PK",
+            "src_ldts": "LOAD_DATE",
+            "src_satellite": {
+                "SATELLITE_CUSTOMER": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_1"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF_1"
+                    }
+                },
+                "SATELLITE_CUSTOMER_DETAILS": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_2"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF_2"
+                    }
+                },
+                "SATELLITE_CUSTOMER_LOCATION": {
+                    "sat_name": {
+                        "SATELLITE_NAME": "SATELLITE_3"
+                    },
+                    "hashdiff": {
+                        "HASHDIFF": "HASHDIFF_3"
+                    }
+                }
+            },
+            "src_source": "SOURCE"
+        }
+    }
+
+    context.seed_config = {
+        "RAW_STAGE": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_1": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_2": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_2SAT": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_2SAT_1": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_2SAT_2": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "RAW_STAGE_3SAT": {
+            "+column_types": {
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING",
+            }
+        },
+        "STG_CUSTOMER": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "HASHDIFF": "STRING",
+                "EFFECTIVE_FROM": "DATE",
+                "SATELLITE_NAME": "STRING",
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        },
+        "STG_CUSTOMER_2SAT": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "HASHDIFF_1": "STRING",
+                "HASHDIFF_2": "STRING",
+                "EFFECTIVE_FROM": "DATE",
+                "SATELLITE_1": "STRING",
+                "SATELLITE_2": "STRING",
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        },
+        "STG_CUSTOMER_3SAT": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "HASHDIFF_1": "STRING",
+                "HASHDIFF_2": "STRING",
+                "HASHDIFF_3": "STRING",
+                "EFFECTIVE_FROM": "DATE",
+                "SATELLITE_1": "STRING",
+                "SATELLITE_2": "STRING",
+                "SATELLITE_3": "STRING",
+                "CUSTOMER_ID": "STRING",
+                "CUSTOMER_FIRSTNAME": "STRING",
+                "CUSTOMER_LASTNAME": "STRING",
+                "CUSTOMER_DOB": "DATE",
+                "CUSTOMER_PHONE": "STRING",
+                "CUSTOMER_COUNTY": "STRING",
+                "CUSTOMER_CITY": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        },
+        "XTS": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "LOAD_DATE": "DATE",
+                "SATELLITE_NAME": "STRING",
+                "HASHDIFF": "STRING",
+                "SOURCE": "STRING"
+            }
+        },
+        "XTS_2SAT": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "SATELLITE_NAME": "STRING",
+                "HASHDIFF": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        },
+        "XTS_3SAT": {
+            "+column_types": {
+                "CUSTOMER_PK": "STRING",
+                "SATELLITE_NAME": "STRING",
+                "HASHDIFF": "STRING",
+                "LOAD_DATE": "DATE",
+                "SOURCE": "STRING"
+            }
+        }
+    }
 
 @fixture
 def enable_auto_end_date(context):
