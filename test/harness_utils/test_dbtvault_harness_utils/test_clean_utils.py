31bcc998e069967de4d23427813940a54575e179
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-08-12 09:38:42 +0000

    Unit tests for clean_* functions

--- test/harness_utils/test_dbtvault_harness_utils/test_clean_utils.py
+++ test/harness_utils/test_dbtvault_harness_utils/test_clean_utils.py
@@ -1,56 +1,44 @@
-import tempfile
+import os
 from pathlib import Path
 from unittest.mock import patch
 
-import pytest
+from harness_utils import dbtvault_harness_utils
 
 
-@pytest.fixture()
-def sample_directory_tree(tmp_path):
-    directory_dict = {
-        'csv': {
-            'my_file_1.csv': "my, csv, file, 2",
-            'my_file_2.csv': "my, csv, file, 3",
-        },
-        'sql': {
-            'my_file_1.sql': "SELECT * FROM 1",
-            'my_file_2.sql': "SELECT * FROM 2",
-        },
-        'target': {
-            'my_file_1.sql': "SELECT * FROM 1",
-            'my_file_2.sql': "SELECT * FROM 2",
-        }
-    }
+def test_clean_csv_success(sample_directory_tree):
+    paths, tmp_dir = sample_directory_tree
 
-    paths = dict()
+    tmp_csv_dir = Path(tmp_dir) / 'csv'
 
-    # Generate directory structure from the above dict
-    for k, v in directory_dict.items():
-        full_path = Path(tmp_path) / k
-        Path(full_path).mkdir(parents=True)
-        if isinstance(v, dict):
-            paths[k] = list()
-            for ik, iv in v.items():
-                if '.' in ik:
-                    file_name, ext = ik.split('.')
-                    file = tempfile.NamedTemporaryFile(prefix=file_name, suffix=f'.{ext}', dir=full_path, delete=False)
-                    full_file_path = Path(full_path) / file.name
-                    paths[k].append(full_file_path)
-                    with open(full_file_path, 'w+') as f:
-                        f.write(iv)
-    return paths
+    assert set(os.listdir(tmp_csv_dir)) == {path.name for path in paths['csv']}
+
+    with patch('test.CSV_DIR', tmp_csv_dir):
+        dbtvault_harness_utils.clean_csv()
+
+    assert not os.listdir(tmp_csv_dir)
 
 
-def test_clean_csv_success(tmp_path, sample_directory_tree):
-    
-    with patch('test.INVOKE_YML_FILE', Path(file.name)):
-        sample_directory_tree['csv']
-    assert False
+def test_clean_target_success(sample_directory_tree):
+    paths, tmp_dir = sample_directory_tree
+
+    tmp_target_dir = Path(tmp_dir) / 'target'
+
+    assert set(os.listdir(tmp_target_dir)) == {path.name for path in paths['target']}
+
+    with patch('test.TESTS_DBT_ROOT', tmp_dir):
+        dbtvault_harness_utils.clean_target()
+
+    assert 'target' not in os.listdir(tmp_dir)
 
 
-def test_clean_target_success(tmp_path):
-    assert False
+def test_clean_models_success(sample_directory_tree):
+    paths, tmp_dir = sample_directory_tree
 
+    tmp_models_dir = Path(tmp_dir) / 'models'
 
-def test_clean_models_success(tmp_path):
-    assert False
+    assert set(os.listdir(tmp_models_dir)) == {path.name for path in paths['models']}
+
+    with patch('test.FEATURE_MODELS_ROOT', tmp_models_dir):
+        dbtvault_harness_utils.clean_models()
+
+    assert not os.listdir(tmp_models_dir)
