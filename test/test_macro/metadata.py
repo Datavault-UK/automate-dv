af62b86da06328234fd36726a58ea45c84d77baa
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-08-13 10:48:09 +0000

    All Link macro tests working

--- test/test_macro/metadata.py
+++ test/test_macro/metadata.py
@@ -32,3 +32,22 @@ def multi_source_multi_nk_hub() -> dict:
                         "CUSTOMER_NAME"],
                 src_ldts="LOADDATE",
                 src_source="RECORD_SOURCE")
+
+
+def single_source_link() -> dict:
+    return dict(source_model="raw_source",
+                src_pk="CUSTOMER_PK",
+                src_fk=["ORDER_FK",
+                        "BOOKING_FK"],
+                src_ldts="LOADDATE",
+                src_source="RECORD_SOURCE")
+
+
+def multi_source_link() -> dict:
+    return dict(source_model=["raw_source",
+                              "raw_source_2"],
+                src_pk="CUSTOMER_PK",
+                src_fk=["ORDER_FK",
+                        "BOOKING_FK"],
+                src_ldts="LOADDATE",
+                src_source="RECORD_SOURCE")
