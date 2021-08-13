453c5949c6c4f36f6aed9593f33fe801a18d7e51
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-08-13 16:17:40 +0000

    WIP

--- test/test_macro/staging/test_derive_columns.py
+++ test/test_macro/staging/test_derive_columns.py
@@ -1,39 +1,41 @@
 import pytest
-from unittest import TestCase
+
+import dbtvault_harness_utils
+
+macro_name = "derive_columns"
 
 
-# @pytest.mark.usefixtures('dbt_test_utils', 'clean_database', 'run_seeds')
-# class TestDeriveColumnsMacro(TestCase):
-#     maxDiff = None
-#
-#     def test_derive_columns_correctly_generates_sql_with_source_columns(self):
-#         var_dict = {'source_model': 'raw_source', 'columns': {'SOURCE': "!STG_BOOKING", 'EFFECTIVE_FROM': 'LOADDATE'}}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_derive_columns_correctly_generates_sql_without_source_columns(self):
-#         var_dict = {'columns': {'SOURCE': "!STG_BOOKING", 'LOADDATE': 'EFFECTIVE_FROM'}}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_derive_columns_raises_error_with_only_source_columns(self):
-#         var_dict = {'source_model': 'raw_source'}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_names=[self.current_test_name],
-#                                                           args=var_dict)
-#
-#         warning_message = "Invalid column configuration:"
-#
-#         assert warning_message in process_logs
+@pytest.mark.macro
+def test_derive_columns_correctly_generates_sql_with_source_columns(request):
+    var_dict = {'source_model': 'raw_source', 'columns': {'SOURCE': "!STG_BOOKING", 'EFFECTIVE_FROM': 'LOADDATE'}}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_derive_columns_correctly_generates_sql_without_source_columns(request):
+    var_dict = {'columns': {'SOURCE': "!STG_BOOKING", 'LOADDATE': 'EFFECTIVE_FROM'}}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_derive_columns_raises_error_with_only_source_columns(request):
+    var_dict = {'source_model': 'raw_source'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+
+    assert "Invalid column configuration:" in dbt_logs
--- test/test_macro/staging/test_hash_columns.py
+++ test/test_macro/staging/test_hash_columns.py
@@ -1,19 +1,53 @@
 import pytest
-from unittest import TestCase
+
+import dbtvault_harness_utils
+
+macro_name = "hash_columns"
 
 
-# @pytest.mark.usefixtures('dbt_test_utils', 'clean_database')
-# class TestHashColumnsMacro(TestCase):
-#     maxDiff = None
-#
-#     def test_hash_columns_correctly_generates_hashed_columns_for_single_columns(self):
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_names=[self.current_test_name])
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
+def pytest_configure():
+    pytest.metadata_dict = dict()
+
+
+@pytest.mark.macro
+def test_hash_columns_correctly_generates_hashed_columns_for_single_columns(request):
+    var_dict = {
+        "columns": {
+            "BOOKING_PK": "BOOKING_REF",
+            "CUSTOMER_PK": "CUSTOMER_ID",
+            "CUSTOMER_BOOKING_PK": [
+                "CUSTOMER_ID",
+                "BOOKING_REF"
+            ],
+            "BOOK_CUSTOMER_HASHDIFF": {
+                "is_hashdiff": True,
+                "columns": [
+                    "PHONE",
+                    "NATIONALITY",
+                    "CUSTOMER_ID"
+                ]
+            },
+            "BOOK_BOOKING_HASHDIFF": {
+                "is_hashdiff": True,
+                "columns": [
+                    "BOOKING_REF",
+                    "BOOKING_DATE",
+                    "DEPARTURE_DATE"
+                    "PRICE",
+                    "DESTINATION"
+                ]
+            }
+        }
+    }
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
 #
 #     def test_hash_columns_correctly_generates_hashed_columns_for_composite_columns_hashdiff(self):
 #
