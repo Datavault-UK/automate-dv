eeddf761e71ab1c8a160ffb640a93a29d77d245c
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-08-13 13:29:16 +0000

    Prefix macro tests working

--- test/test_macro/supporting/test_prefix.py
+++ test/test_macro/supporting/test_prefix.py
@@ -1,77 +1,93 @@
 import pytest
-from unittest import TestCase
+
+import dbtvault_harness_utils
+
+macro_name = "prefix"
 
 
-# @pytest.mark.usefixtures('dbt_test_utils', 'clean_database')
-# class TestPrefixMacro(TestCase):
-#     maxDiff = None
-#
-#     def test_prefix_column_in_single_item_list_is_successful(self):
-#         var_dict = {'columns': ["CUSTOMER_HASHDIFF"], 'prefix': 'c'}
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_prefix_multiple_columns_is_successful(self):
-#         var_dict = {'columns': ["CUSTOMER_HASHDIFF", 'CUSTOMER_PK', 'LOADDATE', 'SOURCE'], 'prefix': 'c'}
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_prefix_aliased_column_is_successful(self):
-#         columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
-#         var_dict = {'columns': columns, 'prefix': 'c'}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_prefix_aliased_column_with_alias_target_as_source_is_successful(self):
-#         columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
-#         var_dict = {'columns': columns, 'prefix': 'c', 'alias_target': 'source'}
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_prefix_aliased_column_with_alias_target_as_target_is_successful(self):
-#         columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
-#         var_dict = {'columns': columns, 'prefix': 'c', 'alias_target': 'target'}
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#         actual_sql = self.dbt_test_utils.retrieve_compiled_model(self.current_test_name)
-#         expected_sql = self.dbt_test_utils.retrieve_expected_sql(self.current_test_name)
-#
-#         assert 'Done' in process_logs
-#         assert 'SQL compilation error' not in process_logs
-#         self.assertEqual(expected_sql, actual_sql)
-#
-#     def test_prefix_with_no_columns_raises_error(self):
-#         var_dict = {'prefix': 'c'}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#
-#         assert "Invalid parameters provided to prefix macro. Expected: " \
-#                "(columns [list/string], prefix_str [string]) got: (None, c)" in process_logs
-#
-#     def test_prefix_with_empty_column_list_raises_error(self):
-#         var_dict = {'columns': [], 'prefix': 'c'}
-#
-#         process_logs = self.dbt_test_utils.run_dbt_models(model_name=[self.current_test_name], args=var_dict)
-#
-#         assert "Invalid parameters provided to prefix macro. Expected: " \
-#                "(columns [list/string], prefix_str [string]) got: ([], c)" in process_logs
+@pytest.mark.macro
+def test_prefix_column_in_single_item_list_is_successful(request):
+    var_dict = {'columns': ["CUSTOMER_HASHDIFF"], 'prefix': 'c'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_prefix_multiple_columns_is_successful(request):
+    var_dict = {'columns': ["CUSTOMER_HASHDIFF", 'CUSTOMER_PK', 'LOADDATE', 'SOURCE'], 'prefix': 'c'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_prefix_aliased_column_is_successful(request):
+    columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
+    var_dict = {'columns': columns, 'prefix': 'c'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_prefix_aliased_column_with_alias_target_as_source_is_successful(request):
+    columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
+    var_dict = {'columns': columns, 'prefix': 'c', 'alias_target': 'source'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_prefix_aliased_column_with_alias_target_as_target_is_successful(request):
+    columns = [{"source_column": "CUSTOMER_HASHDIFF", "alias": "HASHDIFF"}, "CUSTOMER_PK", "LOADDATE"]
+    var_dict = {'columns': columns, 'prefix': 'c', 'alias_target': 'target'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert expected_sql == actual_sql
+
+
+@pytest.mark.macro
+def test_prefix_with_no_columns_raises_error(request):
+    var_dict = {'prefix': 'c', 'columns': []}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name], args=var_dict)
+
+    assert "Invalid parameters provided to prefix macro. Expected: " \
+           "(columns [list/string], prefix_str [string]) got: ([], c)" in dbt_logs
+
+
+@pytest.mark.macro
+def test_prefix_with_empty_column_list_raises_error(request):
+    var_dict = {'columns': [], 'prefix': 'c'}
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name], args=var_dict)
+
+    assert "Invalid parameters provided to prefix macro. Expected: " \
+           "(columns [list/string], prefix_str [string]) got: ([], c)" in dbt_logs
