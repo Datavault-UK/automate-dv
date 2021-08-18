ad0b6718e8b0bfb69731a00e55a2b5256b3779a3
Alex Higgs <alex.higgs@data-vault.co.uk>
2021-08-18 16:19:03 +0000

    Removed un-testable test

--- test/features/steps/insert_by_period_steps.py
+++ test/features/steps/insert_by_period_steps.py
@@ -33,34 +33,6 @@ def load_table(context, model_name, vault_structure, period, start_date, stop_da
     assert "Completed successfully" in logs
 
 
-@step("I insert by period into the {model_name} {vault_structure} "
-      "by {period} with start date: {start_date}")
-def load_table(context, model_name, vault_structure, period, start_date=None):
-    metadata = {"source_model": context.processed_stage_name,
-                **context.vault_structure_columns[model_name]}
-
-    config = {"materialized": "vault_insert_by_period",
-              "timestamp_field": "LOAD_DATE",
-              "start_date": start_date,
-              "period": period}
-
-    config = dbtvault_generator.append_end_date_config(context, config)
-
-    context.vault_structure_metadata = metadata
-
-    dbtvault_generator.raw_vault_structure(model_name=model_name,
-                                           vault_structure=vault_structure,
-                                           config=config,
-                                           **metadata)
-
-    is_full_refresh = dbtvault_harness_utils.is_full_refresh(context)
-
-    logs = dbtvault_harness_utils.run_dbt_models(mode="run", model_names=[model_name],
-                                                 full_refresh=is_full_refresh)
-
-    assert "Completed successfully" in logs
-
-
 @step("I insert by period into the {model_name} {vault_structure} by {period}")
 def load_table(context, model_name, vault_structure, period):
     metadata = {"source_model": context.processed_stage_name,
