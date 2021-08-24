3244b737660d9e6309f2ccbecdacdbc98eb978ed
Alex Higgs <alex.higgs@data-vault.com>
2021-08-24 19:01:05 +0000

    Fix feature files SAT-PM-B
    Fix stage name
    
    
    Fix
    
    
    Fix

--- test/features/steps/insert_by_period_steps.py
+++ test/features/steps/insert_by_period_steps.py
@@ -58,3 +58,30 @@ def load_table(context, model_name, vault_structure, period):
                                                  full_refresh=is_full_refresh)
 
     assert "Completed successfully" in logs
+
+
+@step("I insert by period starting from {start_date} by {period} into the {model_name} {vault_structure}")
+def load_table(context, start_date, period, model_name, vault_structure):
+    metadata = {"source_model": context.processed_stage_name,
+                **context.vault_structure_columns[model_name]}
+
+    config = {"materialized": "vault_insert_by_period",
+              "timestamp_field": "LOAD_DATE",
+              "start_date": start_date,
+              "period": period}
+
+    config = dbtvault_generator.append_end_date_config(context, config)
+
+    context.vault_structure_metadata = metadata
+
+    dbtvault_generator.raw_vault_structure(model_name=model_name,
+                                           vault_structure=vault_structure,
+                                           config=config,
+                                           **metadata)
+
+    is_full_refresh = dbtvault_harness_utils.is_full_refresh(context)
+
+    logs = dbtvault_harness_utils.run_dbt_models(mode="run", model_names=[model_name],
+                                                 full_refresh=is_full_refresh)
+
+    assert "Completed successfully" in logs
