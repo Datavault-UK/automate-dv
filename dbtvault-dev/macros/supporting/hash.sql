81733f2bca564db127f3758be97cd20008f293f1
Nikka Zanandrea <nikka.zanandrea@nhs.net>
2021-08-27 13:40:02 +0100

    spark on hash, hub, link, sat

--- dbtvault-dev/macros/supporting/hash.sql
+++ dbtvault-dev/macros/supporting/hash.sql
@@ -205,3 +205,71 @@
 {%- endif -%}
 
 {%- endmacro -%}
+
+{%- macro spark__hash(columns, alias, is_hashdiff) -%}
+
+{%- set concat_string = "||" -%}
+{%- set null_placeholder_string = "^^" -%}
+
+{%- set hash = var('hash', 'MD5') -%}
+
+{#- Select hashing algorithm -#}
+{%- if hash == 'MD5' -%}
+ {%- set hash_alg = 'MD5' -%}
+ {%- set hash_size = 16 -%}
+{%- elif hash == 'SHA' -%}
+ {%- set hash_alg = 'SHA2' -%}
+ {%- set hash_size = 32 -%}
+{%- else -%}
+ {%- set hash_alg = 'MD5' -%}
+ {%- set hash_size = 16 -%}
+{%- endif -%}
+
+
+{%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
+
+{#- Alpha sort columns before hashing if a hashdiff -#}
+{%- if is_hashdiff and dbtvault.is_list(columns) -%}
+ {%- set columns = columns|sort -%}
+{%- endif -%}
+
+{#- If single column to hash -#}
+{%- if columns is string -%}
+ {%- set column_str = dbtvault.as_constant(columns) -%}
+ {{- "CAST(({}({})) AS STRING) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), alias) | indent(4) -}}
+
+{#- Else a list of columns to hash -#}
+{%- else -%}
+ {%- set all_null = [] -%}
+
+ {%- if is_hashdiff -%}
+     {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+ {%- else -%}
+     {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+ {%- endif -%}
+
+ {%- for column in columns -%}
+
+     {%- do all_null.append(null_placeholder_string) -%}
+
+     {%- set column_str = dbtvault.as_constant(column) -%}
+     {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
+     {{- "," if not loop.last -}}
+
+     {%- if loop.last -%}
+         {% if is_hashdiff %}
+             {{- "\n)) AS STRING) AS {}".format(alias) -}}
+         {%- else -%}
+             {{- "\n), '{}')) AS STRING) AS {}".format(all_null | join(""), alias) -}}
+         {%- endif -%}
+     {%- else -%}
+
+         {%- do all_null.append(concat_string) -%}
+
+     {%- endif -%}
+
+ {%- endfor -%}
+
+{%- endif -%}
+
+{%- endmacro -%}
--- dbtvault-dev/macros/tables/hub.sql
+++ dbtvault-dev/macros/tables/hub.sql
@@ -316,3 +316,116 @@ records_to_insert AS (
 SELECT * FROM records_to_insert
 
 {%- endmacro -%}
+
+{%- macro spark__hub(src_pk, src_nk, src_ldts, src_source, source_model) -%}
+
+{{- dbtvault.check_required_parameters(src_pk=src_pk, src_nk=src_nk,
+                                       src_ldts=src_ldts, src_source=src_source,
+                                       source_model=source_model) -}}
+
+{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_nk, src_ldts, src_source]) -%}
+
+{%- if model.config.materialized == 'vault_insert_by_rank' %}
+    {%- set source_cols_with_rank = source_cols + [config.get('rank_column')] -%}
+{%- endif -%}
+
+{{ dbtvault.prepend_generated_by() }}
+
+{{ 'WITH ' -}}
+
+{%- if not (source_model is iterable and source_model is not string) -%}
+    {%- set source_model = [source_model] -%}
+{%- endif -%}
+
+{%- set ns = namespace(last_cte= "") -%}
+
+{%- for src in source_model -%}
+
+{%- set source_number = loop.index | string -%}
+
+row_rank_{{ source_number }}_{{ source_number }} AS (
+    {%- if model.config.materialized == 'vault_insert_by_rank' %}
+    SELECT {{ source_cols_with_rank | join(', ') }},
+    {%- else %}
+    SELECT {{ source_cols | join(', ') }},
+    {%- endif %}
+           ROW_NUMBER() OVER(
+               PARTITION BY {{ src_pk }}
+               ORDER BY {{ src_ldts }}
+           ) AS row_number
+    FROM {{ ref(src) }}
+    WHERE {{ dbtvault.multikey(src_pk, condition='IS NOT NULL') }}
+    {%- set ns.last_cte = "row_rank_{}_{}".format(source_number, source_number) %}
+),
+row_rank_{{ source_number }} AS (
+    {%- if model.config.materialized == 'vault_insert_by_rank' %}
+    SELECT {{ source_cols_with_rank | join(', ') }},
+    {%- else %}
+    SELECT {{ source_cols | join(', ') }},
+    {%- endif %}
+    row_number
+    FROM {{ ns.last_cte }}
+    WHERE {{ dbtvault.multikey(src_pk, condition='IS NOT NULL') }}
+    AND row_number = 1
+    {%- set ns.last_cte = "row_rank_{}".format(source_number) %}
+),
+{{ "\n" if not loop.last }}
+{% endfor -%}
+{% if source_model | length > 1 %}
+stage_union AS (
+    {%- for src in source_model %}
+    SELECT * FROM row_rank_{{ loop.index | string }}
+    {%- if not loop.last %}
+    UNION ALL
+    {%- endif %}
+    {%- endfor %}
+    {%- set ns.last_cte = "stage_union" %}
+),
+{%- endif -%}
+{%- if model.config.materialized == 'vault_insert_by_period' %}
+stage_mat_filter AS (
+    SELECT *
+    FROM {{ ns.last_cte }}
+    WHERE __PERIOD_FILTER__
+    {%- set ns.last_cte = "stage_mat_filter" %}
+),
+{%- elif model.config.materialized == 'vault_insert_by_rank' %}
+stage_mat_filter AS (
+    SELECT *
+    FROM {{ ns.last_cte }}
+    WHERE __RANK_FILTER__
+    {%- set ns.last_cte = "stage_mat_filter" %}
+),
+{%- endif -%}
+{%- if source_model | length > 1 %}
+
+row_rank_union_row_number AS (
+    SELECT *,
+           ROW_NUMBER() OVER(
+               PARTITION BY {{ src_pk }}
+               ORDER BY {{ src_ldts }}, {{ src_source }} ASC
+           ) AS row_rank_number
+    FROM {{ ns.last_cte }}
+    WHERE {{ dbtvault.multikey(src_pk, condition='IS NOT NULL') }}
+    {%- set ns.last_cte = "row_rank_union_row_number" %}
+),
+row_rank_union AS (
+    SELECT *
+    FROM {{ ns.last_cte }}
+    WHERE row_rank_number = 1
+    {%- set ns.last_cte = "row_rank_union" %}
+),
+{% endif %}
+records_to_insert AS (
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='target') }}
+    FROM {{ ns.last_cte }} AS a
+    {%- if dbtvault.is_any_incremental() %}
+    LEFT JOIN {{ this }} AS d
+    ON a.{{ src_pk }} = d.{{ src_pk }}
+    WHERE {{ dbtvault.prefix([src_pk], 'd') }} IS NULL
+    {%- endif %}
+)
+
+SELECT * FROM records_to_insert
+
+{%- endmacro -%}
