3cecf59e0876143a1a5a51d9e37f46ae9ff72bed
Alex Higgs <alex.higgs@data-vault.com>
2021-09-20 18:48:26 +0000

    Removed NODE_INDEX from variables
    Setup variables

--- test/dbtvault_test/macros/generate_schema_name.sql
+++ test/dbtvault_test/macros/generate_schema_name.sql
@@ -9,7 +9,7 @@
 
 {% macro default__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, pipeline_string) -%}
+    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault.pipeline_string()) -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -17,7 +17,7 @@
 
 {%- macro bigquery__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.dataset, target.project, pipeline_string)  -%}
+    {%- set schema_name = "{}_{}{}".format(target.dataset, target.project, dbtvault.pipeline_string())  -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -25,7 +25,7 @@
 
 {%- macro sqlserver__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, pipeline_string) -%}
+    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault.pipeline_string()) -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -38,14 +38,13 @@
 {%- endmacro -%}
 
 
-{%- macro append_pipeline_string() -%}
+{%- macro pipeline_string() -%}
 
-    {%- set pipeline_string -%}
+    {%- set pipeline_str -%}
         {{- '_' ~ env_var('PIPELINE_BRANCH', '') | replace('-','_') | replace('.','_') | replace('/','_') if env_var('PIPELINE_BRANCH', '') -}}
         {{- '_' ~ env_var('PIPELINE_JOB', '') if env_var('PIPELINE_JOB', '') -}}
-        {{- '_' ~ env_var('PIPELINE_NODE_INDEX', '') if env_var('PIPELINE_NODE_INDEX', '') -}}
     {%- endset -%}
 
-    {% do return(pipeline_string) %}
+    {% do return(pipeline_str) %}
 
 {%- endmacro -%}
