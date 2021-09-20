79b98a38de40a398846ffa9345a8d0732a592364
Alex Higgs <alex.higgs@data-vault.com>
2021-09-20 20:59:34 +0000

    Fix pipeline
    Fix schema.yml
    
    
    Fix pipeline config
    
    
    Fix
    
    
    Fixes?
    
    
    Fix?
    
    
    Fix
    
    
    Experiment
    
    
    Experiment
    
    
    
    
    Uncomment
    
    
    Fix job names
    
    
    Fix env vars

--- test/dbtvault_test/macros/generate_schema_name.sql
+++ test/dbtvault_test/macros/generate_schema_name.sql
@@ -9,7 +9,7 @@
 
 {% macro default__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault.pipeline_string()) -%}
+    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault_test.pipeline_string()) -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -17,7 +17,7 @@
 
 {%- macro bigquery__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.dataset, target.project, dbtvault.pipeline_string())  -%}
+    {%- set schema_name = "{}_{}{}".format(target.dataset, target.project, dbtvault_test.pipeline_string())  -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -25,7 +25,7 @@
 
 {%- macro sqlserver__get_schema_name() -%}
 
-    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault.pipeline_string()) -%}
+    {%- set schema_name = "{}_{}{}".format(target.schema, target.user, dbtvault_test.pipeline_string()) -%}
 
     {% do return(clean_schema_name(schema_name)) %}
 
@@ -33,7 +33,7 @@
 
 {%- macro clean_schema_name(schema_name) -%}
 
-    {%- do return(schema_name | replace('-','_') | replace('.','_') | replace('/','_')) -%}
+    {%- do return(schema_name | replace('-','_') | replace('.','_') | replace('/','_') | upper) -%}
 
 {%- endmacro -%}
 
@@ -41,10 +41,10 @@
 {%- macro pipeline_string() -%}
 
     {%- set pipeline_str -%}
-        {{- '_' ~ env_var('PIPELINE_BRANCH', '') | replace('-','_') | replace('.','_') | replace('/','_') if env_var('PIPELINE_BRANCH', '') -}}
-        {{- '_' ~ env_var('PIPELINE_JOB', '') if env_var('PIPELINE_JOB', '') -}}
+        {{- '_' ~ env_var('PIPELINE_BRANCH', '') | replace('-','_') | replace('.','_') | replace('/','_') | replace(' ','_') if env_var('PIPELINE_BRANCH', '') -}}
+        {{- '_' ~ env_var('PIPELINE_JOB', '') | replace('-','_') | replace('.','_') | replace('/','_') | replace(' ','_') if env_var('PIPELINE_JOB', '') -}}
     {%- endset -%}
 
-    {% do return(pipeline_str) %}
+    {% do return(pipeline_str | upper) %}
 
 {%- endmacro -%}
