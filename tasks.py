29729dfe444ecf1e8c2344a9c40f17a1ce7a0cac
Alex Higgs <alex.higgs@data-vault.com>
2021-09-22 13:43:15 +0000

    Split eff sat features
    Move sub_type dictionary to utils file

--- tasks.py
+++ tasks.py
@@ -293,78 +293,7 @@ def run_integration_tests(c, structures=None, subtype=None, platform=None, disab
     feature_directories = {'staging', 'hubs', 'links', 't_links', 'sats', 'sats_with_oos', 'eff_sats',
                            'ma_sats', 'xts', 'bridge', 'pit', 'cycle'}
 
-    sub_types = {
-        'sats': {
-            'main': [
-                'sats',
-
-            ],
-            'cycles': [
-                'sats_cycles'
-            ],
-            'pm_standard': [
-                'sats_period_mat_base',
-                'sats_period_mat_other'
-            ],
-            'pm_ranges': [
-                'sats_period_mat_inferred_range',
-                'sats_period_mat_provided_range'
-            ],
-            'rank': [
-                'sats_rank_mat'
-            ]
-        },
-        'ma_sats': {
-            '1cdk': [
-                'mas_one_cdk_0_base',
-                'mas_one_cdk_1_inc',
-                'mas_one_cdk_base_sats'
-            ],
-            '1cdk_cycles': [
-                'mas_one_cdk_base_sats_cycles',
-                'mas_one_cdk_cycles_duplicates'
-            ],
-            '2cdk': [
-                'mas_two_cdk_0_base',
-                'mas_two_cdk_1_inc',
-                'mas_two_cdk_base_sats'
-            ],
-            '2cdk_cycles': [
-                'mas_two_cdk_base_sats_cycles',
-                'mas_two_cdk_cycles_duplicates'
-            ],
-            'pm': [
-                'mas_period_mat'
-            ]
-        },
-        'pit': {
-            'main': [
-                'pit'
-            ],
-            '1sat': [
-                'pit_one_sat_base',
-                'pit_one_sat_inc'
-            ],
-            '2sat': [
-                'pit_two_sat_base',
-                'pit_two_sat_inc'
-            ]
-        },
-        'bridge': {
-            'inc': [
-                'bridge_incremental'
-            ],
-            '1link': [
-                'bridge_one_hub_one_link'
-            ],
-            '2link': [
-                'bridge_one_hub_two_links'
-            ],
-            '3link': [
-                'bridge_one_hub_three_links'
-            ]
-        },
-    }
+    sub_types = dbtvault_harness_utils.feature_sub_types()
 
     structures = set(str(structures).split(","))
 
