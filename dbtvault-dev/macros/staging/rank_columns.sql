0a10757500f59ae697f13bf0eb74ba8b8f389f6f
Alex Higgs <alex.higgs@data-vault.com>
2021-09-30 15:49:59 +0000

    Re-format/cleanup macro

--- dbtvault-dev/macros/staging/rank_columns.sql
+++ dbtvault-dev/macros/staging/rank_columns.sql
@@ -12,29 +12,31 @@
 
         {%- if columns[col] is mapping and columns[col].partition_by and columns[col].order_by -%}
 
-            {%- if dbtvault.is_nothing(columns[col].dense_rank) %}
+            {% set order_by = columns[col].order_by %}
+            {% set partition_by = columns[col].partition_by %}
+            {% set dense_rank = columns[col].dense_rank %}
+
+            {%- if dbtvault.is_nothing(dense_rank) %}
                 {%- set rank_type = "RANK()" -%}
-            {%- elif columns[col].dense_rank is true -%}
+            {%- elif dense_rank is true -%}
                 {%- set rank_type = "DENSE_RANK()" -%}
             {%- else -%}
                 {%- if execute -%}
-                    {%- do exceptions.raise_compiler_error('If dense_rank is provided, it must be true or false, not {}'.format(columns[col].dense_rank)) -%}
+                    {%- do exceptions.raise_compiler_error('If dense_rank is provided, it must be true or false, not {}'.format(dense_rank)) -%}
                 {% endif %}
             {%- endif -%}
-
-            {% set order_by = columns[col].order_by %}
 
             {%- if dbtvault.is_list(order_by) -%}
 
                 {%- if order_by is mapping %}
                     {%- set column_name, direction = order_by.items()|first -%}
-
+                    {%- set order_by_str = "{} {}".format(column_name, direction) | trim | join(", ") | trim -%}
                 {%- else -%}
                     {%- set column_name = order_by -%}
                     {%- set direction = '' -%}
+                    {%- set order_by_str = order_by | join(", ") -%}
                 {%- endif -%}
 
-                {%- set order_by_str = "{} {}".format(column_name, direction) | join(", ") | trim -%}
             {%- else -%}
 
                 {%- if order_by is mapping %}
@@ -47,10 +49,10 @@
                 {%- set order_by_str = "{} {}".format(column_name, direction) | trim -%}
             {%- endif -%}
 
-            {%- if dbtvault.is_list(columns[col].partition_by) -%}
-                {%- set partition_by_str = columns[col].partition_by | join(", ") -%}
+            {%- if dbtvault.is_list(partition_by) -%}
+                {%- set partition_by_str = partition_by | join(", ") -%}
             {%- else -%}
-                {%- set partition_by_str = columns[col].partition_by -%}
+                {%- set partition_by_str = partition_by -%}
             {%- endif -%}
 
             {{- "{} OVER (PARTITION BY {} ORDER BY {}) AS {}".format(rank_type, partition_by_str, order_by_str, col) | indent(4) -}}
