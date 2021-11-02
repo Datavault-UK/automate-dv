9ebc41b7ce7f3cf5db69cb8586406db978b2f3bb
Borna Almasi <borna@georgian.io>
2021-11-02 16:03:32 -0400

    WIP

--- macros/tables/sat.sql
+++ macros/tables/sat.sql
@@ -1,4 +1,5 @@
-{%- macro bigquery__sat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model, out_of_sequence) -%}
+
+{%- macro bigquery__sat2(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model, out_of_sequence) -%}
 
 {# START > ADDED BY GEORGIAN #}
 {%- if out_of_sequence and execute -%}
@@ -51,3 +52,119 @@ records_to_insert AS (
 SELECT * FROM records_to_insert
 
 {%- endmacro -%}
+
+
+
+
+/*------ OVERWRITE SNOWFLAKE -----*/
+
+
+
+
+
+/*------ OVERWRITE SNOWFLAKE -----*/
+
+{%- macro bigquery__sat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+
+{{- dbtvault.check_required_parameters(src_pk=src_pk, src_hashdiff=src_hashdiff, src_payload=src_payload,
+                                       src_ldts=src_ldts, src_source=src_source,
+                                       source_model=source_model) -}}
+
+{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source]) -%}
+{%- set rank_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_ldts]) -%}
+{%- set pk_cols = dbtvault.expand_column_list(columns=[src_pk]) -%}
+
+{%- if model.config.materialized == 'vault_insert_by_rank' %}
+    {%- set source_cols_with_rank = source_cols + [config.get('rank_column')] -%}
+{%- endif -%}
+
+{{ dbtvault.prepend_generated_by() }}
+
+WITH source_data AS (
+    {%- if model.config.materialized == 'vault_insert_by_rank' %}
+    SELECT {{ dbtvault.prefix(source_cols_with_rank, 'a', alias_target='source') }}
+    {%- else %}
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='source') }}
+    {%- endif %}
+    FROM {{ ref(source_model) }} AS a
+    WHERE {{ dbtvault.prefix([src_pk], 'a') }} IS NOT NULL
+    {%- if model.config.materialized == 'vault_insert_by_period' %}
+    AND __PERIOD_FILTER__
+    {% elif model.config.materialized == 'vault_insert_by_rank' %}
+    AND __RANK_FILTER__
+    {% endif %}
+),
+
+{% if dbtvault.is_any_incremental() %}
+
+
+latest_records AS (
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='target') }}
+    FROM {{ this }} as a
+    JOIN source_data as b
+    ON a.{{ src_pk }} = b.{{ src_pk }}
+    WHERE 1 = 1
+    QUALIFY
+        1 = ROW_NUMBER() OVER (
+            PARTITION BY {{ dbtvault.prefix([src_pk], 'a') }}
+            ORDER BY {{ dbtvault.prefix([src_eff], 'a') }} DESC
+        )
+),
+{%- endif %}
+
+records_to_insert AS (
+    SELECT DISTINCT {{ dbtvault.alias_all(source_cols, 'stage') }}
+    FROM source_data AS stage
+    {%- if dbtvault.is_any_incremental() %}
+    LEFT JOIN latest_records
+    ON {{ dbtvault.prefix([src_pk], 'latest_records', alias_target='target') }} = {{ dbtvault.prefix([src_pk], 'stage') }}
+    WHERE
+        ({{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} != {{ dbtvault.prefix([src_hashdiff], 'stage') }}
+        OR {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} IS NULL)
+        AND {{ dbtvault.prefix([src_eff], 'stage') }} > {{ dbtvault.prefix([src_eff], 'latest_records', alias_target='target') }}
+    {%- endif %}
+)
+
+SELECT * FROM records_to_insert
+
+{%- endmacro -%}
+
+
+/*
+latest_records
+--------------
+
+
+SAT
+hk  | ldts | ef
+id1 | 3    | 1
+
+
+SOURCE
+hk  | ldts | ef
+id1 | 9    | 2  <
+id1 | 9    | 1
+
+
+
+JOIN GOT
+
+hk  | source.ldts | source.ef | sat.ldts | sat.ef
+id1 | 9           | 2         | 3        | 1
+id1 | 9           | 1         | 3        | 1
+
+SELECT + QUALIFY:
+
+hk  | sat.ldts | sat.ef
+id1 | 3        | 1
+id1 | 3        | 1
+
+======================
+
+
+JOIN WANT
+
+id1 | 9    | 1
+
+
+*/
