5ca7281cd229c5cbb98aeaf359d68b1585d574cd
Tim Wilson <tim.wilson@data-vault.com>
2021-11-15 18:04:17 +0000

    Add code for hashdiff not including CDK / PK; formatting
    AB#1989 Tests SF-MAS-1CD-B and SF-MAS-1CD-I now all passing

--- dbtvault-dev/macros/tables/ma_sat_2.sql
+++ dbtvault-dev/macros/tables/ma_sat_2.sql
@@ -48,26 +48,28 @@ WITH source_data AS (
 {% if dbtvault.is_any_incremental() %}
 
 latest_records AS (
-  SELECT *
-        ,DENSE_RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }} ORDER BY {{ dbtvault.prefix([src_hashdiff], 'mas') }} ASC) AS check_rank
-  FROM
-  (
-  SELECT mas.*
-    ,RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'mas') }} DESC) AS latest_rank
-  FROM {{ this }} AS mas
-  INNER JOIN (SELECT DISTINCT {{ dbtvault.prefix([src_pk], 's') }} FROM source_data as s ) AS spk
-    ON {{ dbtvault.multikey([src_pk], ['mas', 'spk'], condition='=') }}
-  QUALIFY latest_rank = 1
-  ) AS mas
+    SELECT *
+        ,DENSE_RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }}
+            ORDER BY {{ dbtvault.prefix([src_hashdiff], 'mas') }}, {{ dbtvault.prefix([src_cdk], 'mas') }} ASC) AS check_rank
+    FROM
+    (
+    SELECT mas.*
+        ,RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }}
+            ORDER BY {{ dbtvault.prefix([src_ldts], 'mas') }} DESC) AS latest_rank
+    FROM {{ this }} AS mas
+    INNER JOIN (SELECT DISTINCT {{ dbtvault.prefix([src_pk], 's') }} FROM source_data as s ) AS spk
+        ON {{ dbtvault.multikey([src_pk], ['mas', 'spk'], condition='=') }}
+    QUALIFY latest_rank = 1
+    ) AS mas
 ),
 
 -- Select summary details for each group of latest records
 latest_group_details AS (
-SELECT {{ dbtvault.prefix([src_pk], 'msat') }}
-    ,{{ dbtvault.prefix([src_ldts], 'msat') }}
-    ,MAX(msat.check_rank) AS latest_count
-FROM latest_records AS msat
-GROUP BY {{ dbtvault.prefix([src_pk], 'msat') }}, {{ dbtvault.prefix([src_ldts], 'msat') }}
+    SELECT {{ dbtvault.prefix([src_pk], 'msat') }}
+        ,{{ dbtvault.prefix([src_ldts], 'msat') }}
+        ,MAX(msat.check_rank) AS latest_count
+    FROM latest_records AS msat
+    GROUP BY {{ dbtvault.prefix([src_pk], 'msat') }}, {{ dbtvault.prefix([src_ldts], 'msat') }}
 ),
 
 -- endif any_incremental
@@ -75,39 +77,40 @@ GROUP BY {{ dbtvault.prefix([src_pk], 'msat') }}, {{ dbtvault.prefix([src_ldts],
 
 -- Select groups of source records where at least one member does not appear in a group of latest records
 records_to_insert AS (
-SELECT {{ dbtvault.alias_all(source_cols, 'source_data') }}
-FROM source_data
+    SELECT {{ dbtvault.alias_all(source_cols, 'source_data') }}
+    FROM source_data
 
 -- if any_incremental
 {% if dbtvault.is_any_incremental() %}
-  WHERE EXISTS
-  (
-    SELECT 1
-    FROM source_data AS stage
-    WHERE NOT EXISTS
+    WHERE EXISTS
     (
         SELECT 1
-        FROM
+        FROM source_data AS stage
+        WHERE NOT EXISTS
         (
-          SELECT {{ dbtvault.prefix([src_pk], 'lr') }}
-            ,{{ dbtvault.prefix([src_hashdiff], 'lr') }}
-            ,{{ dbtvault.prefix([src_ldts], 'lr') }}
-            ,lr.check_rank
-            ,lg.latest_count
-          FROM latest_records AS lr
-          INNER JOIN latest_group_details AS lg
-            ON {{ dbtvault.multikey([src_pk], ['lr', 'lg'], condition='=') }}
-            AND {{ dbtvault.prefix([src_ldts], 'lr') }} = {{ dbtvault.prefix([src_ldts], 'lg') }}
-        ) AS active_records
-        WHERE {{ dbtvault.multikey([src_pk], ['stage', 'active_records'], condition='=') }}
-        AND {{ dbtvault.prefix([src_hashdiff], 'stage') }} = {{ dbtvault.prefix([src_hashdiff], 'active_records') }}
+            SELECT 1
+            FROM
+            (
+                SELECT {{ dbtvault.prefix([src_pk], 'lr') }}
+                ,{{ dbtvault.prefix([src_hashdiff], 'lr') }}
+                ,{{ dbtvault.prefix([src_cdk], 'lr') }}
+                ,{{ dbtvault.prefix([src_ldts], 'lr') }}
+                ,lr.check_rank
+                ,lg.latest_count
+                FROM latest_records AS lr
+                INNER JOIN latest_group_details AS lg
+                    ON {{ dbtvault.multikey([src_pk], ['lr', 'lg'], condition='=') }}
+                    AND {{ dbtvault.prefix([src_ldts], 'lr') }} = {{ dbtvault.prefix([src_ldts], 'lg') }}
+            ) AS active_records
+            WHERE {{ dbtvault.multikey([src_pk], ['stage', 'active_records'], condition='=') }}
+                AND {{ dbtvault.prefix([src_hashdiff], 'stage') }} = {{ dbtvault.prefix([src_hashdiff], 'active_records') }}
 {# In order to maintain the parallel with the standard satellite, we don't allow for groups of records to be updated if the ldts is the only difference #}
 {#        AND {{ dbtvault.prefix([src_ldts], 'stage') }} = {{ dbtvault.prefix([src_ldts], 'active_records') }}#}
-        AND stage.source_count = active_records.latest_count
+                AND {{ dbtvault.multikey(src_cdk, ['stage', 'active_records'], condition='=') }}
+                AND stage.source_count = active_records.latest_count
+        )
+        AND {{ dbtvault.multikey([src_pk], ['source_data', 'stage'], condition='=') }}
     )
-    AND {{ dbtvault.multikey([src_pk], ['source_data', 'stage'], condition='=') }}
-  )
-
 -- endif any_incremental
 {%- endif %}
 )
