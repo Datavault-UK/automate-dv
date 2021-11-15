f3478bc33a5d89091f2cb724d32bf75476744d2d
Norbert Acatrinei <norbert.acatrinei@data-vault.com>
2021-11-15 15:46:56 +0000

    Added incr logic to new MAS macro + a new MAS incr test

--- dbtvault-dev/macros/tables/ma_sat_2.sql
+++ dbtvault-dev/macros/tables/ma_sat_2.sql
@@ -44,27 +44,72 @@ WITH source_data AS (
     {%- endif %}
 ),
 
---
 -- if any_incremental
---
+{% if dbtvault.is_any_incremental() %}
 
+latest_records AS (
+  SELECT *
+        ,DENSE_RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }} ORDER BY {{ dbtvault.prefix([src_hashdiff], 'mas') }} ASC) AS check_rank
+  FROM
+  (
+  SELECT mas.*
+    ,RANK() OVER (PARTITION BY {{ dbtvault.prefix([src_pk], 'mas') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'mas') }} DESC) AS latest_rank
+  FROM {{ this }} AS mas
+  INNER JOIN (SELECT DISTINCT {{ dbtvault.prefix([src_pk], 's') }} FROM source_data as s ) AS spk
+    ON {{ dbtvault.multikey([src_pk], ['mas', 'spk'], condition='=') }}
+  QUALIFY latest_rank = 1
+  ) AS mas
+),
 
---
+-- Select summary details for each group of latest records
+latest_group_details AS (
+SELECT {{ dbtvault.prefix([src_pk], 'msat') }}
+    ,{{ dbtvault.prefix([src_ldts], 'msat') }}
+    ,MAX(msat.check_rank) AS latest_count
+FROM latest_records AS msat
+GROUP BY {{ dbtvault.prefix([src_pk], 'msat') }}, {{ dbtvault.prefix([src_ldts], 'msat') }}
+),
+
 -- endif any_incremental
---
+{%- endif %}
 
 -- Select groups of source records where at least one member does not appear in a group of latest records
 records_to_insert AS (
-SELECT {{ dbtvault.alias_all(source_cols, 'stage') }}
-FROM source_data AS stage
---
+SELECT {{ dbtvault.alias_all(source_cols, 'source_data') }}
+FROM source_data
+
 -- if any_incremental
---
+{% if dbtvault.is_any_incremental() %}
+  WHERE EXISTS
+  (
+    SELECT 1
+    FROM source_data AS stage
+    WHERE NOT EXISTS
+    (
+        SELECT 1
+        FROM
+        (
+          SELECT {{ dbtvault.prefix([src_pk], 'lr') }}
+            ,{{ dbtvault.prefix([src_hashdiff], 'lr') }}
+            ,{{ dbtvault.prefix([src_ldts], 'lr') }}
+            ,lr.check_rank
+            ,lg.latest_count
+          FROM latest_records AS lr
+          INNER JOIN latest_group_details AS lg
+            ON {{ dbtvault.multikey([src_pk], ['lr', 'lg'], condition='=') }}
+            AND {{ dbtvault.prefix([src_ldts], 'lr') }} = {{ dbtvault.prefix([src_ldts], 'lg') }}
+        ) AS active_records
+        WHERE {{ dbtvault.multikey([src_pk], ['stage', 'active_records'], condition='=') }}
+        AND {{ dbtvault.prefix([src_hashdiff], 'stage') }} = {{ dbtvault.prefix([src_hashdiff], 'active_records') }}
+{# In order to maintain the parallel with the standard satellite, we don't allow for groups of records to be updated if the ldts is the only difference #}
+{#        AND {{ dbtvault.prefix([src_ldts], 'stage') }} = {{ dbtvault.prefix([src_ldts], 'active_records') }}#}
+        AND stage.source_count = active_records.latest_count
+    )
+    AND {{ dbtvault.multikey([src_pk], ['source_data', 'stage'], condition='=') }}
+  )
 
-
---
 -- endif any_incremental
---
+{%- endif %}
 )
 
 SELECT * FROM records_to_insert
