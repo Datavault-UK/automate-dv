7a480496421712c4e6a45d4272a2cea64cbe49c8
AakashDV <ad6g18@soton.ac.uk>
2021-12-10 17:20:36 +0000

    Refactored xts_incremental name to fit standard convention

--- test/dbtvault_harness_utils.py
+++ test/dbtvault_harness_utils.py
@@ -717,8 +717,8 @@ def feature_sub_types():
                 'xts',
                 'xts_comppk'
             ],
-            'inc': [
-                'xts_inc'
+            'incremental': [
+                'xts_incremental'
             ]
         },
         'ma_sats': {
