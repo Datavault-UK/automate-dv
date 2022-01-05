e0f771210568dc4fc4b6196943512c434be9b2d4
Tim Wilson <tim.wilson@data-vault.com>
2022-01-05 14:30:12 +0000

    CASTing should be to DATE and not DATETIME
    See commit 0ec624363544c65280bdce583f8633a10e45d415

--- dbtvault-dev/macros/tables/eff_sat.sql
+++ dbtvault-dev/macros/tables/eff_sat.sql
@@ -385,7 +385,7 @@ new_reopened_records AS (
     FROM source_data AS g
     INNER JOIN latest_closed AS lc
     ON {{ dbtvault.multikey(src_pk, prefix=['g','lc'], condition='=') }}
-    WHERE CAST((g.{{ src_end_date }}) AS DATETIME) = CAST(('{{ max_datetime }}') AS DATETIME)
+    WHERE CAST((g.{{ src_end_date }}) AS DATE) = CAST(('{{ max_datetime }}') AS DATE)
 ),
 
 {%- if is_auto_end_dating %}
@@ -424,7 +424,7 @@ new_closed_records AS (
     ON lo.{{ src_pk }} = h.{{ src_pk }}
     LEFT JOIN latest_closed AS lc
     ON lc.{{ src_pk }} = h.{{ src_pk }}
-    WHERE CAST((h.{{ src_end_date }}) AS DATETIME) != CAST(('{{ max_datetime }}') AS DATETIME)
+    WHERE CAST((h.{{ src_end_date }}) AS DATE) != CAST(('{{ max_datetime }}') AS DATE)
     AND lo.{{ src_pk }} IS NOT NULL
     AND lc.{{ src_pk }} IS NULL
 ),
