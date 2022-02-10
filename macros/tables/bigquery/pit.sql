6e8b70e77e07c2e0fabd46ec6ca2bb5de7a165fc
AakashDV <ad6g18@soton.ac.uk>
2022-02-10 16:57:35 +0000

    Fixed bug in BQ pit macro
    On line 187 of new_rows CTE there was a redundant MAX function; it created an error when multiple arguments were being passed.  This MAX has been removed.

--- macros/tables/bigquery/pit.sql
+++ macros/tables/bigquery/pit.sql
@@ -184,7 +184,7 @@ new_rows AS (
         {%- set sat_pk = dbtvault.escape_column_names(satellites[sat_name]['pk'][sat_pk_name]) -%}
         {%- set sat_ldts = dbtvault.escape_column_names(satellites[sat_name]['ldts'][sat_ldts_name]) %}
         COALESCE(MAX({{ dbtvault.escape_column_names( sat_name | lower ~ '_src' ) }}.{{ sat_pk }}), '{{ ghost_pk }}') AS {{ sat_name | upper }}_{{ sat_pk_name | upper }},
-        COALESCE(MAX(MAX({{ dbtvault.escape_column_names( sat_name | lower ~ '_src' ) }}.{{ sat_ldts }}), PARSE_DATETIME('%F %H:%M:%E6S',  '{{ ghost_date }}')) AS {{ sat_name | upper }}_{{ sat_ldts_name | upper }}
+        COALESCE(MAX({{ dbtvault.escape_column_names( sat_name | lower ~ '_src' ) }}.{{ sat_ldts }}), PARSE_DATETIME('%F %H:%M:%E6S',  '{{ ghost_date }}')) AS {{ sat_name | upper }}_{{ sat_ldts_name | upper }}
         {{- "," if not loop.last }}
     {%- endfor %}
     FROM new_rows_as_of_dates AS a
