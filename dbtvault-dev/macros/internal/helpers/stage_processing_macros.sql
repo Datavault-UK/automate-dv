09734ff168b07865528cbf9c7dbf243ed2d467dc
Alex Higgs <alex.higgs@data-vault.com>
2022-03-29 10:49:42 +0000

    Fix #110 - Exclude columns casing issue
    Now converting columns to uppercase for consistent/standard comparisons in the process_columns_to_select macro

--- dbtvault-dev/macros/internal/helpers/stage_processing_macros.sql
+++ dbtvault-dev/macros/internal/helpers/stage_processing_macros.sql
@@ -1,5 +1,8 @@
 {%- macro process_columns_to_select(columns_list=none, exclude_columns_list=none) -%}
 
+    {% set columns_list = columns_list | map('upper') | list %}
+    {% set exclude_columns_list = exclude_columns_list | map('upper') | list %}
+
     {% set columns_to_select = [] %}
 
     {% if not dbtvault.is_list(columns_list) or not dbtvault.is_list(exclude_columns_list)  %}
