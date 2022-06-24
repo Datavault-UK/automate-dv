d0873294e8614b44bbf86021c9c66802b4597228
Alex Higgs <alex.higgs@data-vault.com>
2022-06-24 15:19:12 +0000

    Added functionality for satellites
    - BigQuery and SQLServer Satellite macros now call the default (code was identical across all platforms)

--- macros/tables/snowflake/sat.sql
+++ macros/tables/snowflake/sat.sql
@@ -1,24 +1,26 @@
-{%- macro sat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+{%- macro sat(src_pk, src_hashdiff, src_payload, src_additional_columns, src_eff, src_ldts, src_source, source_model) -%}
+
+    {{- dbtvault.check_required_parameters(src_pk=src_pk, src_hashdiff=src_hashdiff, src_payload=src_payload,
+                                           src_ldts=src_ldts, src_source=src_source,
+                                           source_model=source_model) -}}
+
+    {%- set src_pk = dbtvault.escape_column_names(src_pk) -%}
+    {%- set src_hashdiff = dbtvault.escape_column_names(src_hashdiff) -%}
+    {%- set src_payload = dbtvault.escape_column_names(src_payload) -%}
+    {%- set src_additional_columns = dbtvault.escape_column_names(src_additional_columns) -%}
+    {%- set src_ldts = dbtvault.escape_column_names(src_ldts) -%}
+    {%- set src_source = dbtvault.escape_column_names(src_source) -%}
 
     {{- adapter.dispatch('sat', 'dbtvault')(src_pk=src_pk, src_hashdiff=src_hashdiff,
-                                            src_payload=src_payload, src_eff=src_eff, src_ldts=src_ldts,
+                                            src_payload=src_payload, src_additional_columns=src_additional_columns,
+                                            src_eff=src_eff, src_ldts=src_ldts,
                                             src_source=src_source, source_model=source_model) -}}
 
 {%- endmacro %}
 
-{%- macro default__sat(src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+{%- macro default__sat(src_pk, src_hashdiff, src_payload, src_additional_columns, src_eff, src_ldts, src_source, source_model) -%}
 
-{{- dbtvault.check_required_parameters(src_pk=src_pk, src_hashdiff=src_hashdiff, src_payload=src_payload,
-                                       src_ldts=src_ldts, src_source=src_source,
-                                       source_model=source_model) -}}
-
-{%- set src_pk = dbtvault.escape_column_names(src_pk) -%}
-{%- set src_hashdiff = dbtvault.escape_column_names(src_hashdiff) -%}
-{%- set src_payload = dbtvault.escape_column_names(src_payload) -%}
-{%- set src_ldts = dbtvault.escape_column_names(src_ldts) -%}
-{%- set src_source = dbtvault.escape_column_names(src_source) -%}
-
-{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_payload, src_eff, src_ldts, src_source]) -%}
+{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_payload, src_additional_columns, src_eff, src_ldts, src_source]) -%}
 {%- set rank_cols = dbtvault.expand_column_list(columns=[src_pk, src_hashdiff, src_ldts]) -%}
 {%- set pk_cols = dbtvault.expand_column_list(columns=[src_pk]) -%}
 
@@ -43,15 +45,15 @@ WITH source_data AS (
     {% endif %}
 ),
 
-{%- if dbtvault.is_any_incremental() %}
+{% if dbtvault.is_any_incremental() %}
 
 latest_records AS (
     SELECT {{ dbtvault.prefix(rank_cols, 'a', alias_target='target') }}
     FROM (
         SELECT {{ dbtvault.prefix(rank_cols, 'current_records', alias_target='target') }},
             RANK() OVER (
-                PARTITION BY {{ dbtvault.prefix([src_pk], 'current_records') }}
-                ORDER BY {{ dbtvault.prefix([src_ldts], 'current_records') }} DESC
+               PARTITION BY {{ dbtvault.prefix([src_pk], 'current_records') }}
+               ORDER BY {{ dbtvault.prefix([src_ldts], 'current_records') }} DESC
             ) AS rank
         FROM {{ this }} AS current_records
             JOIN (
@@ -69,10 +71,10 @@ records_to_insert AS (
     SELECT DISTINCT {{ dbtvault.alias_all(source_cols, 'stage') }}
     FROM source_data AS stage
     {%- if dbtvault.is_any_incremental() %}
-        LEFT JOIN latest_records
-            ON {{ dbtvault.multikey(src_pk, prefix=['latest_records','stage'], condition='=') }}
-            WHERE {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} != {{ dbtvault.prefix([src_hashdiff], 'stage') }}
-                OR {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} IS NULL
+    LEFT JOIN latest_records
+    ON {{ dbtvault.multikey(src_pk, prefix=['latest_records','stage'], condition='=') }}
+    WHERE {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} != {{ dbtvault.prefix([src_hashdiff], 'stage') }}
+        OR {{ dbtvault.prefix([src_hashdiff], 'latest_records', alias_target='target') }} IS NULL
     {%- endif %}
 )
 
