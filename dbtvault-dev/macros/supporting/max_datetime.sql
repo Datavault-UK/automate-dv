c21b538e931df4988a59493cdf4626f3383bd4ea
Alex Higgs <alex.higgs@data-vault.com>
2022-07-05 19:45:07 +0000

    WIP Additional Columns for Bridge
    Fixed bug: Why can't I put variables in a return?

--- dbtvault-dev/macros/supporting/max_datetime.sql
+++ dbtvault-dev/macros/supporting/max_datetime.sql
@@ -1,23 +1,23 @@
 {%- macro max_datetime() -%}
 
-    {{- return(adapter.dispatch('max_datetime', 'dbtvault')()) -}}
+    {%- do return(adapter.dispatch('max_datetime', 'dbtvault')()) -%}
 
 {%- endmacro %}
 
 {%- macro default__max_datetime() %}
 
-    {% do return(var('max_datetime', '9999-12-31 23:59:59.999999')) %}
+    {% do return('9999-12-31 23:59:59.999999') %}
 
 {% endmacro -%}
 
 {%- macro sqlserver__max_datetime() %}
 
-    {% do return(var('max_datetime', '9999-12-31 23:59:59.9999999')) %}
+    {% do return('9999-12-31 23:59:59.9999999') %}
 
 {% endmacro -%}
 
 {%- macro bigquery__max_datetime() %}
 
-    {% do return(var('max_datetime', '9999-12-31 23:59:59.999999')) %}
+    {% do return('9999-12-31 23:59:59.999999') %}
 
 {% endmacro -%}
