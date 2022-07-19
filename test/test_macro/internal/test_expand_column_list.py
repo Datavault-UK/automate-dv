2fd557a27cb914c8b88f9d3e762518b2b31c6c86
Alex Higgs <alex.higgs@data-vault.com>
2022-07-19 20:29:38 +0000

    Macro tests now fully passing again

--- test/test_macro/internal/test_expand_column_list.py
+++ test/test_macro/internal/test_expand_column_list.py
@@ -51,12 +51,3 @@ def test_expand_column_list_correctly_generates_list_with_no_nesting(request, ge
 
     assert dbtvault_harness_utils.is_successful_run(dbt_logs)
     assert actual_sql == expected_sql
-
-
-@pytest.mark.macro
-def test_expand_column_list_raises_error_with_missing_columns(request, generate_model):
-    generate_model()
-
-    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name])
-
-    assert 'Expected a list of columns, got: None' in dbt_logs
