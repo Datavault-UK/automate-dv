52c423ba00cf5c7e8c89983ed549e1db288a6a9d
Alex Higgs <alex.higgs@data-vault.com>
2022-07-20 06:50:38 +0000

    Fix t_link AC

--- dbtvault-dev/macros/tables/bigquery/t_link.sql
+++ dbtvault-dev/macros/tables/bigquery/t_link.sql
@@ -1,6 +1,7 @@
-{%- macro bigquery__t_link(src_pk, src_fk, src_payload, src_eff, src_ldts, src_source, source_model) -%}
+{%- macro bigquery__t_link(src_pk, src_fk, src_payload, src_additional_columns, src_eff, src_ldts, src_source, source_model) -%}
 
 {{ dbtvault.default__t_link(src_pk=src_pk, src_fk=src_fk, src_payload=src_payload,
+                            src_additional_columns=src_additional_columns,
                             src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
                             source_model=source_model) }}
 
