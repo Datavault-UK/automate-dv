3a79f6a6f490892dc6b43397060660b83b4061f8
Tim Wilson <tim.wilson@data-vault.com>
2022-08-01 16:25:45 +0100

    Global variables for required and optional replacement values

--- dbtvault-dev/macros/staging/null_columns.sql
+++ dbtvault-dev/macros/staging/null_columns.sql
@@ -2,15 +2,17 @@
 
     {%- set required = columns['REQUIRED'] | indent(4) -%}
     {%- set optional = columns['OPTIONAL'] | indent(4) -%}
+    {%- set required_value = var('null_key_required', -1) -%}
+    {%- set optional_value = var('null_key_required', -2) -%}
 
     {%- if dbtvault.is_something(required) %}
     {{ required }} AS {{ required }}_ORIGINAL,
-    IFNULL({{ required }}, '-1') AS {{ required }}_NEW{{ "," if dbtvault.is_something(optional) else "" }}
+    IFNULL({{ required }}, {{ required_value }}) AS {{ required }}_NEW{{ "," if dbtvault.is_something(optional) else "" }}
     {%- endif -%}
 
     {%- if dbtvault.is_something(optional) %}
     {{ optional }} AS {{ optional }}_ORIGINAL,
-    IFNULL({{ optional }}, '-1') AS {{ optional }}_NEW
+    IFNULL({{ optional }}, {{ optional_value }}) AS {{ optional }}_NEW
     {%- endif -%}
 
 {%- endmacro -%}
