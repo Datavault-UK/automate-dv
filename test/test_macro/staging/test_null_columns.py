dc439980ad53a160902f7f8a2433dd59679acc57
Tim Wilson <tim.wilson@data-vault.com>
2022-08-03 10:32:39 +0100

    Unit tests for column name lists.

--- test/test_macro/staging/test_null_columns.py
+++ test/test_macro/staging/test_null_columns.py
@@ -126,8 +126,53 @@ def test_null_columns_correctly_generates_sql_with_required_columns(request, gen
 
 
 @pytest.mark.macro
+def test_null_columns_correctly_generates_sql_with_required_columns_no_optional_column(request, generate_model):
+    var_dict = {'columns': {'REQUIRED': ["CUSTOMER_ID", "ORDER_REF"], 'OPTIONAL': ''}}
+
+    generate_model()
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
 def test_null_columns_correctly_generates_sql_with_optional_columns(request, generate_model):
     var_dict = {'columns': {'OPTIONAL': ["CUSTOMER_REF", "ORDER_LINE"]}}
+
+    generate_model()
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_null_columns_correctly_generates_sql_with_optional_columns_no_required_column(request, generate_model):
+    var_dict = {'columns': {'REQUIRED': '', 'OPTIONAL': ["CUSTOMER_REF", "ORDER_LINE"]}}
+
+    generate_model()
+
+    dbt_logs = dbtvault_harness_utils.run_dbt_models(model_names=[request.node.name],
+                                                     args=var_dict)
+    actual_sql = dbtvault_harness_utils.retrieve_compiled_model(request.node.name)
+    expected_sql = dbtvault_harness_utils.retrieve_expected_sql(request)
+
+    assert dbtvault_harness_utils.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_null_columns_correctly_generates_sql_with_required_and_optional_columns(request, generate_model):
+    var_dict = {'columns': {"REQUIRED": ["CUSTOMER_ID", "ORDER_REF"], "OPTIONAL": ["CUSTOMER_REF", "ORDER_LINE"]}}
 
     generate_model()
 
