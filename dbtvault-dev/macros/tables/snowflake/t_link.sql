aca990cac5e1bd7e2facef520f15e04cd823922d
Alex Higgs <alex.higgs@data-vault.com>
2022-08-04 08:57:38 +0000

    Fix whitespacing issue in a few table macros for SQLServer

--- dbtvault-dev/macros/tables/snowflake/t_link.sql
+++ dbtvault-dev/macros/tables/snowflake/t_link.sql
@@ -24,7 +24,7 @@
 {%- macro default__t_link(src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
 {%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source]) -%}
-{%- set fk_cols = dbtvault.expand_column_list([src_fk]) -%}
+{%- set fk_cols = dbtvault.expand_column_list([src_fk]) %}
 
 WITH stage AS (
     SELECT {{ source_cols | join(', ') }}
