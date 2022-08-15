b3a148b5125199a0a6ffd5df12b2de9ab617ade5
Tim Wilson <79925028+DVTimWilson@users.noreply.github.com>
2022-08-15 18:19:06 +0100

    Built-in support for configuring NULL business key handling (#148)
    * Initial scenario
    
    * Improvements to scenario
    
    * WIP feature for null keys
    
    * WIP Scenario STG-23 running but failing
    
    * WIP Scenario STG-23 output not matching expected
    
    * Add comment
    
    * WIP null columns step stilll failing
    
    * WIP minor changes, test still not passing! :'(
    
    * First null key scenario (STG-23) passing (hard coded)
    
    * WIP write null_columns macro
    
    * WIP try to fix null_columns macro, still failing
    
    * Fresh start null_columns macro
    
    * WIP null_columns macro
    
    * Resolve the null+columns undefined error
    
    * Tweaks to null_columns() macro
    
    * Global variables for required and optional replacement values
    
    * Test scenario STG-23 now passing. see commit description for more details...
    
    stage() updated to exclude required/optional columns as necessary; null_columns() updated to avoid case sensitivity of columns dictionary keys
    
    * WIP unit testing null_columns() macro
    
    On the way to enabling lists of required / optional columns to be specified, instead of just single columns
    
    * WIP unit testing null_columns() macro
    
    First column list test passing
    
    * Post merge correction of stage test scenario tags
    
    * Post merge correction of stage test scenario tags
    
    * Revise extract_null_column_names() to process lists too
    
    * Unit tests for column name lists.
    
    * Unit tests for column name lists.
    
    * Unit tests for column name lists.
    
    * Simplify the null_columns() macro, add MSSQL specific code
    
    * Add scenarios STG-26, 27, 28
    
    * Fix scenarios 26-28
    
    * Increase test coverage of null columns feature in stage() macro
    
    * Bug fixes for stage tests
    
    * Bug fixes for stage tests
    
    * Bug fix for xts tests
    
    * Bug fix for xts tests
    
    * Makes stages run in parallel 
    
    I'm doing this from my phone so hope it works lmao
    
    * Bug fixes for staging fixtures
    
    * Allow for custom null_key_required and null_key_optional values in test scenarios
    
    * Bug fix for empty primed stage step (xts)
    
    Need to extract the column names (could be either strings or lists of strings) and append _ORIGINAL to each of them before including these in the stage column headings list
    
    * Remove debugging code
    
    * Cycles tests including null key values and custom replacement values
    
    Co-authored-by: Alex Higgs <alex.higgs@data-vault.com>
    Co-authored-by: DV Siobhan <siobhan.strange@data-vault.com>

--- macros/internal/helpers/stage_processing_macros.sql
+++ macros/internal/helpers/stage_processing_macros.sql
@@ -90,3 +90,30 @@
     {%- endfor -%}
 
 {%- endmacro -%}
+
+
+{%- macro extract_null_column_names(columns_dict=none) -%}
+
+    {%- set extracted_column_names = [] -%}
+
+    {%- if columns_dict is mapping -%}
+        {%- for key, value in columns_dict.items() -%}
+            {%- if dbtvault.is_something(value) -%}
+                {% if dbtvault.is_list(value) %}
+                    {% for col_name in value %}
+                        {%- do extracted_column_names.append(col_name) -%}
+                        {%- do extracted_column_names.append(col_name ~ "_ORIGINAL") -%}
+                    {% endfor %}
+                {%  else %}
+                    {%- do extracted_column_names.append(value) -%}
+                    {%- do extracted_column_names.append(value ~ "_ORIGINAL") -%}
+                {% endif %}
+            {%- endif -%}
+        {%- endfor -%}
+
+        {%- do return(extracted_column_names) -%}
+    {%- else -%}
+        {%- do return([]) -%}
+    {%- endif -%}
+
+{%- endmacro -%}
