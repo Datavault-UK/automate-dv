1a9b3fb4975924703cdb39beb76655c33a9c18db
Alex Higgs <alex.higgs@data-vault.com>
2022-08-19 12:00:49 +0000

    WIP

--- test/test_macro/supporting/expected/hash/snowflake/test_hash_multi_column_as_pk_is_successful_md5.sql
+++ test/test_macro/supporting/expected/hash/snowflake/test_hash_multi_column_as_pk_is_successful_md5.sql
@@ -1,7 +1,7 @@
 CAST(MD5_BINARY(NULLIF(
-     CONCAT(
-        IFNULL(NULLIF(UPPER(TRIM(CAST("CUSTOMER_ID" AS VARCHAR))), ''), '^^'), '||',
-        IFNULL(NULLIF(UPPER(TRIM(CAST("PHONE" AS VARCHAR))), ''), '^^'), '||',
-        IFNULL(NULLIF(UPPER(TRIM(CAST("DOB" AS VARCHAR))), ''), '^^')
-    ), '^^||^^||^^')) AS BINARY(16)
+         CONCAT(
+            IFNULL(NULLIF(UPPER(TRIM(CAST("CUSTOMER_ID" AS VARCHAR))), ''), '^^'), '||',
+            IFNULL(NULLIF(UPPER(TRIM(CAST("PHONE" AS VARCHAR))), ''), '^^'), '||',
+            IFNULL(NULLIF(UPPER(TRIM(CAST("DOB" AS VARCHAR))), ''), '^^')
+        ), '^^||^^||^^')) AS BINARY(16)
 ) AS "CUSTOMER_PK"
