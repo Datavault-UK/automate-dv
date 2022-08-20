b89f3ddec7924bc5202325dff069f544d424e6ad
Alex Higgs <alex.higgs@data-vault.com>
2022-08-20 21:13:19 +0000

    WIP Whitespace fixes

--- dbtvault-dev/macros/internal/metadata_processing/concat_ws.sql
+++ dbtvault-dev/macros/internal/metadata_processing/concat_ws.sql
@@ -7,10 +7,10 @@
 {%- macro default__concat_ws(string_list, separator="||") -%}
 
     CONCAT(
-    {%- for str in string_list %}
+         {%- for str in string_list %}
          {{ str }}
          {%- if not loop.last %}, '{{ separator }}', {%- endif -%}
-    {% endfor %}
+         {%- endfor %}
      )
 
 {%- endmacro -%}
