da83482d4106ca67fd04896d5732b237af38e04d
DV Siobhan <siobhan.strange@data-vault.com>
2022-09-05 16:01:40 +0000

    Add files for ghost records

---
+++ macros/internal/helpers/create_ghost_records.sql
@@ -0,0 +1,32 @@
+{%- macro ghost_pk_cte(source_model, source_columns) -%}
+
+  {{ columns = adapter.get_columns_in_relation(ref(source_model)) }}
+  ## {%- set source_relation = ref(source_model) -%}
+
+  {%- set col_defintions = [] -%}
+
+  {%- for col in columns -%}
+
+  {%- if col in source_columns -%}
+
+        {%- set fetched_type = col.dtype() -%}
+        {%- set fetched_string = ref(ghost_string_lookup)[fetched_type] -%}
+
+  {{ kwargs['src_pk']}}
+
+
+        {%- if col.is_string() == True -%}
+
+          {%- set ... }
+
+        {%- endif -%}
+
+
+{%- col_sql = "CAST({} AS {}) AS {}".format(fetched_string, fetched_type, col) -%}
+{{ col_definitons.append(col_sql) }}
+
+    {%- endif -%}
+
+  {%- endfor -%}
+
+SELECT {% for col in col_definitions %} {{ col }} {% if not loop.last %},{% endif %}{% endfor %}
