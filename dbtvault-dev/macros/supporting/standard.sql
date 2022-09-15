cdd6dbd9e477a92672a23e51a5c02920347522de
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-09-15 13:54:48 +0000

    add uppercase macro
    not fully working - receiving weird error from macro

---
+++ dbtvault-dev/macros/supporting/standard.sql
@@ -0,0 +1,10 @@
+{% macro standard(change_case=UPPER) %}
+    {{ return(adapter.dispatch('standard', 'dbtvault')(change_case)) }}
+{% endmacro %}
+
+{%- macro default__standard(change_case='UPPER') -%}
+    {% if {{ change_case }} is 'UPPER' %}
+        {{ return("NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')") }}
+    {% else %}
+        {{ return("NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR)), '')") }}
+{%- endmacro -%}
