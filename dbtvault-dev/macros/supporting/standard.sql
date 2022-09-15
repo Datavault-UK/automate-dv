af502e98519f5627c09bc3c6042e4345ae20546b
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-09-15 14:57:04 +0000

    #123 fixed for snowflake
    added standard macro to default to UPPER can can be changed.

--- dbtvault-dev/macros/supporting/standard.sql
+++ dbtvault-dev/macros/supporting/standard.sql
@@ -1,11 +1,11 @@
-{% macro standard(change_case=UPPER) %}
-    {{ return(adapter.dispatch('standard', 'dbtvault')(change_case)) }}
-{% endmacro %}
+{%- macro standard(change_case='UPPER') -%}
+    {{- return(adapter.dispatch('standard', 'dbtvault')(change_case=change_case)) -}}
+{%- endmacro -%}
 
 {%- macro default__standard(change_case='UPPER') -%}
-    {% if change_case == 'UPPER' %}
-        {{ return("NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')") }}
-    {% else %}
-        {{ return("NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR)), '')") }}
-    {% endif %}
+    {%- if change_case == 'UPPER' -%}
+        NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')
+    {%- else -%}
+        NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR)), '')
+    {%- endif -%}
 {%- endmacro -%}
