3f5f172d4a8e03c5f17871a5d8453d26a4b3cee3
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-09-15 15:02:14 +0000

    fix for all data warehouses

--- dbtvault-dev/macros/supporting/standard.sql
+++ dbtvault-dev/macros/supporting/standard.sql
@@ -9,3 +9,35 @@
         NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR)), '')
     {%- endif -%}
 {%- endmacro -%}
+
+{%- macro bigquery__standard(change_case='UPPER') -%}
+    {%- if change_case == 'UPPER' -%}
+        NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')
+    {%- else -%}
+        NULLIF(TRIM(CAST([EXPRESSION] AS STRING)), '')
+    {%- endif -%}
+{%- endmacro -%}
+
+{%- macro sqlserver__standard(change_case='UPPER') -%}
+    {%- if change_case == 'UPPER' -%}
+        NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR(max)))), '')
+    {%- else -%}
+        NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR(max))), '')
+    {%- endif -%}
+{%- endmacro -%}
+
+{%- macro postgres__standard(change_case='UPPER') -%}
+    {%- if change_case == 'UPPER' -%}
+        NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS VARCHAR))), '')
+    {%- else -%}
+        NULLIF(TRIM(CAST([EXPRESSION] AS VARCHAR)), '')
+    {%- endif -%}
+{%- endmacro -%}
+
+{%- macro databricks__standard(change_case='UPPER') -%}
+    {%- if change_case == 'UPPER' -%}
+        NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')
+    {%- else -%}
+        NULLIF(TRIM(CAST([EXPRESSION] AS STRING)), '')
+    {%- endif -%}
+{%- endmacro -%}
