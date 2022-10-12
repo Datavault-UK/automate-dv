fb4b1a5313a86a20114b55b7c33398d2cefcf775
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-10-12 11:15:34 +0000

    macro refactoring
    correcting for loop and calling macro

--- dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
+++ dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
@@ -6,26 +6,26 @@
 
 {%- macro default__create_ghost_records(source_model, source_columns) -%}
 
-  {%- set columns = adapter.get_columns_in_relation(source_model) -%}
-{% do log("columns1: " ~ col, true) %}
+  {%- set columns = adapter.get_columns_in_relation(ref(source_model)) -%}
   {%- set col_defintions = [] -%}
 
   {%- for col in columns -%}
-  {% do log("columns: " ~ col, true) %}
-  {%- if col in source_columns -%}
+    {%- do log("columns: " ~ col, true) %}
+    {%- set string_col = '"{}"'.format(col.column) -%}
+        {%- if string_col in source_columns -%}
 
-    {%- set fetched_type = col.dtype() -%}
-    {%- set fetched_name = col.name() -%}
-    {%- set fetched_string = ref(ghost_string_lookup)[fetched_type] -%}
+            {%- set fetched_type = col.dtype -%}
+            {%- set fetched_name = col.column -%}
+            {%- set fetched_string = dbtvault.ghost_string_lookup()[fetched_type] -%}
+            {%- set col_sql = "CAST({} AS {}) AS {}".format(fetched_string, fetched_type, col.column) -%}
 
-    {%- set col_sql = "CAST({} AS {}) AS {}".format(fetched_string, fetched_type, col) -%}
-    {% do log("col_sql " ~ col_sql, true) %}
-    {{ col_definitons.append(col_sql) }}
-    {%- endif -%}
+            {%- col_defintions.append(col_sql) %-}
 
-  {%- endfor -%}
 
-{% do log("col_definitions: " ~ col_definitions, true) %}
+
+  {%- endif -%}
+
+{%- endfor -%}
 
 SELECT {% for col in col_definitions %} {{ col }} {% if not loop.last %},{% endif %}{% endfor %}
 
