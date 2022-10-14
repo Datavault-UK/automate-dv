8d97401fd6e115431dca504ccf4e6263d87d81fb
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-10-14 12:51:34 +0000

    Refactor and code tidying

--- dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
+++ dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
@@ -10,54 +10,56 @@
 {%- set columns = adapter.get_columns_in_relation(ref(source_model)) -%}
 
 {%- set col_definitions = [] -%}
-{%- set col_names = [] -%}
-{# Set ghost_string as the dictionary from ghost_string_lookup macro #}
+
+{# Setting the ghost_string as the dictionary from ghost_string_lookup macro #}
 {%- set ghost_string = dbtvault.ghost_string_lookup() -%}
 
-
-
+    {# iterate through the column information from source model #}
 {%- for col in columns -%}
-{# iterate through the column information from source model #}
-    {%- do log("columns: " ~ col, true) -%}
-    {%- set string_col = '"{}"'.format(col.column) -%}
+
     {# Setting the column name to have "" around it so it can be found in the source columns #}
-    {%- if string_col in source_columns -%}
+    {%- set string_col = '"{}"'.format(col.column) -%}
+
     {# If the column name is in the source columns then do the following, else skip it #}
+    {%- if string_col in source_columns -%}
+
+        {# Return the data type of the source model columns #}
         {%- set fetched_type = col.dtype -%}
-        {%- do log("type: " ~ fetched_type, true) -%}
-    {# Return the data type of the source model columns #}
+        {# Return the column name from the source model #}
         {%- set fetched_name = col.column -%}
-        {%- do log("name: " ~ fetched_name, true) -%}
-    {# Return the column name from the source model #}
+        {# Convert the data type to a string in the format TYPE_VARCHAR #}
         {%- set type_string = 'TYPE_{}'.format(fetched_type|string()) -%}
-    {# Convert the data type to a string in the format TYPE_VARCHAR #}
+        {# Return the corresponding ghost record #}
         {%- set fetched_string = ghost_string[type_string] -%}
-        {%- do log("string: " ~ fetched_string, true) -%}
-    {# Return the corresponding ghost record #}
+
+        {# Ensure record source is not set as just VARCHAR #}
         {%- if fetched_name == record_source -%}
-            {%- set col_sql = "CAST('DBTVAULT_SYSTEM' AS STRING) AS {}".format(fetched_name) -%}
-            {%- do log("system col: " ~ col_sql, true) -%}
+            {%- set col_sql = "CAST('DBTVAULT_SYSTEM' AS VARCHAR) AS {}".format(fetched_name) -%}
+
+        {# If ghost record is NULL then dont cast #}
         {%- elif fetched_string == 'NULL' -%}
-            {%- set col_sql = "CAST(NULL AS VARCHAR) AS {}".format(fetched_name) -%}
-    {# If ghost record is NULL then dont cast #}
+            {%- set col_sql = "NULL AS {}".format(fetched_name) -%}
+
+        {# If ghost record had type BINARY cast as BINARY(16) #}
         {%- elif fetched_type == 'BINARY' -%}
             {%- set col_sql = "CAST('{}' AS BINARY(16)) AS {}".format(fetched_string, fetched_name) -%}
-    {# If ghost record had type BINARY cast as BINARY(16) #}
+
+        {# If ghost record has type DATE then cast with TO_DATE #}
         {%- elif fetched_type == 'DATE' -%}
             {%- set col_sql = "TO_DATE('{}') AS {}".format(fetched_string, fetched_name) -%}
-    {# IF ghost record has type DATE then dont cast #}
+
+        {# Otherwise CAST as the necessary type #}
         {%- else -%}
             {%- set col_sql = "CAST({} AS {}) AS {}".format(fetched_string, fetched_type, fetched_name) -%}
-    {# Otherwise CAST as the necessary type #}
+
         {%- endif -%}
 
-        {%- do log("col_sql: " ~ col_sql, true) -%}
         {%- do col_definitions.append(col_sql) -%}
-        {%- do col_names.append(string_col) -%}
 
     {%- endif -%}
 
 {%- endfor -%}
+
 {# Create the SELECT statement for the CTE when the macro is called. #}
 SELECT
     {% for col in col_definitions -%}
