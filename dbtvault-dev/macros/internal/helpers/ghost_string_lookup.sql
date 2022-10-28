c5ee1beb4c48f8ce8ba1c50371d6b16ba67785cd
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-10-28 15:15:12 +0000

    Add sqlserver functionality

--- dbtvault-dev/macros/internal/helpers/ghost_string_lookup.sql
+++ dbtvault-dev/macros/internal/helpers/ghost_string_lookup.sql
@@ -2,6 +2,7 @@
 
     {% set string_lookup = {'TYPE_STRING': 'NULL',
                             'TYPE_VARCHAR': 'NULL',
+                            'TYPE_VARCHAR(50)': 'NULL',
                             'TYPE_CHAR(32)':'00000000000000000000000000000000',
                             'TYPE_INT':'0',
                             'TYPE_FLOAT':'0.0',
@@ -10,6 +11,7 @@
                             'TYPE_DATE':'1900-01-01',
                             'TYPE_BINARY':'00000000000000000000000000000000',
                             'TYPE_bytea':'00000000000000000000000000000000',
+                            'TYPE_BINARY(16)':'00000000000000000000000000000000',
                             'TYPE_character varying':'NULL',
                             'TYPE_date':'1900-01-01'
     } %}
