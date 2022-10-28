fd774d4f2534646118ed5b6665295ddc9fcabadf
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-10-28 15:59:16 +0000

    arguments refactor

--- macros/tables/snowflake/sat.sql
+++ macros/tables/snowflake/sat.sql
@@ -79,7 +79,7 @@ latest_records AS (
 {%- if enable_ghost_record %}
 
 ghost AS (
-{{- dbtvault.create_ghost_records(source_model, source_cols, record_source=src_source) }}
+{{- dbtvault.create_ghost_records(source_model, source_cols, hashes=[src_pk, src_hashdiff], record_source=src_source) }}
 ),
 
 {%- endif %}
