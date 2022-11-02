ab01a89b5c9001a2c4e104bb832e22fcfb69073a
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-11-02 10:15:44 +0000

    add SHA hash to GR
    Also stop running GitHub pull request everytime

--- dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
+++ dbtvault-dev/macros/internal/helpers/create_ghost_records.sql
@@ -6,6 +6,7 @@
 
 {%- macro default__create_ghost_records(source_model, source_columns, hashes, record_source) -%}
 
+{%- set hash = var('hash', 'MD5') -%}
 {# Retrieve information about columns in the source data #}
 {%- set columns = adapter.get_columns_in_relation(ref(source_model)) -%}
 
@@ -42,7 +43,13 @@
 
         {# If ghost record had type BINARY cast as BINARY(16) #}
         {%- elif fetched_type == 'BINARY' -%}
-            {%- set col_sql = "CAST('{}' AS BINARY(16)) AS {}".format(fetched_string, fetched_name) -%}
+            {%- if hash == 'MD5' -%}
+                {%- set col_sql = "CAST('{}' AS BINARY(16)) AS {}".format(fetched_string, fetched_name) -%}
+            {%- elif hash == 'SHA' -%}
+                {%- set col_sql = "CAST('{}' AS BINARY(32)) AS {}".format(fetched_string, fetched_name) -%}
+            {%- else -%}
+                {%- set col_sql = "CAST('{}' AS BINARY(16)) AS {}".format(fetched_string, fetched_name) -%}
+            {%- endif -%}
 
         {# If ghost record has type DATE then cast with TO_DATE #}
         {%- elif fetched_type == 'DATE' -%}
@@ -61,6 +68,77 @@
 {%- endfor -%}
 
 {# Create the SELECT statement for the CTE when the macro is called. #}
+SELECT
+    {% for col in col_definitions -%}
+    {{ col }}
+    {%- if not loop.last -%},
+    {% endif %}
+    {%- endfor -%}
+
+    {%- endmacro -%}
+
+    {%- macro sqlserver__create_ghost_records(source_model, source_columns, hashes, record_source) -%}
+
+    {# Retrieve information about columns in the source data #}
+    {%- set columns = adapter.get_columns_in_relation(ref(source_model)) -%}
+
+    {%- set col_definitions = [] -%}
+
+    {# Setting the ghost_string as the dictionary from ghost_string_lookup macro #}
+    {%- set ghost_string = dbtvault.ghost_string_lookup() -%}
+    {% set hash = var('hash', 'MD5') -%}
+    {# iterate through the column information from source model #}
+    {%- for col in columns -%}
+
+    {# Setting the column name to have "" around it so it can be found in the source columns #}
+    {%- set string_col = '"{}"'.format(col.column) -%}
+
+    {# If the column name is in the source columns then do the following, else skip it #}
+    {%- if string_col in source_columns -%}
+
+    {# Return the data type of the source model columns #}
+    {%- set fetched_type = col.dtype -%}
+    {# Return the column name from the source model #}
+    {%- set fetched_name = col.column -%}
+    {# Convert the data type to a string in the format TYPE_VARCHAR #}
+    {%- set type_string = 'TYPE_{}'.format(fetched_type|string()) -%}
+    {# Return the corresponding ghost record #}
+    {%- set fetched_string = ghost_string[type_string] -%}
+
+    {# Ensure record source is not set as just VARCHAR #}
+    {%- if fetched_name == record_source -%}
+    {%- set col_sql = "CAST('DBTVAULT_SYSTEM' AS varchar(50)) AS {}".format(fetched_name) -%}
+
+    {# If ghost record is binary #}
+    {%- elif fetched_type == 'binary' -%}
+        {%- if hash == 'MD5' -%}
+        {%- set hash_alg = 'MD5' -%}
+            {%- set col_sql = "CAST(REPLICATE(CAST(CAST({} AS tinyint) AS {}), 16) AS {}) AS {}".format(fetched_string, 'binary(16)', 'binary(16)', fetched_name) -%}
+        {%- elif hash == 'SHA' -%}
+        {%- set hash_alg = 'SHA2_256' -%}
+            {%- set col_sql = "CAST(REPLICATE(CAST(CAST({} AS tinyint) AS {}), 32) AS {}) AS {}".format(fetched_string, 'binary(32)', 'binary(32)', fetched_name) -%}
+        {%- else -%}
+        {%- set hash_alg = 'MD5' -%}
+            {%- set col_sql = "CAST(REPLICATE(CAST(CAST({} AS tinyint) AS {}), 16) AS {}) AS {}".format(fetched_string, 'binary(16)', 'binary(16)', fetched_name) -%}
+        {%- endif -%}
+
+    {# If ghost record is NULL #}
+    {%- elif fetched_string == 'NULL' -%}
+    {%- set col_sql = "CAST(NULL AS varchar(50)) AS {}".format(fetched_name) -%}
+
+    {# Otherwise CAST as the necessary type #}
+    {%- else -%}
+    {%- set col_sql = "CAST('{}' AS {}) AS {}".format(fetched_string, fetched_type, fetched_name) -%}
+
+    {%- endif -%}
+
+    {%- do col_definitions.append(col_sql) -%}
+
+    {%- endif -%}
+
+    {%- endfor -%}
+
+    {# Create the SELECT statement for the CTE when the macro is called. #}
 SELECT
     {% for col in col_definitions -%}
     {{ col }}
@@ -173,68 +251,6 @@ SELECT
         {# Otherwise CAST as the necessary type #}
         {%- else -%}
             {%- set col_sql = "CAST({} AS {}) AS {}".format(fetched_string, fetched_type, fetched_name) -%}
-
-        {%- endif -%}
-
-        {%- do col_definitions.append(col_sql) -%}
-
-    {%- endif -%}
-
-{%- endfor -%}
-
-{# Create the SELECT statement for the CTE when the macro is called. #}
-SELECT
-    {% for col in col_definitions -%}
-    {{ col }}
-    {%- if not loop.last -%},
-    {% endif %}
-    {%- endfor -%}
-
-{%- endmacro -%}
-
-{%- macro sqlserver__create_ghost_records(source_model, source_columns, hashes, record_source) -%}
-
-{# Retrieve information about columns in the source data #}
-{%- set columns = adapter.get_columns_in_relation(ref(source_model)) -%}
-
-{%- set col_definitions = [] -%}
-
-{# Setting the ghost_string as the dictionary from ghost_string_lookup macro #}
-{%- set ghost_string = dbtvault.ghost_string_lookup() -%}
-
-    {# iterate through the column information from source model #}
-{%- for col in columns -%}
-
-    {# Setting the column name to have "" around it so it can be found in the source columns #}
-    {%- set string_col = '"{}"'.format(col.column) -%}
-
-    {# If the column name is in the source columns then do the following, else skip it #}
-    {%- if string_col in source_columns -%}
-
-        {# Return the data type of the source model columns #}
-        {%- set fetched_type = col.dtype -%}
-        {# Return the column name from the source model #}
-        {%- set fetched_name = col.column -%}
-        {# Convert the data type to a string in the format TYPE_VARCHAR #}
-        {%- set type_string = 'TYPE_{}'.format(fetched_type|string()) -%}
-        {# Return the corresponding ghost record #}
-        {%- set fetched_string = ghost_string[type_string] -%}
-
-        {# Ensure record source is not set as just VARCHAR #}
-        {%- if fetched_name == record_source -%}
-            {%- set col_sql = "CAST('DBTVAULT_SYSTEM' AS varchar(50)) AS {}".format(fetched_name) -%}
-
-        {# If ghost record is binary #}
-        {%- elif fetched_type == 'binary' -%}
-            {%- set col_sql = "CAST(REPLICATE(CAST(CAST({} AS tinyint) AS {}), 16) AS {}) AS {}".format(fetched_string, 'binary(16)', 'binary(16)', fetched_name) -%}
-
-        {# If ghost record is NULL #}
-        {%- elif fetched_string == 'NULL' -%}
-            {%- set col_sql = "CAST(NULL AS varchar(50)) AS {}".format(fetched_name) -%}
-
-        {# Otherwise CAST as the necessary type #}
-        {%- else -%}
-            {%- set col_sql = "CAST('{}' AS {}) AS {}".format(fetched_string, fetched_type, fetched_name) -%}
 
         {%- endif -%}
 
