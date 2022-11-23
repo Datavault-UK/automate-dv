61be72eeff762eb47f6ae5dce05c27e9f879369b
JocelynShannon <jocelyn.shannon@data-vault.com>
2022-11-23 12:59:33 +0000

    Unit macro test fix; add clean up function

--- test/test_macro/conftest.py
+++ test/test_macro/conftest.py
@@ -82,6 +82,7 @@ def setup(tmp_path_factory, worker_id):
                 fn.write_text(json.dumps({'status': 'in-use'}))
 
 
+
 @pytest.fixture(scope='module', autouse=True)
 def cleanup(tmp_path_factory, worker_id):
     os.chdir(test.TEST_PROJECT_ROOT)
