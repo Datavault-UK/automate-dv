2182f162677057bc6e4a3b0dbfb9a76fdef2916e
Alex Higgs <alex.higgs@data-vault.com>
2022-11-24 23:17:31 +0000

    WIP Further re-factors
    Code is getting messy again.. need to get it working then go through a final re-factor

---
+++ dbtvault-dev/macros/supporting/hash_components/select_hash_alg.sql
@@ -0,0 +1,53 @@
+{%- macro select_hash_alg(hash=hash) -%}
+
+    {%- if hash | lower == 'md5' -%}
+        {%- do return(dbtvault.hash_alg_md5()) -%}
+    {%- elif hash | lower == 'sha' -%}
+        {%- do return(dbtvault.hash_alg_sha256()) -%}
+    {%- else -%}
+        {%- do return(dbtvault.hash_alg_md5()) -%}
+    {%- endif -%}
+
+{%- endmacro %}
+
+{#- MD5 -#}
+
+{%- macro hash_alg_md5() -%}
+
+    {{- adapter.dispatch('hash_alg_md5', 'dbtvault')() -}}
+
+{%- endmacro %}
+
+{% macro default__hash_alg_md5() -%}
+
+    {% do return('MD5_BINARY([PLACEHOLDER])') %}
+
+{% endmacro %}
+
+{% macro bigquery__hash_alg_md5() -%}
+
+    {% do return('MD5([PLACEHOLDER])') %}
+
+{% endmacro %}
+
+{% macro postgres__hash_alg_md5() -%}
+
+    {% do return('UPPER(MD5([PLACEHOLDER]))') %}
+
+{% endmacro %}
+
+
+{#- SHA256 -#}
+
+
+{%- macro hash_alg_sha256() -%}
+
+    {{- adapter.dispatch('hash_alg_sha256', 'dbtvault')() -}}
+
+{%- endmacro %}
+
+{% macro default__hash_alg_sha256() -%}
+
+    {% do return('SHA2_BINARY') %}
+
+{% endmacro %}
