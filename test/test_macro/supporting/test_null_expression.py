14544d52d0e9f2495ac262b511addec00c5ffee1
Alex Higgs <alex.higgs@data-vault.com>
2022-12-06 23:18:05 +0000

    Further hash component macro tests

--- test/test_macro/supporting/test_null_expression.py
+++ test/test_macro/supporting/test_null_expression.py
@@ -18,3 +18,29 @@ def test_null_expression_is_successful(request, generate_model):
 
     assert macro_test_helpers.is_successful_run(dbt_logs)
     assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_null_expression_with_custom_placeholder_is_successful(request, generate_model):
+    var_dict = {'column_str': 'TEST_COLUMN',
+                'null_placeholder_string': '!!'}
+
+    generate_model()
+
+    dbt_logs = dbt_runner.run_dbt_models(model_names=[request.node.name],
+                                         args=var_dict)
+    actual_sql = macro_test_helpers.retrieve_compiled_model(request.node.name)
+    expected_sql = macro_test_helpers.retrieve_expected_sql(request)
+
+    assert macro_test_helpers.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_null_expression_with_no_column_str_raises_errors(request, generate_model):
+    generate_model()
+
+    dbt_logs = dbt_runner.run_dbt_models(model_names=[request.node.name],
+                                         args=dict())
+
+    assert "Must provide a column_str argument to null expression macro!" in dbt_logs
