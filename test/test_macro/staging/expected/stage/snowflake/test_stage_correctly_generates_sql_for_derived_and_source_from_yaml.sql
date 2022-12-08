19e4cd53b9b569074277803ef7f7623effa7f8b1
Alex Higgs <alex.higgs@data-vault.com>
2022-12-08 07:18:16 +0000

    WIP

--- test/test_macro/staging/expected/stage/snowflake/test_stage_correctly_generates_sql_for_derived_and_source_from_yaml.sql
+++ test/test_macro/staging/expected/stage/snowflake/test_stage_correctly_generates_sql_for_derived_and_source_from_yaml.sql
@@ -59,15 +59,15 @@ columns_to_select AS (
     SELECT
 
     BOOKING_FK,
-    "ORDER_FK",
-    "CUSTOMER_PK",
-    "CUSTOMER_ID",
-    "LOAD_DATE",
-    "RECORD_SOURCE",
-    "CUSTOMER_DOB",
-    "CUSTOMER_NAME",
-    "NATIONALITY",
-    "PHONE",
+    ORDER_FK,
+    CUSTOMER_PK,
+    CUSTOMER_ID,
+    LOAD_DATE,
+    RECORD_SOURCE,
+    CUSTOMER_DOB,
+    CUSTOMER_NAME,
+    NATIONALITY,
+    PHONE,
     "TEST_COLUMN_2",
     "TEST_COLUMN_3",
     "TEST_COLUMN_4",
@@ -76,9 +76,9 @@ columns_to_select AS (
     "TEST_COLUMN_7",
     "TEST_COLUMN_8",
     "TEST_COLUMN_9",
-    "BOOKING_DATE",
-    "SOURCE",
-    "EFFECTIVE_FROM"
+    BOOKING_DATE,
+    SOURCE,
+    EFFECTIVE_FROM
 
     FROM derived_columns
 )
