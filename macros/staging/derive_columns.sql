0375f872cf05dc99441a5a99235187cee478f398
Alex Higgs <alex.higgs@data-vault.com>
2022-12-08 12:35:27 +0000

    Fixed derived column and null column macro tests
    Minor formatting changes in derived column tests as well

--- macros/staging/derive_columns.sql
+++ macros/staging/derive_columns.sql
@@ -47,13 +47,13 @@
                     {%- endfor -%}
 
                     {%- set concat = dbtvault.concat_ws(column_list, "||") -%}
-                    {%- set concat_string = concat ~ " AS " ~ dbtvault.escape_column_names(derived_column) -%}
+                    {%- set concat_string = concat ~ " AS " ~ derived_column -%}
 
                     {%- do der_columns.append(concat_string) -%}
                 {%- else -%}
                     {%- set column_str = dbtvault.as_constant(column_config['source_column']) -%}
                     {%- if column_escape is true -%}
-                        {%- do der_columns.append(dbtvault.escape_column_names(column_str) ~ " AS " ~ dbtvault.escape_column_names(derived_column)) -%}
+                        {%- do der_columns.append(dbtvault.escape_column_names(column_str) ~ " AS " ~ derived_column) -%}
                     {%- else -%}
                         {%- do der_columns.append(column_str ~ " AS " ~ derived_column) -%}
                     {%- endif -%}
