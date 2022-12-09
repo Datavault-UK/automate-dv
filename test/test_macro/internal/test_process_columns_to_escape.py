ca72831ee94bf4cb6a4a9e04c6a8b226ccab9843
Alex Higgs <alex.higgs@data-vault.com>
2022-12-09 15:08:51 +0000

    Implement regex to check column definitions for quoted columns
    Now we can properly escape these columns everywhere else in stage

--- test/test_macro/internal/test_process_columns_to_escape.py
+++ test/test_macro/internal/test_process_columns_to_escape.py
@@ -53,3 +53,51 @@ def test_process_columns_to_escape_with_string_is_successful(request, generate_m
 
     assert macro_test_helpers.is_successful_run(dbt_logs)
     assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_process_columns_to_escape_with_not_string_is_successful(request, generate_model):
+    var_dict = {
+        'derived_columns_list': {
+            'CUSTOMER_NAME': {
+                'source_column': 'CUSTOMER NAME',
+                'escape': True
+            },
+            'EFFECTIVE_FROM': 'LOAD_DATE',
+            'SOURCE': '!RAW_STAGE',
+            'INVERTED_FLAG': 'NOT "CUSTOMER FLAG"'
+        }
+    }
+
+    generate_model()
+
+    dbt_logs = dbt_runner.run_dbt_models(model_names=[request.node.name],
+                                         args=var_dict)
+
+    actual_sql = macro_test_helpers.retrieve_compiled_model(request.node.name)
+    expected_sql = macro_test_helpers.retrieve_expected_sql(request)
+
+    assert macro_test_helpers.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
+
+
+@pytest.mark.macro
+def test_process_columns_to_escape_with_multiple_not_string_is_successful(request, generate_model):
+    var_dict = {
+        'derived_columns_list': {
+            'EFFECTIVE_FROM': 'LOAD_DATE',
+            'SOURCE': '!RAW_STAGE',
+            'INVERTED_FLAG': 'NOT "CUSTOMER FLAG" AND NOT "CUSTOMER NAME"'
+        }
+    }
+
+    generate_model()
+
+    dbt_logs = dbt_runner.run_dbt_models(model_names=[request.node.name],
+                                         args=var_dict)
+
+    actual_sql = macro_test_helpers.retrieve_compiled_model(request.node.name)
+    expected_sql = macro_test_helpers.retrieve_expected_sql(request)
+
+    assert macro_test_helpers.is_successful_run(dbt_logs)
+    assert actual_sql == expected_sql
