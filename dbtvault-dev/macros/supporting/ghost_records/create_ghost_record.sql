ff071385ecd94aaa2e37a0a90bd25d755f8feef0
Alex Higgs <alex.higgs@data-vault.com>
2022-12-13 20:49:58 +0000

    Fix and move create_ghost_record

--- dbtvault-dev/macros/supporting/ghost_records/create_ghost_record.sql
+++ dbtvault-dev/macros/supporting/ghost_records/create_ghost_record.sql
@@ -1,3 +1,8 @@
+/*
+ *  Copyright (c) Business Thinking Ltd. 2019-2022
+ *  This software includes code developed by the dbtvault Team at Business Thinking Ltd. Trading as Datavault
+ */
+
 {%- macro create_ghost_record(src_pk, src_hashdiff, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
     {{- adapter.dispatch('create_ghost_record', 'dbtvault')(src_pk=src_pk, src_hashdiff=src_hashdiff,
@@ -24,25 +29,26 @@
 
     {%- set col_name = col.column -%}
 
-    {%- if (col_name | lower == src_pk | lower) or (col_name | lower == src_hashdiff | lower) -%}
+    {%- if ((col_name | lower) == (src_pk | lower)) or ((col_name | lower) == (src_hashdiff | lower)) -%}
         {%- set col_sql = dbtvault.binary_ghost(alias=col_name, hash=hash) -%}
         {%- do col_definitions.append(col_sql) -%}
 
-    {%- elif (col_name | lower == src_eff | lower) or (col_name | lower == src_ldts | lower) -%}
-        {%- if (col.dtype | lower == 'date') -%}
+    {%- elif ((col_name | lower) == (src_eff | lower)) or ((col_name | lower) == (src_ldts | lower))-%}
+        {%- if (col.dtype | lower) == 'date' -%}
             {%- set col_sql = dbtvault.cast_date('1900-01-01', as_string=true, datetime=false, alias=col_name)-%}
         {%- else -%}
             {%- set col_sql = dbtvault.cast_date('1900-01-01 00:00:00', as_string=true, datetime=true, alias=col_name)-%}
         {%- endif -%}
         {%- do col_definitions.append(col_sql) -%}
 
-    {%- elif col_name | lower == src_source | lower -%}
+    {%- elif (col_name | lower) == (src_source | lower) -%}
         {%- set col_sql -%}
             CAST('{{ source_str }}' AS {{ col.dtype }}) AS {{ src_source }}
         {%- endset -%}
         {%- do col_definitions.append(col_sql) -%}
 
-    {%- elif (col_name is in string_columns[0]) or (col_name is in string_columns[1]) -%}
+    {%- elif ((col_name | lower) is in (string_columns[0] | map('lower') | list))
+             or ((col_name | lower) is in (string_columns[1] | map('lower') | list)) -%}
         {% set col_sql = dbtvault.null_ghost(col.dtype, col_name) -%}
         {%- do col_definitions.append(col_sql) -%}
 
