17ea2df4b315967246d912cca78eb2bc493b0f5e
Liam Coley <liam@noriaenterprises.com>
2023-04-24 10:56:38 +1200

    redshift branch

--- macros/staging/stage.sql
+++ macros/staging/stage.sql
@@ -26,14 +26,12 @@
 
     {#- Check for source format or ref format and create
     relation OBJECT FROM source_model - #}
-    {{print('!!!!!')}}
-    {{print(source_model)}}
     {% if source_model is mapping and source_model is not none -%}
         {%- set source_name = source_model | first -%}
         {%- set source_table_name = source_model[source_name] -%}
         {%- set source_relation = source(source_name, source_table_name) -%}
         {%- set all_source_columns = dbtvault.source_columns(source_relation = source_relation) -%}
-        {%- elif source_model is not mapping and source_model is not none -%}
+    {%- elif source_model is not mapping and source_model is not none -%}
         {%- set source_relation = ref(source_model) -%}
         {%- set all_source_columns = dbtvault.source_columns(source_relation = source_relation) -%}
     {%- else -%}
