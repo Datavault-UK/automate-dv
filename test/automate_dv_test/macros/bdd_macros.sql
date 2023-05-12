b5a5f7e9c81c987439fa984db970bc02009173c6
Alex Higgs <alex.higgs@data-vault.com>
2023-05-12 09:34:26 +0000

    Update test folder

--- test/automate_dv_test/macros/bdd_macros.sql
+++ test/automate_dv_test/macros/bdd_macros.sql
@@ -1,6 +1,6 @@
 {%- macro drop_model(model_name) -%}
 
-    {% set schema_name = dbtvault_test.get_schema_name() %}
+    {% set schema_name = automate_dv_dev.get_schema_name() %}
 
     {%- if target.type == 'databricks' -%}
         {%- set source_relation = adapter.get_relation(
@@ -25,7 +25,7 @@
 
 {% macro check_model_exists(model_name) %}
 
-    {% set schema_name = dbtvault_test.get_schema_name() %}
+    {% set schema_name = automate_dv_dev.get_schema_name() %}
 
     {%- if target.type == 'databricks' -%}
         {%- set source_relation = adapter.get_relation(
@@ -50,7 +50,7 @@
 {% macro drop_all_custom_schemas(schema_prefix=none) %}
 
     {%- if not schema_prefix -%}
-        {% set schema_name = dbtvault_test.get_schema_name() -%}
+        {% set schema_name = automate_dv_dev.get_schema_name() -%}
     {%- else -%}
         {% set schema_name = schema_prefix -%}
     {%- endif -%}
@@ -93,7 +93,7 @@
 
 {%- macro drop_test_schemas() -%}
 
-    {% set schema_name = dbtvault_test.get_schema_name() %}
+    {% set schema_name = automate_dv_dev.get_schema_name() %}
 
     {%- if target.type == 'databricks' -%}
         {% do adapter.drop_schema(api.Relation.create(schema=schema_name)) %}
@@ -120,7 +120,7 @@
 
 {%- macro drop_current_schema() -%}
 
-    {% set schema_to_drop = dbtvault_test.get_schema_name() %}
+    {% set schema_to_drop = automate_dv_dev.get_schema_name() %}
 
     {%- if target.type == 'databricks' -%}
         {% do adapter.drop_schema(api.Relation.create(schema=schema_to_drop)) %}
@@ -135,7 +135,7 @@
 
 {%- macro create_test_schemas() -%}
 
-    {% set schema_name = dbtvault_test.get_schema_name() %}
+    {% set schema_name = automate_dv_dev.get_schema_name() %}
 
     {%- if target.type == 'databricks' -%}
         {% do adapter.create_schema(api.Relation.create(schema=schema_name)) %}
