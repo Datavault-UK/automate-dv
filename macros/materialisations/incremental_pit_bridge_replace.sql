ab70dee3ec3595a68cbd835d9ead0ce32b01658d
Jocelyn Shannon <jocelyn.shannon@datavault.com>
2023-05-21 14:28:49 +0000

    All pit tests passing

--- macros/materialisations/incremental_pit_bridge_replace.sql
+++ macros/materialisations/incremental_pit_bridge_replace.sql
@@ -4,6 +4,28 @@
  */
 
 {% macro incremental_pit_replace(tmp_relation, target_relation, statement_name="main") %}
+
+    {{ adapter.dispatch('incremental_pit_replace', 'automate_dv')(tmp_relation=tmp_relation,
+                                                                    target_relation=target_relation,
+                                                                    statement_name="main") }}
+
+{%- endmacro -%}
+
+{% macro default__incremental_pit_replace(tmp_relation, target_relation, statement_name="main") %}
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
+
+    TRUNCATE TABLE {{ target_relation }};
+
+    INSERT INTO {{ target_relation }} ({{ dest_cols_csv }})
+    (
+       SELECT {{ dest_cols_csv }}
+       FROM {{ tmp_relation }}
+    );
+{%- endmacro %}
+
+
+{% macro databricks__incremental_pit_replace(tmp_relation, target_relation, statement_name="main") %}
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
 
@@ -16,6 +38,15 @@
 
 
 {% macro incremental_bridge_replace(tmp_relation, target_relation, statement_name="main") %}
+
+    {{ adapter.dispatch('incremental_bridge_replace', 'automate_dv')(tmp_relation=tmp_relation,
+                                                                    target_relation=target_relation,
+                                                                    statement_name="main") }}
+
+{%- endmacro -%}
+
+
+{% macro default__incremental_bridge_replace(tmp_relation, target_relation, statement_name="main") %}
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
 
@@ -26,6 +57,17 @@
        SELECT {{ dest_cols_csv }}
        FROM {{ tmp_relation }}
     );
+{%- endmacro %}
+
+
+{% macro databricks__incremental_bridge_replace(tmp_relation, target_relation, statement_name="main") %}
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
+
+    INSERT OVERWRITE {{ target_relation }} ({{ dest_cols_csv }})
+       SELECT {{ dest_cols_csv }}
+       FROM {{ tmp_relation }}
+    ;
 {%- endmacro %}
 
 
