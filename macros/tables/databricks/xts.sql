ec682b1cdfc81c607cb4927cd3347ccf41ced39c
Jocelyn Shannon <jocelyn.shannon@datavault.com>
2023-05-23 08:47:46 +0000

    Add missed extra columns call to xts macro

--- macros/tables/databricks/xts.sql
+++ macros/tables/databricks/xts.sql
@@ -5,8 +5,11 @@
 
 {%- macro databricks__xts(src_pk, src_satellite, src_extra_columns, src_ldts, src_source, source_model) -%}
 
-{{- automate_dv.default__xts(src_pk=src_pk, src_satellite=src_satellite,
-                               src_ldts=src_ldts, src_source=src_source,
-                               source_model=source_model) -}}
+{{ automate_dv.default__xts(src_pk=src_pk,
+                         src_satellite=src_satellite,
+                         src_extra_columns=src_extra_columns,
+                         src_ldts=src_ldts,
+                         src_source=src_source,
+                         source_model=source_model) }}
 
 {%- endmacro -%}
