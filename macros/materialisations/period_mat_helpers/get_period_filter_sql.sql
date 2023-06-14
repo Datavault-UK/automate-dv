47b82c40db49338975e64cc714136aece2ec6a76
Alex Higgs <alex.higgs@data-vault.com>
2023-06-14 09:45:52 +0000

    Move error message to macro

--- macros/materialisations/period_mat_helpers/get_period_filter_sql.sql
+++ macros/materialisations/period_mat_helpers/get_period_filter_sql.sql
@@ -7,12 +7,12 @@
 
     {% set macro = adapter.dispatch('get_period_filter_sql',
                                     'automate_dv')(target_cols_csv=target_cols_csv,
-                                                base_sql=base_sql,
-                                                timestamp_field=timestamp_field,
-                                                period=period,
-                                                start_timestamp=start_timestamp,
-                                                stop_timestamp=stop_timestamp,
-                                                offset=offset) %}
+                                                   base_sql=base_sql,
+                                                   timestamp_field=timestamp_field,
+                                                   period=period,
+                                                   start_timestamp=start_timestamp,
+                                                   stop_timestamp=stop_timestamp,
+                                                   offset=offset) %}
     {% do return(macro) %}
 {%- endmacro %}
 
@@ -23,10 +23,10 @@
     {%- set filtered_sql = {'sql': base_sql} -%}
 
     {%- do filtered_sql.update({'sql': automate_dv.replace_placeholder_with_period_filter(core_sql=filtered_sql.sql,
-                                                                                       timestamp_field=timestamp_field,
-                                                                                       start_timestamp=start_timestamp,
-                                                                                       stop_timestamp=stop_timestamp,
-                                                                                       offset=offset, period=period)}) -%}
+                                                                                          timestamp_field=timestamp_field,
+                                                                                          start_timestamp=start_timestamp,
+                                                                                          stop_timestamp=stop_timestamp,
+                                                                                          offset=offset, period=period)}) -%}
     select {{ target_cols_csv }} from ({{ filtered_sql.sql }})
 {%- endmacro %}
 
@@ -38,10 +38,10 @@
     {%- set filtered_sql = {'sql': base_sql} -%}
 
     {%- do filtered_sql.update({'sql': automate_dv.replace_placeholder_with_period_filter(core_sql=filtered_sql.sql,
-                                                                                       timestamp_field=timestamp_field,
-                                                                                       start_timestamp=start_timestamp,
-                                                                                       stop_timestamp=stop_timestamp,
-                                                                                       offset=offset, period=period)}) -%}
+                                                                                          timestamp_field=timestamp_field,
+                                                                                          start_timestamp=start_timestamp,
+                                                                                          stop_timestamp=stop_timestamp,
+                                                                                          offset=offset, period=period)}) -%}
     {# MSSQL does not allow CTEs in a subquery #}
     {{ filtered_sql.sql }}
 {%- endmacro %}
@@ -53,9 +53,9 @@
     {%- set filtered_sql = {'sql': base_sql} -%}
 
     {%- do filtered_sql.update({'sql': automate_dv.replace_placeholder_with_period_filter(core_sql=filtered_sql.sql,
-                                                                                       timestamp_field=timestamp_field,
-                                                                                       start_timestamp=start_timestamp,
-                                                                                       stop_timestamp=stop_timestamp,
-                                                                                       offset=offset, period=period)}) -%}
+                                                                                          timestamp_field=timestamp_field,
+                                                                                          start_timestamp=start_timestamp,
+                                                                                          stop_timestamp=stop_timestamp,
+                                                                                          offset=offset, period=period)}) -%}
     select {{ target_cols_csv }} from ({{ filtered_sql.sql }})
 {%- endmacro %}
