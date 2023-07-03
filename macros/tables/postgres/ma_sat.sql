464b5af0e68b16dc99cfc60b2f9e1c0d13cc1fd1
Alex Higgs <alex.higgs@data-vault.com>
2023-07-03 15:12:35 +0000

    Minor changes for Postgres fixes

--- macros/tables/postgres/ma_sat.sql
+++ macros/tables/postgres/ma_sat.sql
@@ -6,9 +6,9 @@
 {%- macro postgres__ma_sat(src_pk, src_cdk, src_hashdiff, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
     {{- automate_dv.sqlserver__ma_sat(src_pk=src_pk, src_cdk=src_cdk,
-                            src_hashdiff=src_hashdiff, src_payload=src_payload,
-                            src_extra_columns=src_extra_columns, src_eff=src_eff,
-                            src_ldts=src_ldts, src_source=src_source,
-                            source_model=source_model) -}}
+                                      src_hashdiff=src_hashdiff, src_payload=src_payload,
+                                      src_extra_columns=src_extra_columns, src_eff=src_eff,
+                                      src_ldts=src_ldts, src_source=src_source,
+                                      source_model=source_model) -}}
 
 {%- endmacro -%}
