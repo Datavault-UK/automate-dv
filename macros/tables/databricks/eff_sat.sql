8fda3b9c6f5be1693bb846fd5276fa26e9f57026
Alex Higgs <alex.higgs@data-vault.com>
2023-07-04 18:14:03 +0000

    Formatting

--- macros/tables/databricks/eff_sat.sql
+++ macros/tables/databricks/eff_sat.sql
@@ -5,10 +5,10 @@
 
 {%- macro databricks__eff_sat(src_pk, src_dfk, src_sfk, src_extra_columns, src_start_date, src_end_date, src_eff, src_ldts, src_source, source_model) -%}
 
-    {{ automate_dv.default__eff_sat(src_pk=src_pk, src_dfk=src_dfk, src_sfk=src_sfk,
-                                   src_extra_columns=src_extra_columns,
-                                   src_start_date=src_start_date, src_end_date=src_end_date,
-                                   src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
-                                   source_model=source_model) -}}
+    {{- automate_dv.default__eff_sat(src_pk=src_pk, src_dfk=src_dfk, src_sfk=src_sfk,
+                                     src_extra_columns=src_extra_columns,
+                                     src_start_date=src_start_date, src_end_date=src_end_date,
+                                     src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
+                                     source_model=source_model) -}}
 
 {%- endmacro -%}
--- macros/tables/databricks/hub.sql
+++ macros/tables/databricks/hub.sql
@@ -5,11 +5,11 @@
 
 {%- macro databricks__hub(src_pk, src_nk, src_extra_columns, src_ldts, src_source, source_model) -%}
 
-{{ automate_dv.default__hub(src_pk=src_pk,
-                         src_nk=src_nk,
-                         src_extra_columns=src_extra_columns,
-                         src_ldts=src_ldts,
-                         src_source=src_source,
-                         source_model=source_model) }}
+{{- automate_dv.default__hub(src_pk=src_pk,
+                             src_nk=src_nk,
+                             src_extra_columns=src_extra_columns,
+                             src_ldts=src_ldts,
+                             src_source=src_source,
+                             source_model=source_model) -}}
 
 {%- endmacro -%}
