3e9ed8f4add4157ee54b98ca31a0d3b95363862b
Aakash Datta <aakash.datta@data-vault.com>
2023-07-26 08:43:33 +0000

    Updated satellite macro tests with new loading scheme

--- test/test_macro/tables/expected/sat/snowflake/test_sat_correctly_generates_sql_when_all_columns_are_added_to_excluded.sql
+++ test/test_macro/tables/expected/sat/snowflake/test_sat_correctly_generates_sql_when_all_columns_are_added_to_excluded.sql
@@ -1,12 +1,31 @@
 WITH source_data AS (
     SELECT a.CUSTOMER_PK, a.HASHDIFF, a.EFFECTIVE_FROM, a.LOAD_DATE, a.RECORD_SOURCE
-    FROM [DATABASE_NAME].[SCHEMA_NAME].raw_source_sat AS a
+    FROM AUTOMATE_DV_DEV.TEST_AAKASH_DATTA.raw_source_sat AS a
     WHERE a.CUSTOMER_PK IS NOT NULL
 ),
 
+
+first_record_in_set AS (
+    SELECT
+    sd.CUSTOMER_PK, sd.HASHDIFF, sd.EFFECTIVE_FROM, sd.LOAD_DATE, sd.RECORD_SOURCE
+    , ROW_NUMBER() OVER(PARTITION BY CUSTOMER_PK ORDER BY LOAD_DATE ASC) as asc_row_number
+    FROM source_data as sd
+    QUALIFY asc_row_number = 1
+),
+
+unique_source_records AS (
+    SELECT DISTINCT
+        sd.CUSTOMER_PK, sd.HASHDIFF, sd.EFFECTIVE_FROM, sd.LOAD_DATE, sd.RECORD_SOURCE
+    FROM source_data as sd
+    QUALIFY HASHDIFF != LAG(HASHDIFF) OVER(PARTITION BY CUSTOMER_PK ORDER BY LOAD_DATE ASC)
+),
+
 records_to_insert AS (
-    SELECT DISTINCT stage.CUSTOMER_PK, stage.HASHDIFF, stage.EFFECTIVE_FROM, stage.LOAD_DATE, stage.RECORD_SOURCE
-    FROM source_data AS stage
+        SELECT frin.CUSTOMER_PK, frin.HASHDIFF, frin.EFFECTIVE_FROM, frin.LOAD_DATE, frin.RECORD_SOURCE
+        FROM first_record_in_set AS frin
+        UNION
+        SELECT usr.CUSTOMER_PK, usr.HASHDIFF, usr.EFFECTIVE_FROM, usr.LOAD_DATE, usr.RECORD_SOURCE
+        FROM unique_source_records as usr
 )
 
 SELECT * FROM records_to_insert
