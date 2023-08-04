77109e592f7339705d3a6ec4855b90cdd4ac2bb7
Alex Higgs <alex.higgs@data-vault.com>
2023-08-04 09:31:36 +0000

    Remove empty string from SAT-CYC-03
    Something has changed in either dbt or Python itself, cant seem to pin it down even with extensive testing

--- test/automate_dv_generator.py
+++ test/automate_dv_generator.py
@@ -745,8 +745,6 @@ def template_to_file(template, model_name):
         f.write(template.strip())
 
 
-
-
 def add_seed_config(seed_name: str, seed_config: dict, include_columns=None,
                     additional_config=None):
     """
