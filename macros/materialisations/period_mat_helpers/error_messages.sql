f9fc816475ac4c185ef9bd0f4aec0da1c06df9d4
Alex Higgs <alex.higgs@data-vault.com>
2023-08-09 12:39:01 +0000

    Improve vault_inset_by_x error messages and restore func. to Sats

--- macros/materialisations/period_mat_helpers/error_messages.sql
+++ macros/materialisations/period_mat_helpers/error_messages.sql
@@ -3,24 +3,79 @@
  * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading as Datavault
  */
 
-{%- macro sqlserver_datepart_too_small_error(period) -%}
 
-    {%- set error_message -%}
-    This datepart ({{ period }}) is too small and cannot be used for this purpose in MS SQL Server, consider using a different datepart value (e.g. day).
-    vault_insert_by materialisations are not intended for this purpose, please see https://automate-dv.readthedocs.io/en/latest/materialisations/
+{% macro wrap_warning(warning_message) %}
+
+    {%- set new_message = [] -%}
+    {%- set length_list = [] -%}
+
+    {%- for ln in warning_message.split('\n') -%}
+        {%- do new_message.append((ln | trim)) -%}
+        {%- do length_list.append((ln | length)) -%}
+    {%- endfor -%}
+
+    {%- set max_line_length = length_list | max -%}
+    {%- set padding_length = (max_line_length - 7) // 2 -%}
+
+    {%- set border = modules.itertools.repeat('=', padding_length) | join ('') ~ 'WARNING' ~ modules.itertools.repeat('=', padding_length) | join ('') -%}
+
+    {%- set wrapped_message = '\n' ~ border ~ '\n' ~ new_message | join('\n') ~ '\n' ~ border -%}
+
+    {%- do return(wrapped_message) -%}
+
+{% endmacro %}
+
+
+{%- macro datepart_too_small_error(period) -%}
+
+    {%- set message -%}
+    This datepart ({{ period }}) is too small and not recommended, donsider using a different datepart value (e.g. day) or rank column.
+    'vault_insert_by_x' materialisations are not intended for production use.
+
+    Please see: https://automate-dv.readthedocs.io/en/latest/materialisations/
     {%- endset -%}
 
     {{- exceptions.raise_compiler_error(error_message) -}}
 
 {%- endmacro -%}
 
-{%- macro sqlserver_max_iterations_error() -%}
 
-    {%- set error_message -%}
-    Max iterations is 100,000. Consider using a different datepart value (e.g. day) or loading data for a shorter time period.
-    vault_insert_by materialisations are not intended for this purpose, please see https://automate-dv.readthedocs.io/en/latest/materialisations/
+{%- macro datepart_not_recommended_warning(period) -%}
+
+    {%- set message -%}
+    This datepart ({{ period }}) is too small and not recommended, consider using a different datepart value (e.g. day) or rank column.
+    'vault_insert_by_x' materialisations are not intended for production use.
+
+    Please see: https://automate-dv.readthedocs.io/en/latest/materialisations/
     {%- endset -%}
 
-    {{- exceptions.raise_compiler_error(error_message) -}}
+    {{- exceptions.warn(automate_dv.wrap_warning(message)) -}}
+
+{%- endmacro -%}
+
+
+{%- macro max_iterations_error() -%}
+
+    {%- set message -%}
+    Max iterations is 100,000. Consider using a different datepart value (e.g. day), rank column or loading data for a shorter time period.
+    'vault_insert_by_x' materialisations are not intended for production use.
+
+    Please see: https://automate-dv.readthedocs.io/en/latest/materialisations/
+    {%- endset %}
+
+    {{ exceptions.raise_compiler_error(message) }}
+
+{%- endmacro -%}
+
+
+{%- macro experimental_not_recommended_warning(func_name) -%}
+
+    {%- set message -%}
+    This functionality ({{ func_name }}) is intended for experimental or testing purposes only.
+    Its behavior, reliability, and performance have not been thoroughly vetted for production environments.
+    Using this functionality in a live production setting may result in unpredictable outcomes, data loss, or system instability.
+    {%- endset -%}
+
+    {{- exceptions.warn(automate_dv.wrap_warning(message)) -}}
 
 {%- endmacro -%}
