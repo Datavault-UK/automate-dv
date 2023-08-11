a8bfcec98d9aea674ff4c34ed0529902dc1faf09
Alex Higgs <alex.higgs@data-vault.com>
2023-08-11 16:09:06 +0000

    Make src_ldts and src_source optional in ref_table

--- macros/tables/snowflake/ref_table.sql
+++ macros/tables/snowflake/ref_table.sql
@@ -5,8 +5,7 @@
 
 {%- macro ref_table(src_pk, src_extra_columns, src_ldts, src_source, source_model) -%}
 
-{{- automate_dv.check_required_parameters(src_pk=src_pk, src_ldts=src_ldts, src_source=src_source,
-                                          source_model=source_model) -}}
+{{- automate_dv.check_required_parameters(src_pk=src_pk, source_model=source_model) -}}
 
     {%- if not automate_dv.is_list(source_model) -%}
         {%- set source_model = [source_model] -%}
