a5547ab25a8ed3e64ccae2c6a202cb55a67bd990
Alex Higgs <alex.higgs@data-vault.com>
2023-08-26 11:01:50 +0000

    Fixes for failing tests

--- automate_dv_dev/macros/tables/snowflake/sat.sql
+++ automate_dv_dev/macros/tables/snowflake/sat.sql
@@ -118,9 +118,9 @@ unique_source_records AS (
 
 ghost AS (
     {{ automate_dv.create_ghost_record(src_pk=src_pk, src_hashdiff=src_hashdiff,
-                                    src_payload=src_payload, src_extra_columns=src_extra_columns,
-                                    src_eff=src_eff, src_ldts=src_ldts,
-                                    src_source=src_source, source_model=source_model) }}
+                                       src_payload=src_payload, src_extra_columns=src_extra_columns,
+                                       src_eff=src_eff, src_ldts=src_ldts,
+                                       src_source=src_source, source_model=source_model) }}
 ),
 
 {%- endif %}
