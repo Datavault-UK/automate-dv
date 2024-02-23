387030c8aea65a7413beaa3d01f61bbd5efd60c2
Alex Higgs <alex.higgs@data-vault.com>
2024-02-23 12:58:33 +0000

    Minor
    Minor

--- macros/tables/snowflake/sat.sql
+++ macros/tables/snowflake/sat.sql
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) Business Thinking Ltd. 2019-2023
- * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading AS Datavault
+ * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading as Datavault
  */
 
 {%- macro sat(src_pk, src_hashdiff, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
     {{- automate_dv.check_required_parameters(src_pk=src_pk, src_hashdiff=src_hashdiff, src_payload=src_payload,
-                                           src_ldts=src_ldts, src_source=src_source,
-                                           source_model=source_model) -}}
+                                              src_ldts=src_ldts, src_source=src_source,
+                                              source_model=source_model) -}}
 
     {%- set src_payload = automate_dv.process_payload_column_excludes(
                               src_pk=src_pk, src_hashdiff=src_hashdiff,
@@ -17,9 +17,9 @@
     {{ automate_dv.prepend_generated_by() }}
 
     {{ adapter.dispatch('sat', 'automate_dv')(src_pk=src_pk, src_hashdiff=src_hashdiff,
-                                           src_payload=src_payload, src_extra_columns=src_extra_columns,
-                                           src_eff=src_eff, src_ldts=src_ldts,
-                                           src_source=src_source, source_model=source_model) -}}
+                                              src_payload=src_payload, src_extra_columns=src_extra_columns,
+                                              src_eff=src_eff, src_ldts=src_ldts,
+                                              src_source=src_source, source_model=source_model) -}}
 
 {%- endmacro -%}
 
