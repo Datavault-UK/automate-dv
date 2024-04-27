f377f07360ee661b63fe1377e326a34c24bc851f
Alex Higgs <alex.higgs@data-vault.com>
2024-04-27 15:43:23 +0000

    Update copyright

--- macros/tables/snowflake/ref_table.sql
+++ macros/tables/snowflake/ref_table.sql
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) Business Thinking Ltd. 2019-2022
- * This software includes code developed by the dbtvault Team at Business Thinking Ltd. Trading as Datavault
+ * Copyright (c) Business Thinking Ltd. 2019-2024
+ * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading as Datavault
  */
 
 {%- macro ref_table(src_pk, src_extra_columns, src_ldts, src_source, source_model) -%}
