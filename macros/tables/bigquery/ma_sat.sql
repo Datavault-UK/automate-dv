be225a109488573c1b5a88b3b1c1a599635b93fa
Alex Higgs <alex.higgs@data-vault.com>
2024-04-30 15:36:43 +0000

    Fix MSAT

--- macros/tables/bigquery/ma_sat.sql
+++ macros/tables/bigquery/ma_sat.sql
@@ -5,10 +5,10 @@
 
 {%- macro bigquery__ma_sat(src_pk, src_cdk, src_hashdiff, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) %}
 
-{{- automate_dv.snowflake__ma_sat(src_pk=src_pk, src_cdk=src_cdk,
-                                  src_hashdiff=src_hashdiff, src_payload=src_payload,
-                                  src_extra_columns=src_extra_columns, src_eff=src_eff,
-                                  src_ldts=src_ldts, src_source=src_source,
-                                  source_model=source_model) -}}
+{{- automate_dv.default__ma_sat(src_pk=src_pk, src_cdk=src_cdk,
+                                src_hashdiff=src_hashdiff, src_payload=src_payload,
+                                src_extra_columns=src_extra_columns, src_eff=src_eff,
+                                src_ldts=src_ldts, src_source=src_source,
+                                source_model=source_model) -}}
 
 {%- endmacro -%}
