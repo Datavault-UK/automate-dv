441c4f5c8374cc5597390ae083e9366c0f63933b
Alex Higgs <alex.higgs@data-vault.com>
2024-05-27 23:58:34 +0000

    WIP

--- macros/supporting/ghost_records/create_ghost_record.sql
+++ macros/supporting/ghost_records/create_ghost_record.sql
@@ -19,17 +19,22 @@
 {%- set columns_in_source = adapter.get_columns_in_relation(ref(source_model)) -%}
 {%- set col_definitions = [] -%}
 
+{%- set binary_columns = automate_dv.expand_column_list([src_pk, src_hashdiff]) | map('lower') | list -%}
 {%- set null_columns = automate_dv.expand_column_list([src_payload, src_extra_columns]) | map('lower') | list -%}
-{%- set pk_columns = automate_dv.expand_column_list([src_pk]) | map('lower') | list -%}
+{%- set time_columns = automate_dv.expand_column_list([src_ldts, src_eff]) | map('lower') | list -%}
+
 {%- set all_columns = automate_dv.expand_column_list([src_pk, src_hashdiff, src_payload, src_extra_columns,
                                                       src_eff, src_ldts, src_source]) | list -%}
 
 {%- set filtered_source_columns = columns_in_source | selectattr('column', 'in', all_columns) | list -%}
 {%- set all_columns = all_columns | map('lower') | list -%}
 
+{{ print('binary_columns: ' ~ binary_columns) }}
 {{ print('null_columns: ' ~ null_columns) }}
-{{ print('pk_columns: ' ~ pk_columns) }}
+{{ print('time_columns: ' ~ time_columns) }}
+
 {{ print('all_columns: ' ~ all_columns) }}
+
 {{ print('columns_in_source: ' ~ columns_in_source) }}
 {{ print('filtered_source_columns: ' ~ filtered_source_columns) }}
 
@@ -41,23 +46,19 @@
     {%- set source_system_str = var('system_record_value', 'AUTOMATE_DV_SYSTEM') -%}
 
     {# If src_pk col, use binary ghost unless composite #}
-    {%- if (col_compare in pk_columns) or (col_compare == src_hashdiff) -%}
-        {{ print('is src_pk') }}
-        {%- do col_definitions.append(automate_dv.ghost_for_type(col_type, col_name)) -%}
+    {%- if col_compare in binary_columns -%}
+        {%- do col_definitions.append(automate_dv.binary_ghost(alias=col_name, hash=hash)) -%}
     {# If record source col, replace with system value #}
     {%- elif col_compare == (src_source | lower) -%}
-        {{ print('is src_source') }}
         {%- set col_sql -%}
             CAST('{{ source_system_str }}' AS {{ col.dtype }}) AS {{ src_source }}
         {%- endset -%}
         {%- do col_definitions.append(col_sql) -%}
     {# If column in payload, make its ghost representation NULL  #}
     {%- elif col_compare in null_columns -%}
-        {{ print('is null col') }}
         {%- do col_definitions.append(automate_dv.null_ghost(data_type=col_type, alias=col_name)) -%}
     {# Handle anything else as its correct ghost representation #}
     {%- else -%}
-        {{ print('is other col') }}
         {%- do col_definitions.append(automate_dv.ghost_for_type(col_type, col_name)) -%}
     {%- endif -%}
 
