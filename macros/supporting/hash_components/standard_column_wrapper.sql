4c3ad0a40e189e436cce69a905cfddaca7197533
Alex Higgs <alex.higgs@data-vault.com>
2025-01-17 19:44:21 +0000

    Change year 2024 -> 2025

--- macros/supporting/hash_components/standard_column_wrapper.sql
+++ macros/supporting/hash_components/standard_column_wrapper.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Business Thinking Ltd. 2019-2024
+ * Copyright (c) Business Thinking Ltd. 2019-2025
  * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading as Datavault
  */
 
