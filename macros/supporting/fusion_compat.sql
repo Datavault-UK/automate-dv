eeea20ac739ff48230aa64810882dc6fba1efac1
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2025-09-12 17:25:58 +0200

    Fix typo

--- macros/supporting/fusion_compat.sql
+++ macros/supporting/fusion_compat.sql
@@ -13,10 +13,10 @@
     {# the first case is required to avoid errors #}
     {%- if config == {} -%}
         {{ return(none) }}
-    {%- elif automate_dv.config_meta_get(key) != none -%}
-        {{ return(automate_dv.config_meta_get(key)) }}
+    {%- elif config.get(key) != none -%}
+        {{ return(config.get(key)) }}
     {%- elif config.get("meta") != none and (key in config.get("meta", {}).keys()) -%}
-        {{ return(automate_dv.config_meta_get("meta").get(key)) }}
+        {{ return(config.get("meta").get(key)) }}
     {%- else -%}
         {% do exceptions.raise_compiler_error("Configuration '" ~ key ~ "' is required but was not found under config or meta (Fusion requires custom configuration under meta)") %}
     {%- endif -%}
