cd37dabf420351d08d545940c4b00da0f6888c59
Alex Higgs <alexjhiggs@outlook.com>
2025-12-03 21:45:36 +0000

    Improve and simplify is_checks

--- macros/internal/helpers/is_checks.sql
+++ macros/internal/helpers/is_checks.sql
@@ -4,30 +4,27 @@
  */
 
 {%- macro is_list(obj, empty_is_false=false) -%}
-    {%- do log('IS_LIST 1: ' ~ obj, true) -%}
-    {%- if obj is undefined -%}
-        {%- do log('IS_LIST 2: ' ~ obj, true) -%}
+    {%- if obj is not defined -%}
         {%- do return(false) -%}
+
     {%- elif obj is iterable and obj is not string and obj is not mapping -%}
-        {%- if empty_is_false and (obj is none or not obj) -%}
-            {%- do log('IS_LIST 3: ' ~ obj, true) -%}
+
+        {%- if empty_is_false and (obj | length == 0) -%}
             {%- do return(false) -%}
         {%- else -%}
-            {%- do log('IS_LIST 4: ' ~ obj, true) -%}
             {%- do return(true) -%}
         {%- endif -%}
+
     {%- else -%}
-        {%- do log('IS_LIST 5: ' ~ obj, true) -%}
         {%- do return(false) -%}
     {%- endif -%}
-
 {%- endmacro -%}
 
 
 
 {%- macro is_nothing(obj) -%}
 
-    {%- if (obj is none) or (obj is not defined) or (not obj) or automate_dv.is_list(obj, empty_is_false=true) -%}
+    {%- if obj is not defined or not obj -%}
         {%- do return(true) -%}
     {%- else -%}
         {%- do return(false) -%}
@@ -38,21 +35,11 @@
 
 
 {%- macro is_something(obj) -%}
-    {%- do log('IS_SOMETHING 1: ' ~ obj, true) -%}
-    {%- if obj is not none and obj is defined and obj -%}
-        {#- if an empty list, do not consider the object something -#}
-        {% if automate_dv.is_list(obj, empty_is_false=true) %}
-            {%- do log('IS_SOMETHING 2: ' ~ obj, true) -%}
-            {%- do return(true) -%}
-        {%- else -%}
-            {%- do log('IS_SOMETHING 3: ' ~ obj, true) -%}
-            {%- do return(false) -%}
-        {%- endif -%}
+    {%- if obj is defined and obj -%}
+        {%- do return(true) -%}
     {%- else -%}
-        {%- do log('IS_SOMETHING 4: ' ~ obj, true) -%}
         {%- do return(false) -%}
     {%- endif -%}
-
 {%- endmacro -%}
 
 
