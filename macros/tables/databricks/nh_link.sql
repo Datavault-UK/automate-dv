d36609616f2a1148b5bbaa2836bb697854a5f604
DVAlexHiggs <alex.higgs@data-vault.com>
2025-12-05 13:54:40 +0000

    Added warning for t_link usage, switched macros

--- macros/tables/databricks/nh_link.sql
+++ macros/tables/databricks/nh_link.sql
@@ -3,7 +3,7 @@
  * This software includes code developed by the AutomateDV (f.k.a dbtvault) Team at Business Thinking Ltd. Trading as Datavault
  */
 
-{%- macro databricks__t_link(src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
+{%- macro databricks__nh_link(src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
 {%- set source_cols = automate_dv.expand_column_list(columns=[src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source]) -%}
 {%- set fk_cols = automate_dv.expand_column_list([src_fk]) %}
--- macros/tables/snowflake/nh_link.sql
+++ macros/tables/snowflake/nh_link.sql
@@ -5,11 +5,50 @@
 
 {%- macro nh_link(src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
 
-    {#- This macro is an alias for the t_link macro, so simply calls t_link -#}
+    {{- automate_dv.check_required_parameters(src_pk=src_pk, src_fk=src_fk, src_eff=src_eff,
+                                              src_ldts=src_ldts, src_source=src_source,
+                                              source_model=source_model) -}}
 
-    {{ adapter.dispatch('t_link', 'automate_dv')(src_pk=src_pk, src_fk=src_fk, src_payload=src_payload,
-                                                 src_extra_columns=src_extra_columns,
-                                                 src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
-                                                 source_model=source_model) -}}
+    {{ automate_dv.prepend_generated_by() }}
+
+    {{ adapter.dispatch('nh_link', 'automate_dv')(src_pk=src_pk, src_fk=src_fk, src_payload=src_payload,
+                                                  src_extra_columns=src_extra_columns,
+                                                  src_eff=src_eff, src_ldts=src_ldts, src_source=src_source,
+                                                  source_model=source_model) -}}
 
 {%- endmacro %}
+
+{%- macro default__nh_link(src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source, source_model) -%}
+
+{%- set source_cols = automate_dv.expand_column_list(columns=[src_pk, src_fk, src_payload, src_extra_columns, src_eff, src_ldts, src_source]) -%}
+{%- set fk_cols = automate_dv.expand_column_list([src_fk]) %}
+
+WITH stage AS (
+    SELECT {{ source_cols | join(', ') }}
+    FROM {{ ref(source_model) }}
+    {%- if model.config.materialized == 'vault_insert_by_period' %}
+    WHERE __PERIOD_FILTER__
+    AND {{ automate_dv.multikey(src_pk, condition='IS NOT NULL') }}
+    AND {{ automate_dv.multikey(fk_cols, condition='IS NOT NULL') }}
+    {%- elif model.config.materialized == 'vault_insert_by_rank' %}
+    WHERE __RANK_FILTER__
+    AND {{ automate_dv.multikey(src_pk, condition='IS NOT NULL') }}
+    AND {{ automate_dv.multikey(fk_cols, condition='IS NOT NULL') }}
+    {%- else %}
+    WHERE {{ automate_dv.multikey(src_pk, condition='IS NOT NULL') }}
+    AND {{ automate_dv.multikey(fk_cols, condition='IS NOT NULL') }}
+    {%- endif %}
+),
+records_to_insert AS (
+    SELECT DISTINCT {{ automate_dv.prefix(source_cols, 'stg') }}
+    FROM stage AS stg
+    {% if automate_dv.is_any_incremental() -%}
+    LEFT JOIN {{ this }} AS tgt
+    ON {{ automate_dv.multikey(src_pk, prefix=['stg','tgt'], condition='=') }}
+    WHERE {{ automate_dv.multikey(src_pk, prefix='tgt', condition='IS NULL') }}
+    {%- endif %}
+)
+
+SELECT * FROM records_to_insert
+
+{%- endmacro -%}
