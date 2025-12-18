ece24e469749521f4f1ac8c1d5da022d19bebea9
Jordy Heusdens <jordyheusdens@Jordys-MacBook-Pro.local>
2025-12-18 10:47:15 +0100

    dbt cloud warning

--- macros/supporting/fusion_compat.sql
+++ macros/supporting/fusion_compat.sql
@@ -1,23 +1,35 @@
 {% macro config_meta_get(key, default=none) %}
-    {%- if config.get(key) != none -%}
-        {{ return(config.get(key)) }}
-    {%- elif config.get("meta") != none and (key in config.get("meta", {}).keys()) -%}
-        {{ return(config.get("meta").get(key)) }}
-    {%- else -%}
-        {{ return(default) }}
+    {# 1. Try to get from meta #}
+    {%- set meta_val = config.meta_get(key) -%}
+    {%- if meta_val != none -%}
+        {{ return(meta_val) }}
     {%- endif -%}
+
+    {# 2. If not found in meta, try the standard config #}
+    {# This line is now safe because we know the key is NOT in meta #}
+    {%- set config_val = config.get(key) -%}
+    {%- if config_val != none -%}
+        {{ return(config_val) }}
+    {%- endif -%}
+
+    {# 3. Return default if found in neither #}
+    {{ return(default) }}
 {% endmacro %}
 
 
 {% macro config_meta_require(key) %}
-    {# the first case is required to avoid errors #}
-    {%- if config == {} -%}
-        {{ return(none) }}
-    {%- elif config.get(key) != none -%}
-        {{ return(config.get(key)) }}
-    {%- elif config.get("meta") != none and (key in config.get("meta", {}).keys()) -%}
-        {{ return(config.get("meta").get(key)) }}
-    {%- else -%}
-        {% do exceptions.raise_compiler_error("Configuration '" ~ key ~ "' is required but was not found under config or meta (Fusion requires custom configuration under meta)") %}
+    {# 1. Try to get from meta #}
+    {%- set meta_val = config.meta_require(key) -%}
+    {%- if meta_val != none -%}
+        {{ return(meta_val) }}
     {%- endif -%}
+
+    {# 2. If not found in meta, try the standard config #}
+    {%- set config_val = config.require(key) -%}
+    {%- if config_val != none -%}
+        {{ return(config_val) }}
+    {%- endif -%}
+
+    {# 3. Raise error if found in neither #}
+    {% do exceptions.raise_compiler_error("Configuration '" ~ key ~ "' is required but was not found under config or meta (Fusion requires custom configuration under meta)") %}
 {% endmacro %}
